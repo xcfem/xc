968cb043f8ae7d7af457db0d67efe639f6cb805b
Luis <l.pereztato@gmail.com>
2015-10-08 10:55:58 +0200

    Added two tests to evaluate and verify discretization error in fiber models.

---
+++ verif/tests/materials/fiber_section/test_fiber_section_discretization_error_01.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+# Fiber section model: testing error due to section discretization.
+
+from __future__ import division
+import xc_base
+import geom
+import xc
+from model import predefined_spaces
+from solution import predefined_solutions
+from materials import typical_materials
+
+ancho= 1
+canto= 2
+areaTeor= ancho*canto
+iyTeor= 1/12.0*ancho*canto**3
+izTeor= 1/12.0*canto*ancho**3
+y0= 0
+z0= 0
+F= 1000 # Magnitud de la fuerza
+
+# Problem type
+prueba= xc.ProblemaEF()
+mdlr= prueba.getModelador
+
+# Materials definition
+E= 2.1e6 # Módulo de Young del acero.
+elast= typical_materials.defElasticMaterial(mdlr,"elast",E)
+
+# Section
+geomSCC= mdlr.getMaterialLoader.newSectionGeometry("geomSCC")
+y1= ancho/2.0
+z1= canto/2.0
+regiones= geomSCC.getRegions
+rg= regiones.newQuadRegion("elast")
+rg.nDivIJ= 1
+rg.nDivJK= 2
+nFibTeor= rg.nDivIJ*rg.nDivJK
+rg.pMin= geom.Pos2d(y0-y1,z0-z1)
+rg.pMax= geom.Pos2d(y0+y1,z0+z1)
+
+
+import os
+fiberModel= mdlr.getMaterialLoader.newMaterial("fiber_section_3d","fiberModel")
+fiberSectionRepr= fiberModel.getFiberSectionRepr()
+fiberSectionRepr.setGeomNamed("geomSCC")
+fiberModel.setupFibers()
+fibras= fiberModel.getFibers()
+
+zinf= z0-z1
+zsup= z0+z1
+epsilon= 3.5e-3
+p1= geom.Pos3d(-epsilon,ancho/2.0,zsup)
+p2= geom.Pos3d(-epsilon,-ancho/2.0,zsup)
+p3= geom.Pos3d(epsilon,ancho/2.0,zinf)
+fiberModel.setTrialDeformationPlane(xc.PlanoDeformacion(p1,p2,p3))
+
+N= fiberModel.getStressResultantComponent("N")
+Mz= fiberModel.getStressResultantComponent("Mz")
+My= fiberModel.getStressResultantComponent("My")
+
+nfibras= fibras.getNumFibers()
+Iy= 1/12.0*ancho*canto**3
+fFibra= E*ancho*canto/4.0*epsilon
+MFibras= -2*fFibra*canto/4.0
+
+MyTeor= -2*E*Iy*epsilon/canto
+expectedError= 2*-1/48.0*E*ancho*(canto**2)*epsilon
+MyCorr= My+expectedError
+
+ratio1= (MyCorr-MyTeor)/MyTeor
+ratio2= (MyCorr-My)/MyCorr-0.25
+
+''' 
+print "nfibras= ", nfibras
+print " fFibra= ", fFibra
+print " MFibras= ", MFibras
+print " My= ", My
+print " MyTeor= ", MyTeor
+print " expectedError= ", expectedError
+print " MyCorr= ", MyCorr
+print "ratio1= ", ratio1
+print "ratio2= ", ratio2
+  '''
+  
+fname= os.path.basename(__file__)
+if (abs(ratio1)<1e-15) & (abs(ratio2)<1e-15):
+  print "test ",fname,": ok."
+else:
+  print "test ",fname,": ERROR."
