ea484f25987c5de0935d7c655eb534d0cedf1328
Luis <l.pereztato@gmail.com>
2015-11-13 15:52:16 +0100

    Added method to export CombContainer to neutral combination container (import/export stuff).

--- python_modules/actions/CombContainer.py
+++ python_modules/actions/CombContainer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from import_export import NeutralLoadDescription as nld
+
 '''CombContainer.py: container for load combinations.'''
 
 __author__= "Ana Ortega (AOO) and Luis C. Pérez Tato (LCPT)"
@@ -20,6 +22,15 @@ class SituationCombs(dict):
     self.description= desc
   def add(self,name,expr):
     self[name]= CombinationRecord(name,expr)
+  def getNeutralFormat(self, counter, typ, mapLoadCases):
+    retval= dict()
+    for key in self:
+      comb= self[key]
+      retval[comb.name]= nld.LoadComb(counter,comb.name,self.description,typ,nld.getComponentsFromStr(comb.expr,mapLoadCases))
+      counter+=1
+    return retval
+
+
 
 class SLSCombinations(object):
   def __init__(self):
@@ -28,6 +39,13 @@ class SLSCombinations(object):
     self.freq= SituationCombs('Frequent situations.')
     self.qp= SituationCombs('Quasi-permanent situations.')
     self.earthquake= SituationCombs('Earthquake situations for SLS.')
+  def getNeutralFormat(self, counter, mapLoadCases):
+    retval= self.rare.getNeutralFormat(counter,'SLSR', mapLoadCases)
+    retval.update(self.freq.getNeutralFormat(counter+len(retval),'SLSF', mapLoadCases))
+    retval.update(self.qp.getNeutralFormat(counter+len(retval),'SLSQP', mapLoadCases))
+    retval.update(self.earthquake.getNeutralFormat(counter+len(retval),'SLSS', mapLoadCases))
+    return retval
+    
 
 class ULSCombinations(object):
   def __init__(self):
@@ -36,8 +54,20 @@ class ULSCombinations(object):
     self.acc= SituationCombs('Exceptional (accidental) situations.')
     self.fatigue= SituationCombs('Fatigue situations.')
     self.earthquake= SituationCombs('Earthquake situations for ULS.')
+  def getNeutralFormat(self, counter, mapLoadCases):
+    retval= self.perm.getNeutralFormat(counter,'ULST2', mapLoadCases)
+    retval.update(self.acc.getNeutralFormat(counter+len(retval),'ULST2A', mapLoadCases))
+    retval.update(self.fatigue.getNeutralFormat(counter+len(retval),'ULSF', mapLoadCases))
+    retval.update(self.earthquake.getNeutralFormat(counter+len(retval),'ULSS', mapLoadCases))
+    return retval
 
 class CombContainer(object):
   def __init__(self):
     self.SLS= SLSCombinations()
     self.ULS= ULSCombinations()
+  def getNeutralFormat(self, mapLoadCases):
+    counter= 1
+    retval= self.SLS.getNeutralFormat(counter, mapLoadCases)
+    retval.update(self.ULS.getNeutralFormat(counter+len(retval), mapLoadCases))
+    return retval
+    
