10e3498d082ff5e3419e5df85c2e39778466c05b
Ana <anneisabelleortega@gmail.com>
2015-12-09 10:47:04 +0100

    Comments added to python modules for the generation of RC fiber sections

--- python_modules/materials/fiber_section/creaSetsFibras.py
+++ python_modules/materials/fiber_section/creaSetsFibras.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 class FiberSet:
+  '''This class constructs a set of all the  fibers made of the same material from a fiber section
+  Attributes:
+    scc:       name identifying the fiber section
+    nmbSet:    name of the set of fibers to be generated
+    tagDiag:   tag of the uniaxial material which forms the fibers
+  '''
   fSet= None
 
   def __init__(self,scc,nmbSet,tagDiag):
@@ -11,6 +17,8 @@ class FiberSet:
         self.fSet.insert(f)
     self.fSet.updateCDG()
   def getFiberWithMinStrain(self):
+    '''returns the fiber with the minimum straim from the set of fibers
+    '''
     retval= None
     epsMin= 1e9
     for f in self.fSet:
@@ -20,6 +28,8 @@ class FiberSet:
         epsMin= eps
     return retval
   def getFiberWithMaxStrain(self):
+    '''returns the fiber with the maximum straim from the set of fibers
+    '''
     retval= None
     epsMax= -1e9
     for f in self.fSet:
@@ -30,6 +40,14 @@ class FiberSet:
     return retval     
 
 class RCSets(object):
+  '''This class constructs both the concrete and reinforced steel fiber sets from a reinforced concrete fiber section
+  Attributes:
+    scc:       name identifying the fiber section
+    tagCDiag:  tag of the uniaxial material that makes up the concrete fibers of the section
+    nmbSetC:   name of the set of fibers of concrete to be generated
+    tagSdiag:  tag of the uniaxial material that makes up the reinforcing steel fibers of the section
+    nmbSetS:   name of the set of fibers of reinforcing steel to be generated
+  '''
   fibrasHormigon= None
   fibrasArmadura= None
   tractionFibers= None
@@ -40,6 +58,8 @@ class RCSets(object):
     self.fibrasArmadura= FiberSet(scc,nmbSetS,tagSdiag)
     self.tractionFibers= None
   def reselTractionFibers(self,scc,tractionFibersSetName):
+    '''returns a set with those fibers in tension from the total set 
+    '''
     self.tractionFibers= scc.getFiberSets().create(tractionFibersSetName)
     for f in self.fibrasArmadura.fSet:
       if(f.getMaterial().getStress()>0.0):
@@ -47,14 +67,24 @@ class RCSets(object):
     self.tractionFibers.updateCDG()
     return self.tractionFibers
   def getConcreteArea(self,factor):
+    '''returns the cross section area of concrete in the set of fibers
+    '''
     return self.fibrasHormigon.fSet.getArea(factor)
   def getMaxConcreteStrain(self):
+    '''returns the maximum strain in the set of concrete fibers
+    '''
     return self.fibrasHormigon.fSet.getStrainMax()
   def getConcreteInitialTangent(self):
+    '''returns the initial tangent in the stress-strain diagram of the material that makes up the fibers of concrete
+    '''
     return self.fibrasHormigon.fSet[0].getMaterial().getInitialTangent()
   def getConcreteCompression(self):
+    '''returns the resultant of compressive stresses in the concrete fibers of the section
+    '''
     return self.fibrasHormigon.fSet.ResultanteComp()
   def getNumBarrasTraccion(self):
+    '''returns the number of reinforcing steel fibers in tension
+    '''
     return self.tractionFibers.getNumFibers()
 
 def fiberSectionSetupRCSets(scc,tagCdiag, nmbSetC,tagSdiag, nmbSetS):
@@ -62,6 +92,16 @@ def fiberSectionSetupRCSets(scc,tagCdiag, nmbSetC,tagSdiag, nmbSetS):
 
 # Crea conjuntos de fibras en las secciones de los elementos de conjunto cuyo nombre.
 def creaSetsFibrasHA(preprocessor, nmbSet, tagHA, tagAcero):
+  '''Construct the sets of concrete fibers 'hormigon' and reinforcing steel fibers 'armadura' for all the elements included in a set of elements.
+  Returns the following two sets of fibers:
+    set named hormigon: include all the fibers of material tagHA from the set of elements nmbSet
+    set named armadura: include all the fibers of material tagAcero from the set of elements nmbSet
+  Parameters:
+    mdlr:     preprocessor name
+    nmbSet:   name identifying the set of element
+    tagHA:    tag of the uniaxial material that makes up the concrete fibers
+    tagAcero: tag of the uniaxial material that makes up the reinforcing steel fibers
+  '''
   st= preprocessor.getSets.getSet(nmbSet)
   elements= st.getElements
   for e in elements:
@@ -70,6 +110,11 @@ def creaSetsFibrasHA(preprocessor, nmbSet, tagHA, tagAcero):
     scc.defProp("fiberSets",sets)
 
 def reselTractionFibers(scc,fiberSetName,tractionFibersSetName):
+  '''Returns the fibers under tension included in a set of fibers of a fiber section type
+  Parameters:
+  scc:          name identifying the fiber section
+  fiberSetName: name identifying the set of fibers 
+  '''
   fiberSet= scc.getFiberSets()[fiberSetName]
   tractionFibers= scc.getFiberSets().create(tractionFibersSetName)
   for f in fiberSet:
@@ -78,6 +123,14 @@ def reselTractionFibers(scc,fiberSetName,tractionFibersSetName):
   return tractionFibers
 
 def fiberSectionSetupRC3Sets(scc,tagCdiag, nmbSetC,tagSdiag, nmbSetS):
+  '''returns a set of tensioned fibers 'armaduraTraccion' of a fiber section of reinforced concrete.
+  Parameters:
+  scc:          name identifying the fiber section
+  tagCdiag:     tag of the uniaxial material that makes up the concrete fibers
+  nmbSetC:      name of the set of fibers of concrete to be generated
+  tagSdiag:     tag of the uniaxial material that makes up the reinforcing steel fibers
+  nmbSetS:      name of the set of fibers of reinforcing steel to be generated
+  '''
   sets= fiberSectionSetupRCSets(scc,tagCdiag,nmbSetC,tagSdiag,nmbSetS)
   sets.reselTractionFibers(scc,"armaduraTraccion")
   return sets
