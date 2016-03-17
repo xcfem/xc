0d44637feec5d8afa2135ff79f54a1b2f79aef27
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2016-03-17 18:37:47 +0100

    Comments added to reinforcing steels

--- python_modules/materials/ehe/EHE_reinforcing_steel.py
+++ python_modules/materials/ehe/EHE_reinforcing_steel.py
@@ -2,16 +2,22 @@
 
 '''EHE_reinforcing_steel.py: reinforcing steel as defined in EHE (spanish RC standard).'''
 
-__author__= "Luis C. Pérez Tato (LCPT)"
-__cppyright__= "Copyright 2015, LCPT"
+__author__= "Luis C. Pérez Tato (LCPT) Ana Ortega (AOO)"
+__cppyright__= "Copyright 2015, LCPT, AOO"
 __license__= "GPL"
 __version__= "3.0"
-__email__= "l.pereztato@gmail.com"
+__email__= "l.pereztato@gmail.com ana.Ortega.Ort@gmail.com"
 
 import math
 from materials import reinforcingSteel
 
 # Deformación máxima (0.08 para B400S y 0.05 para B500S)
-B400S= reinforcingSteel.ReinforcingSteel("B400S", 400e6, 0.08,1.15)
-B500S= reinforcingSteel.ReinforcingSteel("B500S", 500e6, 0.05,1.15)
+#Attributes:
+#   nmbAcero: Name identifying the material.
+#   fyk:      Characteristic value of the yield strength.
+#   emax:     maximum strain in tension
+#   gammaS:   Partial factor for material.
+#   k:        fmaxk/fyk ratio
+B400S= reinforcingSteel.ReinforcingSteel(nmbAcero="B400S", fyk=400e6, emax=0.08,gammaS=1.15)
+B500S= reinforcingSteel.ReinforcingSteel(nmbAcero="B500S", fyk=500e6, emax=0.05,gammaS=1.15)
 
--- python_modules/materials/sia262/SIA_reinforcing_steel.py
+++ python_modules/materials/sia262/SIA_reinforcing_steel.py
@@ -10,5 +10,11 @@ from materials import reinforcingSteel
 #               fsd=435MPa,, ks=1,05,, epsud=2,0%
 #  acier B500B: fsk=500MPa,, k=ftk/fsk>=1,08,, epsuk>=5,0%
 #               fsd=435MPa,, ks=1,08,, epsud=4,5%
-B500A= reinforcingSteel.ReinforcingSteel("B500A", 500e6, 0.02,500/435,1.05)
-B500B= reinforcingSteel.ReinforcingSteel("B500B", 500e6, 0.045,500/435,1.08)
+#Attributes:
+#   nmbAcero: Name identifying the material.
+#   fyk:      Characteristic value of the yield strength.
+#   emax:     maximum strain in tension
+#   gammaS:   Partial factor for material.
+#   k:        fmaxk/fyk ratio
+B500A= reinforcingSteel.ReinforcingSteel(nmbAcero="B500A", fyk=500e6, emax=0.02,gammaS=500/435,k=1.05)
+B500B= reinforcingSteel.ReinforcingSteel(nmbAcero="B500B", fyk=500e6, emax=0.045,gammaS=500/435,k=1.08)
