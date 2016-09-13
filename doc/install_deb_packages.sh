349f4c1829ee0ef22bb0b7f0ea2f7234469b3f85
Luis C. Pérez Tato <l.pereztato@gmail.com>
2016-09-13 19:16:50 +0200

    Added matplotlib and mayavi to the Python packages to install.

--- doc/install_deb_packages.sh
+++ doc/install_deb_packages.sh
@@ -14,4 +14,10 @@ if [ $REPLY != "s" ]; then
 	exit 1
 fi
 
-apt-get install cmake cimg-dev g++ gfortran gnuplot libarpack2-dev libarpack++2-dev libcgal-dev libdb-dev libf2c2-dev libglib2.0-dev libgmp3-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libgts-bin libgts-dev libhdf5-mpi-dev liblapack-dev libmpfr-dev libmysql++-dev libparmetis-dev libplot-dev libsqlite3-dev libsuperlu3-dev libsuitesparse-dev libvtk5-dev libX11-dev petsc-dev tcl-dev python-vtk python-scipy python-sympy
+apt-get install cmake cimg-dev g++ gfortran gnuplot libarpack2-dev libarpack++2-dev libcgal-dev libdb-dev libf2c2-dev libglib2.0-dev libgmp3-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libgts-bin libgts-dev libhdf5-mpi-dev liblapack-dev libmpfr-dev libmysql++-dev libparmetis-dev libplot-dev libsqlite3-dev libsuperlu3-dev libsuitesparse-dev libvtk5-dev libX11-dev petsc-dev tcl-dev python-vtk python-scipy python-sympy python-matplotlib
+
+#mayavi installation. Some 'mayavi' packages seems
+#to require VTK 6 so we use pip. If you're a Debian user
+#you can help us with this sending us your comments. 
+
+sudo -H pip install mayavi
