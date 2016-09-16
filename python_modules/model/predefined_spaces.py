0d0129eedaeb7da0a32bd69c68761f4d40990eed
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2016-09-16 09:05:03 +0200

    Comments added to predefined spaces

--- python_modules/model/predefined_spaces.py
+++ python_modules/model/predefined_spaces.py
@@ -1,29 +1,43 @@
 # -*- coding: utf-8 -*-
 
+__author__= "Luis C. Pérez Tato (LCPT) , Ana Ortega (A_OO) "
+__copyright__= "Copyright 2016, LCPT, A_OO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com, ana.Ortega.Ort@gmail.com "
+
 import xc
 
-# Definición de nodos habitual en problemas de
-# elasticidad y resistencia de materiales.
 
 
 def gdls_elasticidad2D(nodos):
+  '''Defines the dimension of the space: nodes by two coordinates (x,y) and two DOF for each node (Ux,Uy)
+
+  :param nodos: nodes loader
+  '''
   nodos.dimEspace= 2 #two coord. for each node (x,y).
   nodos.numGdls= 2 #two DOF for each node (Ux,Uy).
 
-# Definición de nodos habitual en problemas de
-# resistencia de materiales en dos dimensiones: 2 coordenadas por
-# nodo y 3 grados de libertad por nodo: (ux,uy,theta)
 def gdls_resist_materiales2D(nodos):
+  '''Defines the dimension of the space: nodes by two coordinates (x,y) and three DOF for each node (Ux,Uy,theta)
+
+  :param nodos: nodes loader
+  '''
   nodos.dimEspace= 2 #two coord. for each node (x,y).
   nodos.numGdls= 3 #three DOF for each node (Ux,Uy,theta).
 
 def gdls_elasticidad3D(nodos):
+  '''Defines the dimension of the space: nodes by three coordinates (x,y,z) and three DOF for each node (Ux,Uy,Uz)
+
+  :param nodos: nodes loader
+  '''
   nodos.dimEspace= 3 #three coord. for each node (x,y,z).
   nodos.numGdls= 3 #three DOF for each node (Ux,Uy,Uz).
 
-# Definición de nodos habitual en problemas de
-# resistencia de materiales en tres dimensiones: 3 coordenadas por
-# nodo y 6 grados de libertad por nodo: (ux,uy,uz,thetaX,thetaY,thetaZ)
 def gdls_resist_materiales3D(nodos):
+  '''Defines the dimension of nodes  three coordinates (x,y,z) and six DOF for each node (Ux,Uy,Uz,thetaX,thetaY,thetaZ)
+
+  :param nodos: nodes loader
+  '''
   nodos.dimEspace= 3 #two coord. for each node (x,y,z).
   nodos.numGdls= 6 #three DOF for each node (ux,uy,uz,thetaX,thetaY,thetaZ)
