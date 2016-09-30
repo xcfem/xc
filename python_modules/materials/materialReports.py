f10c314732529226f5602270dec2d2f94627e9c9
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2016-09-30 17:08:38 +0200

    utils to generate concrete02 reports

--- python_modules/materials/materialReports.py
+++ python_modules/materials/materialReports.py
@@ -19,26 +19,22 @@ class concreteReport(object):
         ''':returns: a LaTeX table that depicts the concrete compressive 
         properties.
         '''
-        retval='\\begin{tabular}{lr} \n '
-        retval+='\\multicolumn{2}{c}{Compressive prop.} \\\\ \n '
+        retval='\\multicolumn{2}{c}{Compressive properties} \\\\ \n '
         retval+='\\hline \n '
         retval+='$f_{c}$ & ' + str(self.concr.fpc*1e-6) + ' MPa \\\\ \n '
         retval+='$\\epsilon_{c0}$ & ' + str(self.concr.epsc0*1e3) + ' $\\permil$ \\\\ \n '
         retval+='$f_{cu}$ & ' + str(self.concr.fpcu*1e-6) + ' MPa \\\\ \n '
         retval+='$\\epsilon_{cu}$ & ' + str(self.concr.epscu*1e3) + ' $\\permil$ \\\\ \n '
         retval+='$E_{c0}$ & ' + str(self.concr.getInitialTangent()*1e-9) + ' GPa \\\\ \n '
-        retval+='\\end{tabular} \n '
         return retval
     
     def tableTens(self):
         ''':returns: a LaTeX table that depicts the concrete tensile 
         properties.
         '''
-        retval='\\begin{tabular}{lr} \n '
-        retval+='\\multicolumn{2}{c}{Tensile prop.} \\\\ \n '
+        retval='\\multicolumn{2}{c}{Tensile properties} \\\\ \n '
         retval+='\\hline \n '
-        retval+='$f_{ct}$ & ' + str(self.concr.ft*1e-6) + ' MPa \\\\ \n '
-        retval+='\\end{tabular} \n '
+        retval+='$f_{ct}$ & ' + str(round(self.concr.ft*1e-6,2)) + ' MPa \\\\ \n '
         return retval
 
     def tableTensStiff(self,paramTensStiffeness):
@@ -48,15 +44,13 @@ class concreteReport(object):
         concreteBase.paramTensStiffenes that defines the parameters 
         of tension stiffenes
         '''
-        retval='\\begin{tabular}{ll} \n '
-        retval+='\\multicolumn{2}{c}{Tension-stiffening} \\\\ \n '
+        retval='\\multicolumn{2}{c}{Tension-stiffening} \\\\ \n '
         retval+='\\hline \n '
         retval+='$\\nu$ & ' + str(paramTensStiffeness.nu())+'  \\\\ \n '
         retval+='$\\rho_{eff}$ & ' + str(paramTensStiffeness.ro*1e2) +'\%  \\\\ \n '
         retval+='\\multicolumn{2}{l}{exponential curve $\\alpha$=' + str(round(paramTensStiffeness.alfa(),3)) +'} \\\\ \n '
         retval+='\\multicolumn{2}{l}{slope regression line:' + str(round(paramTensStiffeness.regresLine()['slope']*1e-9,2)) +' } \\\\ \n '
-        retval+='\\multicolumn{2}{l}{intersection point (onset carcking):} \\\\ \n '
+        retval+='\\multicolumn{2}{l}{inters. point (onset carcking):} \\\\ \n '
         retval+='\\multicolumn{2}{c}{($\\epsilon$='+str(round(paramTensStiffeness.pointOnsetCracking()['eps_ct']*1e2,2)) +'\\%, $\\sigma$='+ str(round(paramTensStiffeness.pointOnsetCracking()['ft']*1e-6,2)) + 'MPa)} \\\\ \n '
-        retval+='\\end{tabular} \n '
         return retval
         
