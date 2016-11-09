5fe05452e6808f70d959f7132ac75453971da23a
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2016-11-09 16:11:13 +0100

    utils for graphical reports on loads

--- python_modules/postprocess/reports/graphical_reports.py
+++ python_modules/postprocess/reports/graphical_reports.py
@@ -6,68 +6,102 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= " ana.Ortega@ciccp.es "
 
+import xc_base
+import geom
+import xc
 from xcVtk.malla_ef import vtk_grafico_ef
 from xcVtk.malla_ef import Fields
+from postprocess import utils_display
 
 class RecordLoadCaseDisp(object):
   '''Generation of graphic files and adding to report-tex files for a load case
   
-  :ivar loadCaseName:   name of the load case to be depicted
-  :ivar setToDisplay:   set of elements to be displayed
-                   (defaults to 'total')
-  :ivar unitsScale:     factor to apply to the results if we want to change
+  :ivar loadCaseName:  name of the load case to be depicted
+  :ivar loadCaseDescr: description text of the load case
+  :ivar loadCaseExpr:  mathematical expression to define the load case (ex:
+                   '1.0*GselfWeight+1.0*DeadLoad')
+  :ivar setsToDispLoads: ordered list of sets of elements to display loads
+  :ivar unitsScaleLoads: factor to apply to loads if we want to change
                    the units (defaults to 1).
-  :ivar vectorScale:    factor to apply to the vectors length in the 
-                   representation (defaults to 1).
-  :ivar multByElemArea: boolean value that must be True if we want to 
+  :ivar unitsLoads: text to especify the units in which loads are 
+                   represented (defaults to 'units:[kN,m]')
+  :ivar vectorScaleLoads: factor to apply to the vectors length in the 
+                   representation of loads (defaults to 1).
+  :ivar multByElemAreaLoads: boolean value that must be True if we want to 
                    represent the total load on each element 
                    (=load multiplied by element area) and False if we 
                    are going to depict the value of the uniform load 
                    per unit area (defaults to False)
-  :ivar viewName:       name of the view  that contains the renderer (possible
+  :ivar listDspRot: ordered list of displacement or rotations to be displayed
+                   available components: 'uX', 'uY', 'uZ', 'rotX', rotY', 'rotZ'
+                   (defaults to ['uX', 'uY', 'uZ'])
+  :ivar setsToDispDspRot: ordered list of sets of elements to display displacements 
+                   or rotations
+  :ivar unitsScaleDispl: factor to apply to displacements if we want to change
+                   the units (defaults to 1).
+  :ivar unitsDispl: text to especify the units in which displacements are 
+                   represented (defaults to '[m]'
+  :ivar listIntForc: ordered list of internal forces to be displayed
+                   available components: 'N1', 'N2', 'M1', 'M2', 'Q1', 'Q2'
+                   (defaults to ['N1', 'N2', 'M1', 'M2', 'Q1', 'Q2'])
+  :ivar setsToDispIntForc: ordered list of sets of elements to display internal      
+                   forces
+  :ivar unitsScaleForc: factor to apply to internal forces if we want to change
+                   the units (defaults to 1).
+  :ivar unitsForc: text to especify the units in which forces are 
+                   represented (defaults to '[kN/m]')
+  :ivar unitsScaleMom: factor to apply to internal moments if we want to change
+                   the units (defaults to 1).
+  :ivar unitsMom:  text to especify the units in which bending moments are 
+                   represented (defaults to '[kN.m/m]')
+  :ivar viewName:  name of the view  that contains the renderer (possible
                    options: "XYZPos", "XPos", "XNeg","YPos", "YNeg",
                    "ZPos", "ZNeg") (defaults to "XYZPos")
-  :ivar descGrph:       description text to be displayed in the graphic
-  :ivar captTex:        caption to describe the graphic content in the tex file
-  :ivar lablTex:        label to be associated to the graphic in the tex file
+
   '''
 
-  def __init__(self,loadCaseName):
+  def __init__(self,loadCaseName,loadCaseDescr,loadCaseExpr,setsToDispLoads,setsToDispDspRot,setsToDispIntForc):
     self.loadCaseName=loadCaseName
-    self.setToDisplay=None
-    self.unitsScale=1.0
-    self.vectorScale=1.0
-    self.multByElemArea=False
+    self.loadCaseDescr=loadCaseDescr
+    self.loadCaseExpr=loadCaseExpr
+    self.setsToDispLoads=setsToDispLoads
+    self.unitsScaleLoads=1.0
+    self.unitsLoads='units:[kN,m]'
+    self.vectorScaleLoads=1.0
+    self.multByElemAreaLoads=False
+    self.listDspRot=['uX', 'uY', 'uZ']
+    self.setsToDispDspRot=setsToDispDspRot
+    self.unitsScaleDispl=1.0
+    self.unitsDispl='[m]'
+    self.listIntForc=['N1', 'N2', 'M1', 'M2', 'Q1', 'Q2']
+    self.setsToDispIntForc=setsToDispIntForc
+    self.unitsScaleForc=1.0
+    self.unitsForc='[kN/m]'
+    self.unitsScaleMom=1.0
+    self.unitsMom='[kN.m/m]'
     self.viewName="XYZPos"
-    self.descGrph=''
-    self.captTex=''
-    self.lablTex=''
 
-  def createGraphicFile(self,gridmodl,fileName):
-    '''Creates the graphic file
+  def loadReports(self,gridmodl,pathGr,texFile,grWdt):
+    '''Creates the graphics files of loads for the load case and insert them in
+    a LaTex file
     
     :param gridmodl:   object of type GridModel
-    :param fileName:   full name of the graphic file to be created
+    :param pathGr:     directory to place figures (ex: 'text/graphics/loads/')
+    :param texFile:    laTex file where to include the graphics 
+                       (e.g.:'text/report_loads.tex')
+    :param grWdt:      width to be applied to graphics
     '''
-    gridmodl.displayLoad(self.setToDisplay,self.loadCaseName,self.unitsScale,self.vectorScale, self.multByElemArea,self.viewName,self.descGrph,fileName)
+    for st in self.setsToDispLoads:
+      grfname=pathGr+self.loadCaseName+st.elSet.name
+      capt=self.loadCaseDescr + ', ' + st.genDescr + ', '  + self.unitsLoads
+      gridmodl.displayLoad(setToDisplay=st.elSet,loadCaseNm=self.loadCaseName,unitsScale=self.unitsScaleLoads,vectorScale=self.vectorScaleLoads, multByElemArea=self.multByElemAreaLoads,viewNm=self.viewName,caption= capt,fileName=grfname+'.jpg')
+      gridmodl.displayLoad(setToDisplay=st.elSet,loadCaseNm=self.loadCaseName,unitsScale=self.unitsScaleLoads,vectorScale=self.vectorScaleLoads, multByElemArea=self.multByElemAreaLoads,viewNm=self.viewName,caption= capt,fileName=grfname+'.eps')
+      texFile.write('\\begin{center}\n')
+      texFile.write('\includegraphics[width='+grWdt+']{'+grfname+'}\n')
+      texFile.write('\caption{'+capt+'}\n')
+      texFile.write('\end{center}\n')
     return
 
-  def insertGraphic(self,grfileNm,grWdt,texFile):
-    '''Inserts the graphic file in the report tex file
-    
-    :param grfileNm:   name of the graphics file
-    :param grWdt:      width of the grafic 
-    :param texFile:    opened tex file in which the graphic is being inserted
-    '''
-    grFileNameNoPath=grfileNm.split('/')[grfileNm.count('/')]
-    texFile.write('\\begin{center}\n')
-    texFile.write('\includegraphics[width='+grWdt+']{'+grFileNameNoPath+'}\n')
-    if len(self.captTex)<>0:
-      texFile.write('\caption{'+self.captTex+'}\n')
-    if len(self.lablTex)<>0:
-      texFile.write('\label{'+self.lablTex+'}\n')
-    texFile.write('\end{center}\n')
-    return
 
 def checksReports(limitStateLabel,setsToReport,argsToReport,capTexts,pathGr,texReportFile):
     report=open(texReportFile,'w')    #report latex file
