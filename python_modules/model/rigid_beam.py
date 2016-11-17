7e4f4bda646d446fd79f7bc2ea5af1c4c84555c9
Luis C. Pérez Tato <l.pereztato@gmail.com>
2016-11-17 10:33:33 +0100

    Documentation of routines from rigid_beam.py

--- python_modules/model/rigid_beam.py
+++ python_modules/model/rigid_beam.py
@@ -3,19 +3,33 @@
 import xc_base
 import xc
 from materials import typical_materials
+from miscUtils import LogMessages as lmsg
 
-# Crea un rigid_link entre los nodos que se pasan como parámetro.
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2016,LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
 def creaRigidBeamNodos(preprocessor,tagNodA, tagNodB):
+  '''Create a rigid_link between the nodes being passed as parameters.
+
+  :param   tagNodA: tag of the master node.
+  :param   tagNodB: tag of the slave node.
+  '''
   return preprocessor.getConstraintLoader.newRigidBeam(tagNodA,tagNodB)
 
 
-'''
-Crea un rigid_link articulado en tabNodoB
+def creaFulcrumNodos3d(preprocessor, tagNodA, tagNodB):
+  '''
+   Crea un rigid_link articulado en tabNodoB
    entre los nodos que se pasan como parámetro.
    Le llamamos «fulcrum» porque puede pivotar
-   en torno al nodeo designado por tagNodoB
-'''
-def creaFulcrumNodos3d(preprocessor, tagNodA, tagNodB):
+   en torno al node designado por tagNodoB
+
+  :param   tagNodA: tag of the master node.
+  :param   tagNodB: tag of the slave node.
+  '''
   nodos= preprocessor.getNodeLoader
   coordNodoB= nodos.getNode(tagNodB).getCoo
   fulcrumNode= nodos.newNodeFromVector(coordNodoB)
@@ -23,18 +37,27 @@ def creaFulcrumNodos3d(preprocessor, tagNodA, tagNodB):
   rb= constraints.newRigidBeam(tagNodA,fulcrumNode.tag)
   ed= constraints.newEqualDOF(fulcrumNode.tag,tagNodB,xc.ID([0,1,2]))
 
-# Crea un rigid_beam entre los nodos de los puntos que se pasan como parámetro.
-def creaRigidBeamPuntos(preprocessor,idPuntoA, idPuntoB):
-  tagNodeA= preprocessor.getPoint(idPuntoA).getTagNode
-  tagNodeB= preprocessor.getPoint(igPuntoB).getTagNode
+def creaRigidBeamPuntos(preprocessor,idPointA, idPointB):
+  '''Create a rigid_link between the nodes at points passed as parameters.
+
+  :param   idPointA: id of the point that "contains" the master node.
+  :param   idPointB: id of the point that "contains" the slave node.
+  '''
+  tagNodeA= preprocessor.getPoint(idPointA).getTagNode
+  tagNodeB= preprocessor.getPoint(igPointB).getTagNode
   creaRigidBeamNodos(preprocessor,tagNodeA,tagNodeB)
 
-'''
-Coloca una barra gorda (muy rígida) entre los nodos que se pasan como parámetro.
-   (lo uso porque las condiciones de contorno multipunto hacen cosas raras con los valores de las
-    reacciones)
-'''
-def creaBarraGordaNodos(preprocessor,tagNodA, tagNodB, nmbTransf):
+def putHuge3DBarBetweenNodes(preprocessor,tagNodA, tagNodB, nmbTransf):
+  '''
+  Creates a very stiff bar between the two nodes being passed as parameters.
+  (it's a workaround to the problem with the reactions values in nodes when
+  using multipoint constraints. This problem will be solved with the
+  implementation of MP_ConstraintBase::addResistingForceToNodalReaction).
+
+  :param   tagNodA: tag of bar's from node.
+  :param   tagNodB: tag of bar's to node.
+  :param   nmbTransf: name of the coordinate transformation to use for the new bar.
+  '''
   elementos= preprocessor.getElementLoader
   elementos.defaultTransformation= nmbTransf
   # Material definition
@@ -55,12 +78,22 @@ def creaBarraGordaNodos(preprocessor,tagNodA, tagNodB, nmbTransf):
   #print "A= ", elem.sectionProperties.A
   return elem
 
-'''
-Coloca una barra gorda (muy rígida) entre los nodos de los puntos que se pasan como parámetro.
-   (lo uso porque las condiciones de contorno multipunto hacen cosas raras con los valores de las
-    reacciones)
-'''
-def creaBarraGordaPuntos(idPuntoA, idPuntoB, nmbTransf):
-  tagNodeA= preprocessor.getPoint(idPuntoA).getTagNode
-  tagNodeB= preprocessor.getPoint(igPuntoB).getTagNode
+def creaBarraGordaNodos(preprocessor,tagNodA, tagNodB, nmbTransf):
+  '''DEPRECATED; use putHuge3DBarBetweenNodes.'''
+  lmsg.warning('creaBarraGordaNodos DEPRECATED; use putHuge3DBarBetweenNodes.')
+  return putHuge3DBarBetweenNodes(preprocessor,tagNodA, tagNodB, nmbTransf)
+
+def creaBarraGordaPuntos(idPointA, idPointB, nmbTransf):
+  '''
+  Creates a very stiff bar between the nodes on the two points being passed
+  as parameters. (it's a workaround to the problem with the reactions values
+  in nodes when using multipoint constraints. This problem will be solved with the
+  implementation of MP_ConstraintBase::addResistingForceToNodalReaction).
+
+  :param   tagNodA: tag of bar's from node.
+  :param   tagNodB: tag of bar's to node.
+  :param   nmbTransf: name of the coordinate transformation to use for the new bar.
+  '''
+  tagNodeA= preprocessor.getPoint(idPointA).getTagNode
+  tagNodeB= preprocessor.getPoint(igPointB).getTagNode
   return creaBarraGordaNodos(tagNodeA,tagNodeB)
