24191a1c3bd19636c5335c30cbebb15a5c95d1af
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2017-03-01 17:50:46 +0100

    doc strings

--- python_modules/model/element_vectors.py
+++ python_modules/model/element_vectors.py
@@ -5,15 +5,21 @@ import xc
 
 # Vectores de elementos unidimensionales.
 def getVectorIElem(preprocessor,idElem):
+  '''return the vector in the I direction of the 1D element 
+  '''
   elem= preprocessor.getElementLoader.getElement(idElem)
   return elem.getCoordTransf.getIVector
 
 
 def getVectorJElem(preprocessor,idElem):
+  '''return the vector in the J direction of the 1D element 
+  '''
   elem= preprocessor.getElementLoader.getElement(idElem)
   return elem.getCoordTransf.getJVector
 
 def getVectorKElem(preprocessor,idElem):
+  '''return the vector in the K direction of the 1D element 
+  '''
   elem= preprocessor.getElementLoader.getElement(idElem)
   return elem.getCoordTransf.getKVector
 
