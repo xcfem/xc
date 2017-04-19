35813c3dca7448919de62de2dd2df5ed1e0e8a32
Luis C. Pérez Tato <l.pereztato@gmail.com>
2017-04-19 20:52:47 +0200

    Added a test to verify handling of multi-freedom constraints with transformation constraint handlers.

---
+++ verif/tests/solution/constraint_handler/transformation_handler_test_03.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+# Home made test
+# Testing multi-freedom conditions with transformation constraint handler.
+
+import xc_base
+import geom
+import xc
+from solution import predefined_solutions
+from model import predefined_spaces
+from materials import typical_materials
+from model import fix_node_6dof
+import math
+
+# Material properties
+E= 2.1e6*9.81/1e-4 # Elastic modulus (Pa)
+nu= 0.3 # Poisson's ratio
+G= E/(2*(1+nu)) # Shear modulus
+
+# Cross section properties (IPE-80)
+A= 7.64e-4 # Cross section area (m2)
+Iy= 80.1e-8 # Cross section moment of inertia (m4)
+Iz= 8.49e-8 # Cross section moment of inertia (m4)
+J= 0.721e-8 # Cross section torsion constant (m4)
+
+
+# Geometry
+L= 15 # Bar length (m)
+
+# Load
+F= 1.5e3 # Load magnitude (kN)
+
+# Problem type
+prueba= xc.ProblemaEF()
+preprocessor=  prueba.getPreprocessor   
+
+# Materials
+caracMecSeccion= xc.CrossSectionProperties3d()
+caracMecSeccion.A= A; caracMecSeccion.E= E; caracMecSeccion.G= G;
+caracMecSeccion.Iz= Iz; caracMecSeccion.Iy= Iy; caracMecSeccion.J= J
+seccion= typical_materials.defElasticSectionFromMechProp3d(preprocessor, "seccion",caracMecSeccion)
+
+#Nodes
+nodes= preprocessor.getNodeLoader
+predefined_spaces.gdls_resist_materiales3D(nodes)
+nodes.defaultTag= 1 #First node number.
+nod1= nodes.newNodeXYZ(0,0.0,0.0)
+nod2= nodes.newNodeXYZ(L/2.0,0.0,0.0)
+nod3= nodes.newNodeXYZ(L,0.0,0.0)
+
+trfs= preprocessor.getTransfCooLoader
+lin= trfs.newLinearCrdTransf3d("lin")
+lin.xzVector= xc.Vector([0,1,0])
+
+elementos= preprocessor.getElementLoader
+elementos.defaultTransformation= "lin"
+elementos.defaultMaterial= "seccion"
+elementos.defaultTag= 1 #Tag for next element.
+beam3d= elementos.newElement("elastic_beam_3d",xc.ID([nod1.tag,nod2.tag]));
+
+# Constraints
+coacciones= preprocessor.getConstraintLoader
+
+fix_node_6dof.fixNode6DOF(coacciones,1)
+
+rr= preprocessor.getConstraintLoader.newRigidBeam(nod2.tag,nod3.tag)
+
+
+# Loads definition
+cargas= preprocessor.getLoadLoader
+
+casos= cargas.getLoadPatterns
+
+#Load modulation.
+ts= casos.newTimeSeries("constant_ts","ts")
+casos.currentTimeSeries= "ts"
+#Load case definition
+lp0= casos.newLoadPattern("default","0")
+lp0.newNodalLoad(2,xc.Vector([F,0,0,0,0,0]))
+#We add the load case to domain.
+casos.addToDomain("0")
+
+# Solution
+import os
+pth= os.path.dirname(__file__)
+#print "pth= ", pth
+if(not pth):
+  pth= "."
+execfile(pth+"/../../aux/solu_transf_handler2.py")
+
+delta= nodes.getNode(nod3.tag).getDisp[0] #x displacement of node 3.
+elementos.getElement(1).getResistingForce()
+N1= elementos.getElement(nod1.tag).getN1
+
+deltateor= (F*0.5*L/(E*A))
+ratio1= (delta-deltateor)/deltateor
+ratio2= (N1-F)/F
+
+# print delta
+# print deltateor
+# print ratio1
+# print N1
+# print ratio2
+
+
+import os
+fname= os.path.basename(__file__)
+if abs(ratio1)<1e-5 and abs(ratio2)<1e-5:
+  print "test ",fname,": ok."
+else:
+  print "test ",fname,": ERROR."
