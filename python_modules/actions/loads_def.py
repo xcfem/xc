fac3e4728c59ff261bce630935ec7c60e40a1d71
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2017-05-29 08:49:45 +0200

    load definition

--- python_modules/actions/loads_def.py
+++ python_modules/actions/loads_def.py
@@ -81,6 +81,33 @@ class UniformLoadOnBeams(object):
                     e.vector3dUniformLoadGlobal(self.loadVector)
  
 
+class UniformLoadOnLines(object):
+    '''Uniform load applied to all the lines (not necessarily defined as lines
+    for latter generation of beam elements, they can be lines belonging to 
+    surfaces for example) found in the xcSet
+    The uniform load is introduced as point loads in the nodes
+    
+    :ivar name:   name identifying the load
+    :ivar xcSet:  set that contains the lines
+    :ivar loadVector: xc.Vector with the six components of the load: 
+                      xc.Vector([Fx,Fy,Fz,Mx,My,Mz]).
+    '''                            
+    def __init__(self,name,  xcSet, loadVector):
+        self.name=name
+        self.xcSet=xcSet
+        self.loadVector= loadVector
+
+    def appendLoadToCurrentLoadPattern(self):
+        for l in self.xcSet.getLines:
+            nod=[n for n in l.getNodes()]
+            ndistOrig=[n.getCoo.Norm() for n in nod]
+            sortNod=[nod for ndistOrig,nod in sorted(zip(ndistOrig,nod))]
+            lnInfl=[(sortNod[i-1].getInitialPos3d).distPos3d(sortNod[i+1].getInitialPos3d)/2 for i in range(1,len(sortNod)-1)]
+            lnInfl.insert(0,(sortNod[0].getInitialPos3d).distPos3d(sortNod[1].getInitialPos3d)/2.0)
+            lnInfl.append((sortNod[len(sortNod)-2].getInitialPos3d).distPos3d(sortNod[len(sortNod)-1].getInitialPos3d)/2.0)
+        for i in range(len(sortNod)):
+            sortNod[i].newLoad(lnInfl[i]*self.loadVector)
+
 class UniformLoadOnSurfaces(object):
     '''Uniform load applied on the shell elements generated from
     all the surfaces in the xcSet.
@@ -89,17 +116,24 @@ class UniformLoadOnSurfaces(object):
     :ivar xcSet:      set that contains the surfaces
     :ivar loadVector: xc.Vector with the six components of the load: 
                       xc.Vector([Fx,Fy,Fz,Mx,My,Mz]).
+    :ivar refSystem: reference system in which loadVector is defined:
+                     'Local': element local coordinate system
+                     'Global': global coordinate system (defaults to 'Global)
     '''
-    def __init__(self,name, xcSet, loadVector):
+    def __init__(self,name, xcSet, loadVector,refSystem='Global'):
         self.name=name
         self.xcSet=xcSet
         self.loadVector= loadVector
-
+        self.refSystem=refSystem
+        
     def appendLoadToCurrentLoadPattern(self):
         ''' Append load to the current load pattern.'''
         for s in self.xcSet.getSurfaces:
             for e in s.getElements():
-                e.vector3dUniformLoadGlobal(self.loadVector)
+                if self.refSystem=='Local':
+                    e.vector3dUniformLoadLocal(self.loadVector)
+                else:
+                    e.vector3dUniformLoadGlobal(self.loadVector)
 
 class EarthPressLoad(object):
     '''Earth pressure applied on the shell elements generated from
@@ -120,6 +154,7 @@ class EarthPressLoad(object):
         self.xcSet=xcSet
         self.soilData=soilData
         self.vDir=vDir
+
     def appendLoadToCurrentLoadPattern(self):
         ''' Append load to the current load pattern.'''
         for s in self.xcSet.getSurfaces:
@@ -129,4 +164,28 @@ class EarthPressLoad(object):
                     e.vector3dUniformLoadGlobal(presElem*self.vDir)
  
 
- 
+# TO DO: change the method in order to be able to append to current load pattern
+class StrainGradientLoadOnSurfaces(object):
+    '''Strain gradient applied on the shell elements generated from
+    all the surfaces in the xcSet. 
+    
+    :ivar name:  name identifying the load
+    :ivar xcSet: set that contains the surfaces
+    :ivar nabla: strain gradient in the thickness of the elements:
+                 nabla=espilon/thickness    
+    '''
+    def __init__(self,name, xcSet,nabla):
+        self.name=name
+        self.xcSet=xcSet
+        self.nabla=nabla
+    
+    def appendLoadToLoadPattern(self,loadPattern):
+        ''' Append load to the current load pattern.'''
+        for s in self.xcSet.getSurfaces:
+            for e in s.getElements():
+                eLoad= loadPattern.newElementalLoad("shell_strain_load")
+                eLoad.elementTags= xc.ID([e.tag])
+                eLoad.setStrainComp(0,3,self.nabla)
+                eLoad.setStrainComp(1,3,self.nabla)
+                eLoad.setStrainComp(2,3,self.nabla)
+                eLoad.setStrainComp(3,3,self.nabla)
