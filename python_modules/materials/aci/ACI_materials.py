2e4cbfc6692de5bf216d0af93edd92f5b2ff04bb
Luis C. Pérez Tato <l.pereztato@gmail.com>
2017-06-04 16:11:08 +0200

    Rearranging Python modules.

--- python_modules/materials/aci/ACI_materials.py
+++ python_modules/materials/aci/ACI_materials.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-''' Concrete material according to ACI 318 VERIFICATION TESTS REQUIRED!!!.'''
+''' Reinforced concrete materials according to ACI 318. VERIFICATION TESTS REQUIRED!!!.'''
 
 from materials import concreteBase
 from miscUtils import LogMessages as lmsg
+from materials import reinforcingSteel
 
 __author__= "Ana Ortega (A_OO) and Luis C. Pérez Tato (LCPT)"
 __copyright__= "Copyright 2015, A_OO and LCPT"
@@ -18,6 +19,25 @@ class ACIConcrete(concreteBase.Concrete):
         super(ACIConcrete,self).__init__(nmbConcrete,fck, gammaC)
         lmsg.error('Implementation of ACI concrete pending\n.')
         
-# EHE concretes
+# ACI concretes
 A36M= ACIConcrete(nmbConcrete="A36M",fck=-20e6,gammaC=1.5)
 
+# Reinforcing steel.
+
+A706G60= reinforcingSteel.ReinforcingSteel(nmbAcero="A706G60", fyk=420e6, emax=0.08,gammaS=1.15)
+
+
+#Bar areas in square meters.
+
+num3Area= 71e-6
+num4Area= 129e-6
+num5Area= 200e-6
+num6Area= 284e-6
+num7Area= 387e-6
+num8Area= 509e-6
+num9Area= 645e-6
+num10Area= 819e-6
+num11Area= 1006e-6
+num14Area= 1452e-6
+num18Area= 2581e-6
+num18JArea= 2678e-6
