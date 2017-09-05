df0231598bbc67b293915424ba1d00f8b3d250d2
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2017-09-05 16:36:56 +0200

    Minor debugging

--- python_modules/model/boundary_cond/spring_bound_cond.py
+++ python_modules/model/boundary_cond/spring_bound_cond.py
@@ -21,30 +21,32 @@ class SpringBC(object):
     '''Spring to be used as boundary condition
 
     :ivar name: name
+    :ivar modelSpace: model space
     :ivar Kx: spring stiffness in X direction (defaults to 0 -> free)
     :ivar Ky: spring stiffness in Y direction (defaults to 0 -> free)
     :ivar Kz: spring stiffness in Z direction (defaults to 0 -> free)
     '''
-    def __init__(self,name,Kx=0,Ky=0,Kz=0):
+    def __init__(self,name,modelSpace,Kx=0,Ky=0,Kz=0):
         self.name=name
+        self.modelSpace=modelSpace
         self.Kx=Kx
         self.Ky=Ky
         self.Kz=Kz
         self.springMat=[None,None,None]  #material initialization (no springs)
     
-    def createSpringMaterials(self,modelSpace):
+    def createSpringMaterials(self):
         '''create the spring materials in X, Y, Z directions. If there is
         no spring in any of these directions, its corresponding material is
         equal to None 
         '''
         if self.Kx != 0:
-            typical_materials.defElasticMaterial(modelSpace.preprocessor,self.name + '_xSpring',self.Kx)
+            typical_materials.defElasticMaterial(self.modelSpace.preprocessor,self.name + '_xSpring',self.Kx)
             self.springMat[0]=self.name + '_xSpring'
         if self.Ky != 0:
-            typical_materials.defElasticMaterial(modelSpace.preprocessor,self.name + '_ySpring',self.Ky)
+            typical_materials.defElasticMaterial(self.modelSpace.preprocessor,self.name + '_ySpring',self.Ky)
             self.springMat[1]=self.name + '_ySpring'
         if self.Kz != 0:
-            typical_materials.defElasticMaterial(modelSpace.preprocessor,self.name + '_zSpring',self.Kz)
+            typical_materials.defElasticMaterial(self.modelSpace.preprocessor,self.name + '_zSpring',self.Kz)
             self.springMat[2]=self.name + '_zSpring'
 
 
@@ -54,7 +56,7 @@ class SpringBC(object):
         '''
         prep=setEnt.getPreprocessor
         lstnod=sets_mng.get_lstNod_on_points_fromSet(setEnt)
-        return self.applyOnNodesLst(prep,lstnod)
+        return self.applyOnNodesLst(lstnod)
         
     def applyOnNodesInSet(self,setEnt):
         '''create springs in all the nodes included in the set of entities 
@@ -62,32 +64,22 @@ class SpringBC(object):
         '''
         prep=setEnt.getPreprocessor
         lstnod=sets_mng.setNod_to_lst(setEnt)
-        self.applyOnNodesLst(prep,lstnod)
+        self.applyOnNodesLst(lstnod)
         
-    def applyOnNodesIn3Dpos(self,modelSpace,lst3DPos):
+    def applyOnNodesIn3Dpos(self,lst3DPos):
         '''create springs in the nearest nodes to the coordinates in the 
         list of 3D positions given as parameter
         '''
-        lstnod=sets_mng.get_lstNod_from_lst3DPos(modelSpace.preprocessor,lst3DPos)
-        self.applyOnNodesLst(modelSpace,lstnod)
+        lstnod=sets_mng.get_lstNod_from_lst3DPos(self.modelSpace.preprocessor,lst3DPos)
+        self.applyOnNodesLst(lstnod)
         
-    def applyOnNodesLst(self,modelSpace,Nodelist):
+    def applyOnNodesLst(self,Nodelist):
         '''create spring boundary conditions in the nodes included in 
         the list of nodes passed as parameter.
-        Include the elements created in a set called by the name of the 
-        SpringBD + '_set'
         '''
-        # setsMng=modelSpace.preprocessor.getSets
-        # setName=self.name+'_set'
-        # if setsMng.exists(setName):
-        #     s=setsMng.getSet(setName)
-        # else:
-        #     s=setsMng.defSet(setName)
-        self.createSpringMaterials(modelSpace)
-        # idElem= modelSpace.preprocessor.getElementLoader.defaultTag
+        self.createSpringMaterials()
         for n in Nodelist:
-            fixedNode, elem= modelSpace.setBearing(n.tag,self.springMat)
-        #    s.getElements.append(elem)
+            fixedNode, elem= self.modelSpace.setBearing(n.tag,self.springMat)
 
 class ElasticFoundation(object):
     '''Region resting on springs (Winkler elastic foundation)
