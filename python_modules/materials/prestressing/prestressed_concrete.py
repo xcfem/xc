6100934a2f89f725e01d3fe682fd8a15e696f4a1
Ana Isabel Ortega Ortega <ana.Ortega.Ort@gmail.com>
2017-10-05 15:04:44 +0200

    Added class PrestressTendon to deal with prestressed concrete

--- python_modules/materials/prestressing/prestressed_concrete.py
+++ python_modules/materials/prestressing/prestressed_concrete.py
@@ -29,9 +29,9 @@ class PrestressTendon(object):
         self.roughCoordMtr=np.array(lstRoughPnts).transpose()
 
     def pntsInterpTendon(self,nPntsFine,smoothness,kgrade=3):
-        '''Generates a cubic spline (default) or a spline of grade kgrade interpolation from the rough points and 
-        calculates its value and the value of its derivative in nPntsFine
-        equispaced.
+        '''Generates a cubic spline (default) or a spline of grade kgrade 
+        interpolation from the rough points and calculates its value and 
+        the value of its derivative in nPntsFine equispaced.
         '''
         tck, u = interpolate.splprep(self.roughCoordMtr, k=kgrade,s=smoothness)
         x_knots, y_knots,z_knots = interpolate.splev(np.linspace(0, 1, nPntsFine), tck,der=0)
@@ -41,25 +41,56 @@ class PrestressTendon(object):
         return
 
     def getLengthSequence(self):
-        '''Return for each point in fineCoordMtr the distance to the preceding point
+        '''Return for each point in fineCoordMtr the distance to the preceding 
+        point
         '''
         lseq=[0]+[distance.euclidean((self.fineCoordMtr[0][i],self.fineCoordMtr[1][i],self.fineCoordMtr[2][i]),(self.fineCoordMtr[0][i+1],self.fineCoordMtr[1][i+1],self.fineCoordMtr[2][i+1])) for i in range(len(self.fineCoordMtr[0])-1)]
         return lseq
 
     def getCumLength(self):
-        '''Return for each point in fineCoordMtr the cumulative lenght of tendon from its 
-        starting point
+        '''Return for each point in fineCoordMtr the cumulative lenght of 
+        tendon from its starting point
         '''
-        length_sequence=getLengthSequence()
+        length_sequence=self.getLengthSequence()
         return np.cumsum(length_sequence)
 
 
     def getReverseCumLength(self):
-        '''Return for each point in fineCoordMtr the cumulative lenght of tendon from its 
-        ending point
+        '''Return for each point in fineCoordMtr the cumulative lenght of 
+        tendon from its ending point
         '''
-        lseq_forw=getLengthSequence().pop(0)+[0]
-        lcum_forw=np.cumsum(lseq_forw.reverse).reverse
-        return lcum_forw
+        lseq=self.getLengthSequence()
+        lseq.append(lseq.pop(0))
+        lseq.reverse()
+        lcum_back=np.cumsum(lseq)
+        return np.flipud(lcum_back)
 
+    def getAngleSequence(self):
+        '''Return for each point in fineCoordMtr the deviation (angle in rad) 
+        with respect to the preceding point
+        '''
+        aseq=[0]+ [angle_between((self.fineDerivMtr[0][i], self.fineDerivMtr[1][i], self.fineDerivMtr[2][i]),(self.fineDerivMtr[0][i+1], self.fineDerivMtr[1][i+1], self.fineDerivMtr[2][i+1])) for i in range(len(self.fineDerivMtr[0])-1)]
+        return aseq
+        
+    def getCumAngle(self):
+        '''Return for each point in fineCoordMtr the cumulative deviation 
+        (angle in rad) of the tendon from its starting point
+        '''
+        angl_sequence=self.getAngleSequence()
+        return np.cumsum(angl_sequence)
 
+    def getReverseCumAngle(self):
+        '''Return for each point in fineCoordMtr the cumulative deviation 
+        (angle in rad) of the tendon from its ending point
+        '''
+        aseq=self.getAngleSequence()
+        aseq.append(aseq.pop(0))
+        aseq.reverse()
+        acum_back=np.cumsum(aseq)
+        return np.flipud(acum_back)
+
+def angle_between(a,b):
+  arccosInput = np.dot(a,b)/np.linalg.norm(a)/np.linalg.norm(b)
+  arccosInput = 1.0 if arccosInput > 1.0 else arccosInput
+  arccosInput = -1.0 if arccosInput < -1.0 else arccosInput
+  return math.acos(arccosInput)
