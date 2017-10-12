b9c9b961208f2791ceaba28c972c353673c3d88f
Luis C. Pérez Tato <l.pereztato@gmail.com>
2017-10-12 08:04:23 +0200

    Solved case problem in element type inquiry.

--- python_modules/actions/loads.py
+++ python_modules/actions/loads.py
@@ -46,12 +46,12 @@ class InertialLoad(BaseVectorLoad):
 
     def appendLoadToCurrentLoadPattern(self):
         for ms in self.lstMeshSets:
-            if 'shell' in ms.elemType:
+            if 'shell' in ms.elemType.lower():
                 loadVector=ms.matSect.getAreaDensity()*self.loadVector
-                el_group=ms.primitiveSet.getElements
-            elif 'beam' in ms.elemType:
+                el_group= ms.primitiveSet.getElements
+            elif 'beam' in ms.elemType.lower():
                 loadVector=ms.matSect.getLongitudinalDensity()*self.loadVector
-                el_group=ms.primitiveSet.getElements
+                el_group= ms.primitiveSet.getElements
             for e in el_group:
                 e.vector3dUniformLoadGlobal(loadVector)
 
