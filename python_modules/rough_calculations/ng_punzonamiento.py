ca0d4095c7aa6cc2365704848c1f3c00be0c2142
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-08-07 19:07:46 +0200

    English translation.

--- python_modules/rough_calculations/ng_punzonamiento.py
+++ python_modules/rough_calculations/ng_punzonamiento.py
@@ -4,41 +4,47 @@ import sys
 import math
 
 def esfuerzoPunzonamiento(qk,A):
-    #Estimación del esfuerzo de punzonamiento en la losa sobre
-    #un pilar (HL.3 números gordos)
-    #Datos:
-    #qk: carga uniforme total característica sobre la losa o forjado
-    #A: área de influencia del pilar
+    '''Estimación del esfuerzo de punzonamiento en la losa sobre
+       un pilar (HL.3 números gordos)
+    
+       :param qk: carga uniforme total característica sobre la losa o forjado
+       :param A: área de influencia del pilar.
+    '''
     return 1.6*qk*A
 
 def punzMaximo(fck,d,a,b):
-    #Estimate of the strut strength in the section at the intersection
-    #of the support contour with the deck (HL.3 números gordos)
-    #(Interpreto que este es el punzonamiento máximo si no vamos a disponer
-    #reinforcement de punzonamiento)
-    #Si el esfuerzo de punzonamiento es mayor habrá que:
-    #  Aumentar la escuadría del pilar (lo más barato)
-    #  Aumentar el depth de la losa (lo más efectivo)
-    #  Mejorar la resistencia del hormigón
-    #Datos:
-    #fck: resistencia característica del hormigón (N/m2)
-    #d: effective depth of the floor deck (m)
-    #a,b: escuadría del pilar (m)
-    #El resultado se expresa en N
+    ''' Estimate of the strut strength in the section at the intersection
+        of the support contour with the deck (HL.3 números gordos)
+        (Interpreto que este es el punzonamiento máximo si no vamos a disponer
+        reinforcement de punzonamiento)
+        Si el esfuerzo de punzonamiento es mayor habrá que:
+          - Aumentar la escuadría del pilar (lo más barato)
+          - Aumentar el depth de la losa (lo más efectivo)
+          - Mejorar la resistencia del hormigón
+   
+        :param fck: characteristic strength of concrete (N/m2)
+        :param d: effective depth of the floor deck (m)
+        :param a,b: dimensions of the column (m)
+
+        Result is expressed in N
+    '''
     fcd=fck*10/1.5/1e6  #resistencia de cálculo del hormigón (kp/cm2)
     return 1.5*math.sqrt(fcd)*2*d*(a+b)*100e3
 
 def reinforcementPunz(Vd,fck,d,a,b,h,fyd):
-    #Estimación de la reinforcement necesaria por punzonamiento
-    #comprobación en la superficie crítica de punzonamiento 
-    #situada a d/2 del perímetro del pilar (HL.3 números gordos)
-    #Vd: esfuerzo de cálculo de punzomiento (N)
-    #fck: resistencia característica del hormigón (N/m2)
-    #fyd: design value of reinforcement steel yield strength (Pa).
-    #d: effective depth of the floor deck (m)
-    #h: depth del forjado (m)
-    #a,b: escuadría del pilar (m)
-    #El resultado de la reinforcement se expresa en m2/m
+    ''' Estimate the punching reinforcement area
+        computed at the critical perimeter defined to occur at 
+        d/2 from the column faces (HL.3 números gordos)
+
+        :param Vd: Desing value of the punching shear (N)
+        :param fck: characteristic strength of concrete (N/m2)
+        :param fyd: design value of reinforcement steel yield strength (Pa).
+        :param d: effective depth of the floor deck (m)
+        :param h: slab depth (m).
+        :param a,b: dimensions of the column (m)
+
+    Result is expressed in m2/m
+    '''
     fcd=fck*10/1.5/1e6   #resistencia de cálculo del hormigón (kp/cm2)
     Vcu=math.sqrt(fcd)*2*d*(a+b+2*d)*100e3 #resistencia del hormigón a punzonamiento (kN)
     if Vd < Vcu:
