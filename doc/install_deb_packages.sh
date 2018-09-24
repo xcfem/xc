7f5e8a1c4f7d5aa7a7e5a140d1d0a39c9cd6a13f
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-09-24 14:38:02 +0200

    Migration from VTK 5 to VTK 6.

--- doc/install_deb_packages.sh
+++ doc/install_deb_packages.sh
@@ -14,11 +14,12 @@ if [ $REPLY != "y" ]; then
 	exit 1
 fi
 
-apt-get install cmake cimg-dev g++ gfortran gnuplot libarpack2-dev libarpack++2-dev libcgal-dev libdb-dev libf2c2-dev libglib2.0-dev libgmp3-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libgts-bin libgts-dev liblapack-dev libmpfr-dev libmysql++-dev libparmetis-dev libplot-dev libsqlite3-dev libsuperlu3-dev libsuitesparse-dev libvtk5-dev libX11-dev petsc-dev tcl-dev python-vtk python-scipy python-sympy python-matplotlib python-pandas python-sklearn
+apt-get install cmake cimg-dev g++ gfortran gnuplot libarpack2-dev libarpack++2-dev libcgal-dev libdb-dev libf2c2-dev libglib2.0-dev libgmp3-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libgts-bin libgts-dev liblapack-dev libmpfr-dev libmysql++-dev libparmetis-dev libplot-dev libsqlite3-dev libsuperlu3-dev libsuitesparse-dev libvtk6-dev libX11-dev petsc-dev tcl-dev python-vtk python-scipy python-sympy python-matplotlib python-pandas python-sklearn
 
 
 #mayavi installation. Some 'mayavi' packages seems
 #to require VTK 6 so we use pip. If you're a Debian user
 #you can help us with this sending us your comments.
+# (to reconsider because we already use VTK 6 LCPT 24/09/2018)
 
 sudo -H pip install mayavi
