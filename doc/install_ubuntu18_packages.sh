53dfc770a41d03b53927c0f8ed7c873ba9449d52
Bernd Hahnebach <bernd@bimstatik.org>
2018-10-18 07:51:14 +0200

    install: update ubuntu packages, non-free not needed on ubuntu

--- doc/install_ubuntu18_packages.sh
+++ doc/install_ubuntu18_packages.sh
@@ -5,7 +5,6 @@ if [ $(whoami) != "root" ]; then
 	exit 1
 fi
 
-echo "Algunos paquetes se encuentran en las áreas «contrib» y «non-free» de la distribución Debian por lo que deben incluirse estas áreas en el archivo «sources.list antes de ejecutar este guión."
 
 # Verificar que el usuario desea continuar
 read -p "Continuar (s/n)?" REPLY
@@ -14,9 +13,75 @@ if [ $REPLY != "s" ]; then
 	exit 1
 fi
 
-#matplotlib added thanks to a comment from Guillem González 
-apt-get install cmake cimg-dev g++ gfortran gnuplot libarpack2-dev libarpack++2-dev libcgal-dev libdb-dev libf2c2-dev libglib2.0-dev libgmp3-dev libgtk2.0-dev libgtkgl2.0-dev libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libgts-bin libgts-dev liblapack-dev libmpfr-dev libmysql++-dev libmetis-dev libparmetis-dev libplot-dev libsqlite3-dev libsuperlu-dev libsuitesparse-dev libvtk6-dev petsc-dev tcl-dev python-vtk python-scipy python-sympy python-matplotlib python-pandas python-sklearn python-pip
 
-#mayavi installation. Ubuntu 'mayavi' package seems to require VTK 6 so we use pip (to reconsider because we already use VTK 6 LCPT 24/09/2018)
+# packages installed by package manager apt-get
+# tested on Ubuntu 18.04 Bionic Beaver
+packages_build="\
+    git         \
+    cmake       \
+    g++         \
+    gfortran    \
+    libboost-all-dev"
+sudo apt-get install -y $packages_build
 
+packages_lib="\
+    libarpack2-dev              \
+    libarpack++2-dev            \
+    libcgal-dev                 \
+    libdb-dev                   \
+    libf2c2-dev                 \
+    libglib2.0-dev              \
+    libgmp3-dev                 \
+    libgtk2.0-dev               \
+    libgtkgl2.0-dev             \
+    libgtkglextmm-x11-1.2-dev   \
+    libgtkmm-2.4-dev            \
+    libgts-bin                  \
+    libgts-dev                  \
+    liblapack-dev               \
+    libmpfr-dev                 \
+    libmysql++-dev              \
+    libplot-dev                 \
+    libsqlite3-dev              \
+    libsuperlu-dev              \
+    libsuitesparse-dev          \
+    libvtk6-dev                 \
+    libx11-dev                  \
+    libmetis-dev"
+sudo apt-get install -y $packages_lib
+
+packages_dev="\
+    cimg-dev  \
+    petsc-dev \
+    tcl-dev"
+sudo apt-get install -y $packages_dev
+
+packages_python="\
+    python-vtk6         \
+    python-scipy        \
+    python-sympy        \
+    python-matplotlib   \
+    python-pandas       \
+    python-sklearn      \
+    python-pip"
+sudo apt-get install -y $packages_python
+
+packages_div="\
+    gnuplot \
+    bc"
+# bc is needed by xc_utils verification tests
+sudo apt-get install -y $packages_div
+
+# install nonfree packages (ubuntu does not know nonfree, means we gone just install)
+packages_nonfree="\
+    libparmetis-dev"
+sudo apt-get install -y $packages_nonfree
+
+
+# free disk space by cleaning install files
+apt-get clean
+
+
+# mayavi installation. Ubuntu 'mayavi' package seems to require VTK 6,
+# so we use pip (to reconsider because we already use VTK 6 LCPT 24/09/2018)
 sudo -H pip install mayavi
