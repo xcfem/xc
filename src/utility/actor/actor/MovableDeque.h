1a89ea2b7dd5d6ecd2a1c2ca3813363fcbc0fd3e
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-11-25 16:01:06 +0100

    Added send/receive data method to FiberPtrDeque container.

---
+++ src/utility/actor/actor/MovableDeque.h
@@ -0,0 +1,149 @@
+//----------------------------------------------------------------------------
+//  XC program; finite element analysis code
+//  for structural analysis and design.
+//
+//  Copyright (C)  Luis Claudio Pérez Tato
+//
+//  This program derives from OpenSees <http://opensees.berkeley.edu>
+//  developed by the  «Pacific earthquake engineering research center».
+//
+//  Except for the restrictions that may arise from the copyright
+//  of the original program (see copyright_opensees.txt)
+//  XC is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or 
+//  (at your option) any later version.
+//
+//  This software is distributed in the hope that it will be useful, but 
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details. 
+//
+//
+// You should have received a copy of the GNU General Public License 
+// along with this program.
+// If not, see <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------
+//MovableDeque
+                                                                        
+                                                                        
+#ifndef MovableDeque_h
+#define MovableDeque_h
+
+#include "MovablePointerContainer.h"
+#include "MovableID.h"
+#include <deque>
+#include "utility/tagged/TaggedObject.h"
+#include "CommParameters.h"
+
+namespace XC {
+
+//! @ingroup IPComm
+//
+//! @brief Template class for maps that can move between processes.
+template <class T>
+class MovableDeque: public MovablePointerContainer<T>
+  {
+  protected:
+    typedef std::deque<T *> deque_objects;
+    typedef typename deque_objects::const_iterator const_iterator;
+    typedef typename deque_objects::iterator iterator;
+    deque_objects objects;
+
+    int sendData(CommParameters &);
+    int recvData(const CommParameters &);
+  public:
+    MovableDeque(const deque_objects &deque,T *(FEM_ObjectBroker::*pF)(int));
+    const deque_objects &getDeque(void) const
+      { return objects; }
+
+   };
+
+
+//! @brief Constructor.
+template <class T>
+XC::MovableDeque<T>::MovableDeque(const deque_objects &deque,T *(FEM_ObjectBroker::*pF)(int))
+  : MovablePointerContainer<T>(0,pF), objects(deque) {}
+
+//! @brief Send members through the channel being passed as parameter.
+template <class T>
+int XC::MovableDeque<T>::sendData(CommParameters &cp)
+  {
+    const size_t sz= objects.size();
+    this->setDbTagDataPos(0,sz);
+    int res= 0;
+    if(sz>0)
+      {
+	DbTagData dbTags(sz);
+	ID classTags(sz);
+	for(size_t i= 0;i<sz;i++)
+	  {
+	    T *obj= objects[i];
+	    if(obj)
+	      {
+	        classTags(i)= obj->getClassTag();
+	        res+= cp.sendMovable(*obj,dbTags,CommMetaData(i));
+	      }
+	    else
+	      std::cerr << "MovableDeque::sendData; found null"
+		        << " pointer in position: " << i << std::endl;
+	  }
+	DbTagData &dbTagData= this->getDbTagData();
+        res+= dbTags.send(dbTagData,cp,CommMetaData(1));
+        res+= cp.sendID(classTags,dbTagData,CommMetaData(2));
+      }
+    return res;
+  }
+
+//! @brief Receives members through the channel being passed as parameter.
+template <class T>
+int MovableDeque<T>::recvData(const CommParameters &cp)
+  {
+    const size_t sz= this->getDbTagDataPos(0);
+    int res= 0;
+    if(sz>0)
+      {
+	DbTagData &dbTagData= this->getDbTagData();
+        DbTagData dbTags(sz);
+        res+= dbTags.receive(dbTagData,cp,CommMetaData(1));
+        ID classTags(sz);
+        res+= cp.receiveID(classTags,dbTagData,CommMetaData(2));
+        T *tmp= nullptr;
+	for(size_t i= 0;i<sz;i++)
+	  {
+            const int dbTag= dbTags.getDbTagDataPos(i);
+            tmp= this->getBrokedObject(dbTag,classTags(i),cp);
+            if(tmp)
+              {
+		 res+= tmp->recvSelf(cp);
+                 objects[i]= tmp;
+              }
+            else
+	      std::cerr << "Error in MovableDeque::recvData i= " 
+                        << i << std::endl;
+	  }
+      }
+    return res;
+  }
+
+
+template <class T>
+int sendDeque(const std::deque<T *> &m,CommParameters &cp,DbTagData &dt,const CommMetaData &meta)
+  {
+    MovableDeque<T> mm(m,nullptr);
+    return cp.sendMovable(mm,dt,meta);
+  }
+
+template <class T>
+int receiveDeque(std::deque<T *> &v,const CommParameters &cp,DbTagData &dt,const CommMetaData &meta,T *(FEM_ObjectBroker::*ptrFunc)(int))
+  {
+    MovableDeque<T> mm(v,ptrFunc);
+    int res= cp.receiveMovable(mm,dt,meta);
+    v= mm.getDeque();
+    return res;
+  } 
+
+} // end of XC namespace
+
+#endif
+
---
+++ src/utility/actor/actor/MovablePointerContainer.h
@@ -0,0 +1,108 @@
+//----------------------------------------------------------------------------
+//  XC program; finite element analysis code
+//  for structural analysis and design.
+//
+//  Copyright (C)  Luis Claudio Pérez Tato
+//
+//  This program derives from OpenSees <http://opensees.berkeley.edu>
+//  developed by the  «Pacific earthquake engineering research center».
+//
+//  Except for the restrictions that may arise from the copyright
+//  of the original program (see copyright_opensees.txt)
+//  XC is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or 
+//  (at your option) any later version.
+//
+//  This software is distributed in the hope that it will be useful, but 
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details. 
+//
+//
+// You should have received a copy of the GNU General Public License 
+// along with this program.
+// If not, see <http://www.gnu.org/licenses/>.
+//----------------------------------------------------------------------------
+//MovablePointerContainer
+                                                                        
+                                                                        
+#ifndef MovablePointerContainer_h
+#define MovablePointerContainer_h
+
+#include "MovableObject.h"
+#include "CommParameters.h"
+
+namespace XC {
+
+//! @ingroup IPComm
+//
+//! @brief Template class for maps that can move between processes.
+template <class T>
+class MovablePointerContainer: public MovableObject
+  {
+  protected:
+    T *(FEM_ObjectBroker::*ptrFunc)(int);
+
+    DbTagData &getDbTagData(void) const;
+    T *getBrokedObject(const int &,const int &,const CommParameters &);
+    virtual int sendData(CommParameters &)= 0;
+    virtual int recvData(const CommParameters &)= 0;
+  public:
+    MovablePointerContainer(const int &, T *(FEM_ObjectBroker::*pF)(int));
+    int sendSelf(CommParameters &);
+    int recvSelf(const CommParameters &);
+  };
+
+
+//! @brief Constructor.
+template <class T>
+XC::MovablePointerContainer<T>::MovablePointerContainer(const int &classTag,T *(FEM_ObjectBroker::*pF)(int))
+  :MovableObject(classTag), ptrFunc(pF) {}
+
+//! @brief Returns a vector to store the dbTags
+//! of the class members.
+template <class T>
+XC::DbTagData &XC::MovablePointerContainer<T>::getDbTagData(void) const
+  {
+    static DbTagData retval(5);
+    return retval;
+  }
+
+//! @brief Returns an empty object of the class identified by classTag.
+template <class T>
+T *XC::MovablePointerContainer<T>::getBrokedObject(const int &dbTag,const int &classTag,const CommParameters &cp)
+  { return getBrokedMovable(dbTag,classTag,cp,this->ptrFunc); }
+ 
+//! @brief Send the object through the channel being passed as parameter.
+template <class T>
+int XC::MovablePointerContainer<T>::sendSelf(CommParameters &cp)
+  {
+    inicComm(5);
+    int result= this->sendData(cp);
+
+    const int dataTag= getDbTag(cp);
+    result+= cp.sendIdData(getDbTagData(),dataTag);
+    if(result < 0)
+      std::cerr << "MovablePointerContainer::sendSelf() - failed to send data\n";
+    return result;
+  }
+
+//! @brief Receives object through the channel being passed as parameter.
+template <class T>
+int MovablePointerContainer<T>::recvSelf(const CommParameters &cp)
+  {
+    inicComm(5);
+    const int dataTag = getDbTag();  
+    int result= cp.receiveIdData(getDbTagData(),dataTag);
+    if(result<0)
+      std::cerr << "MovablePointerContainer::recvSelf() - failed to receive data\n";
+    else
+      result+= recvData(cp);
+    return result;
+  }
+
+} // end of XC namespace
+
+#endif
+
