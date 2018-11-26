9ea0cb145e2b2ee9c79945d357e7c314d55bbe97
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-11-26 15:31:33 +0100

    Code cosmetics.

--- verif/tests/elements/shell/test_shell_mitc4_01.py
+++ verif/tests/elements/shell/test_shell_mitc4_01.py
@@ -28,17 +28,17 @@ nodes= preprocessor.getNodeHandler
 
 # Problem type
 modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
-nodes.newNodeIDXYZ(1,0,0,0)
-nodes.newNodeIDXYZ(2,2,0,0)
-nodes.newNodeIDXYZ(3,2,1,1)
-nodes.newNodeIDXYZ(4,0,1,1)
+n1= nodes.newNodeXYZ(0,0,0)
+n2= nodes.newNodeXYZ(2,0,0)
+n3= nodes.newNodeXYZ(2,1,1)
+n4= nodes.newNodeXYZ(0,1,1)
 
 # Materials definition
 memb1= typical_materials.defElasticMembranePlateSection(preprocessor, "memb1",E,nu,dens,h)
 
 elements= preprocessor.getElementHandler
 elements.defaultMaterial= "memb1"
-elem= elements.newElement("ShellMITC4",xc.ID([1,2,3,4]))
+elem= elements.newElement("ShellMITC4",xc.ID([n1.tag,n2.tag,n3.tag,n4.tag]))
 
 
 ratio1= (elem.getCoordTransf.getG2Vector-v1).Norm()
