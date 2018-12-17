33db16514c7904aa4269522bf5a5f6b9de2f581f
Luis C. Pérez Tato <l.pereztato@gmail.com>
2018-12-17 20:12:35 +0100

    Deprecated file.

--- python_modules/postprocess/xcVtk/FE_model/vtk_load_elem.py
+++
@@ -1,7 +0,0 @@
-def VtkLoadElementos(setName):
-  eSet= preprocessor.getSets.getSet(setName)
-  elems= eSet.getElements
-  print "nombre set: ",eSet.nombre
-  print "num. elementos: ",eSet.getNumElementos
-  for e in elems:
-    ugrid.InsertNextCell(e.getVtkCellType,e.getVertices)
