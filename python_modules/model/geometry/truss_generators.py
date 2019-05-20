f88ffb6e1dcdd056364b53f6c3164776490f3699
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-05-20 16:07:15 +0200

    Add fan truss web style.

--- python_modules/model/geometry/truss_generators.py
+++ python_modules/model/geometry/truss_generators.py
@@ -43,41 +43,7 @@ class TrussBase(object):
         self.vertDirectionVector= vertDirectionVector
         self.numberOfModules= round(self.span/self.moduleWidth,0)
         self.moduleWidth= self.span/self.numberOfModules
-
-# Ascii art:
-#     +---+-------+-------+---+  Upper chord.
-#     |  / \     / \     / \  |
-#     | /   \   /   \   /   \ |
-#     |/     \ /     \ /     \|
-#     +-------+-------+-------+  Lower chord.
-
-class WarrenTruss(TrussBase):
-    '''Warren truss.
-
-    :ivar lowerChordJointsPos: (list) truss lower chord joint positions.
-    :ivar upperChordJointsPos: (list) truss upper chord joint positions
-    :ivar lowerChordPoints: (list) truss lower chord key points.
-    :ivar upperChordPoints: (list) truss upper chord key points.
-    :ivar lowerChordLines: (list) truss lower chord lines.
-    :ivar upperChordLines: (list) truss upper chord lines.
-    :ivar diagonalLines: (list) truss diagonals.
-    :ivar endPosts: (list) vertical lines at the ends of the truss.
-    :ivar lowerChordMaterial: material for the lower chord elements.
-    :ivar upperChordMaterial: material for the upper chord elements.
-    :ivar diagonalLines: material for the diagonals.
-    :ivar endPosts: material for the posts.
-    '''
-    def __init__(self,trussRise,trussModule, trussSpan, horizDirectionVector= geom.Vector3d(1.0,0.0,0.0), vertDirectionVector= geom.Vector3d(0,0.0,1.0)):
-        '''Constructor.
-
-            Args:
-                :param trussRise: (float) rise of the truss.
-                :param trussModule: (float) width of each truss.
-                :param trussSpan: (float) span of each truss.
-                :param horizDirectionVector: direction of the chords.
-                :param vertDirectionVector: direction of the vertical studs.
-        '''
-        super(WarrenTruss,self).__init__(trussRise,trussModule, trussSpan, horizDirectionVector, vertDirectionVector)
+        
         self.lowerChordJointsPos= list()
         self.upperChordJointsPos= list()
         self.lowerChordPoints= list()
@@ -85,28 +51,14 @@ class WarrenTruss(TrussBase):
         self.lowerChordLines= list()
         self.upperChordLines= list()
         self.diagonalLines= list()
-        self.endPosts= list()
+        self.posts= list()
         self.crdTransf= None
         self.lowerChordMaterial= None
         self.upperChordMaterial= None
         self.diagonalMaterial= None
-        self.endPostsMaterial= None
+        self.diagonalArea= None
+        self.postsMaterial= None
 
-    def computeJointPositions(self):
-        '''Compute the positions of the truss joints.'''
-        lowerChordX= list()
-        for i in range(0,int(self.numberOfModules+1)):
-            lowerChordX.append(i)
-        upperChordX= [0.0]
-        for i in range(0,int(self.numberOfModules)):
-            upperChordX.append(i+0.5)
-        upperChordX.append(self.numberOfModules)
-
-        for x in lowerChordX:
-            self.lowerChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector)
-        for x in upperChordX:
-            self.upperChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector+self.rise*self.vertDirectionVector)
-    
     def createKeyPoints(self, feProblem, org= geom.Pos3d(0,0,0)):
         '''Create the key points for the Truss.'''
         self.computeJointPositions()
@@ -116,22 +68,43 @@ class WarrenTruss(TrussBase):
             self.lowerChordPoints.append(kPoints.newPntFromPos3d(org+j))
         for j in self.upperChordJointsPos:
             self.upperChordPoints.append(kPoints.newPntFromPos3d(org+j))
-
-    def createGeometry(self, feProblem, org= geom.Pos3d(0,0,0)):
-        '''Creates the geometry of the Truss.'''
-        self.createKeyPoints(feProblem, org)
+            
+    def createChordsGeometry(self):
+        '''Creates the geometry of the truss chords.'''
         lines= self.preprocessor.getMultiBlockTopology.getLines # Line container.
         for p in list(zip(*(self.lowerChordPoints[start:] for start in range(0, 2)))):
             self.lowerChordLines.append(lines.newLine(p[0].tag, p[1].tag))
         for p in list(zip(*(self.upperChordPoints[start:] for start in range(0, 2)))):
             self.upperChordLines.append(lines.newLine(p[0].tag, p[1].tag))
-        for i in range(0,len(self.lowerChordPoints)-1):
-            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i].tag,self.upperChordPoints[i+1].tag))
-            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i+1].tag,self.upperChordPoints[i+1].tag))
-        self.endPosts.append(lines.newLine(self.lowerChordPoints[0].tag,self.upperChordPoints[0].tag))
-        self.endPosts.append(lines.newLine(self.lowerChordPoints[-1].tag,self.upperChordPoints[-1].tag))
+
+    def createGeometry(self, feProblem, org= geom.Pos3d(0,0,0)):
+        '''Creates the geometry of the Truss.'''
+        self.createKeyPoints(feProblem, org)
+        self.createChordsGeometry()
+        self.createDiagonalsGeometry()
+        self.createPostsGeometry()
+
+    def createSets(self):
+        '''Defines the sets for the truss.'''
+        sets= self.preprocessor.getSets
+        self.trussSet= sets.defSet(self.name+'_set')
+        self.lowerChordSet= sets.defSet(self.name+'_lowerChordSet')
+        for l in self.lowerChordLines:
+            self.lowerChordSet.getLines.append(l)
+            self.trussSet.getLines.append(l)
+        self.upperChordSet= sets.defSet(self.name+'_upperChordSet')
+        for l in self.upperChordLines:
+            self.upperChordSet.getLines.append(l)
+            self.trussSet.getLines.append(l)
+        self.diagonalSet= sets.defSet(self.name+'_diagonalSet')
         for l in self.diagonalLines:
-            l.nDiv= 1
+            self.diagonalSet.getLines.append(l)
+            self.trussSet.getLines.append(l)
+        self.postsSet= sets.defSet(self.name+'_postsSet')
+        for l in self.posts:
+            self.postsSet.getLines.append(l)
+            self.trussSet.getLines.append(l)
+        self.fillDownwards()
 
     def getNormalVector(self):
         '''Get a vector normal to the truss plane.'''
@@ -161,33 +134,12 @@ class WarrenTruss(TrussBase):
         self.lowerChordSet.fillDownwards()
         self.upperChordSet.fillDownwards()
         self.diagonalSet.fillDownwards()
-        self.endPostsSet.fillDownwards()
+        self.postsSet.fillDownwards()
         self.trussSet.fillDownwards()
         
-    def createSets(self):
-        '''Defines the sets for the truss.'''
-        sets= self.preprocessor.getSets
-        self.trussSet= sets.defSet(self.name+'_set')
-        self.lowerChordSet= sets.defSet(self.name+'_lowerChordSet')
-        for l in self.lowerChordLines:
-            self.lowerChordSet.getLines.append(l)
-            self.trussSet.getLines.append(l)
-        self.upperChordSet= sets.defSet(self.name+'_upperChordSet')
-        for l in self.upperChordLines:
-            self.upperChordSet.getLines.append(l)
-            self.trussSet.getLines.append(l)
-        self.diagonalSet= sets.defSet(self.name+'_diagonalSet')
-        for l in self.diagonalLines:
-            self.diagonalSet.getLines.append(l)
-            self.trussSet.getLines.append(l)
-        self.endPostsSet= sets.defSet(self.name+'_endPostsSet')
-        for l in self.endPosts:
-            self.endPostsSet.getLines.append(l)
-            self.trussSet.getLines.append(l)
-        self.fillDownwards()
-
-    def genMesh(self):
+    def genMesh(self, feProblem):
         ''' Creates the elements.'''
+        self.createGeometry(feProblem)
         self.createCoordTransformation()
         self.createSets()
         seedElemHandler= self.preprocessor.getElementHandler.seedElemHandler
@@ -204,12 +156,137 @@ class WarrenTruss(TrussBase):
         seedElemHandler.defaultMaterial= self.diagonalMaterial.name  # Material name.
         seedElemHandler.dimElem= 3 #Bars defined ina a three-dimensional space.
         trussElem= seedElemHandler.newElement("Truss",xc.ID([0,0]));
+        trussElem.area= self.diagonalArea
         self.diagonalSet.genMesh(xc.meshDir.I)  # Generate the elements.
         # End posts
-        seedElemHandler.defaultMaterial= self.endPostsMaterial.name  # Material name.
+        seedElemHandler.defaultMaterial= self.postsMaterial.name  # Material name.
         beam3d= seedElemHandler.newElement("ElasticBeam3d",xc.ID([0,0]));
-        self.endPostsSet.genMesh(xc.meshDir.I)  # Generate the elements.
+        self.postsSet.genMesh(xc.meshDir.I)  # Generate the elements.
+        self.fillDownwards()
+
+# Warren truss web style ascii art:
+#     +---+-------+-------+---+  Upper chord.
+#     |  / \     / \     / \  |
+#     | /   \   /   \   /   \ |
+#     |/     \ /     \ /     \|
+#     +-------+-------+-------+  Lower chord.
+
+class WarrenTruss(TrussBase):
+    '''Warren truss.
+
+    :ivar lowerChordJointsPos: (list) truss lower chord joint positions.
+    :ivar upperChordJointsPos: (list) truss upper chord joint positions
+    :ivar lowerChordPoints: (list) truss lower chord key points.
+    :ivar upperChordPoints: (list) truss upper chord key points.
+    :ivar lowerChordLines: (list) truss lower chord lines.
+    :ivar upperChordLines: (list) truss upper chord lines.
+    :ivar diagonalLines: (list) truss diagonals.
+    :ivar posts: (list) vertical lines at the ends of the truss.
+    :ivar lowerChordMaterial: material for the lower chord elements.
+    :ivar upperChordMaterial: material for the upper chord elements.
+    :ivar diagonalLines: material for the diagonals.
+    :ivar diagonalArea: area of the diagonals cross-section.
+    :ivar postsMaterial: material for the posts.
+    '''
+    def __init__(self,trussRise,trussModule, trussSpan, horizDirectionVector= geom.Vector3d(1.0,0.0,0.0), vertDirectionVector= geom.Vector3d(0,0.0,1.0)):
+        '''Constructor.
+
+            Args:
+                :param trussRise: (float) rise of the truss.
+                :param trussModule: (float) width of each truss.
+                :param trussSpan: (float) span of each truss.
+                :param horizDirectionVector: direction of the chords.
+                :param vertDirectionVector: direction of the vertical studs.
+        '''
+        super(WarrenTruss,self).__init__(trussRise,trussModule, trussSpan, horizDirectionVector, vertDirectionVector)
+
+    def computeJointPositions(self):
+        '''Compute the positions of the truss joints.'''
+        lowerChordX= list()
+        for i in range(0,int(self.numberOfModules+1)):
+            lowerChordX.append(i)
+        upperChordX= [0.0]
+        for i in range(0,int(self.numberOfModules)):
+            upperChordX.append(i+0.5)
+        upperChordX.append(self.numberOfModules)
+
+        for x in lowerChordX:
+            self.lowerChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector)
+        for x in upperChordX:
+            self.upperChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector+self.rise*self.vertDirectionVector)
+    
+    def createDiagonalsGeometry(self):
+        '''Creates the geometry of the truss diagonals.'''
+        lines= self.preprocessor.getMultiBlockTopology.getLines # Line container.
+        for i in range(0,len(self.lowerChordPoints)-1):
+            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i].tag,self.upperChordPoints[i+1].tag))
+            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i+1].tag,self.upperChordPoints[i+1].tag))
+        for l in self.diagonalLines:
+            l.nDiv= 1
+
+    def createPostsGeometry(self):
+        '''Creates the geometry of the truss posts.'''
+        lines= self.preprocessor.getMultiBlockTopology.getLines # Line container.
+        self.posts.append(lines.newLine(self.lowerChordPoints[0].tag,self.upperChordPoints[0].tag))
+        self.posts.append(lines.newLine(self.lowerChordPoints[-1].tag,self.upperChordPoints[-1].tag))
+            
+
+        
 
 
-    
-    
+
+# Fan truss web style ascii art:
+#     +---+-------+-------+---+  Upper chord.
+#     |  / \  |  / \  |  / \  |
+#     | /   \ | /   \ | /   \ |
+#     |/     \|/     \|/     \|
+#     +-------+-------+-------+  Lower chord.
+
+class FanTruss(WarrenTruss):
+    '''Fan truss.
+
+    '''
+    def __init__(self, trussRise, trussModule, trussSpan, horizDirectionVector= geom.Vector3d(1.0,0.0,0.0), vertDirectionVector= geom.Vector3d(0,0.0,1.0)):
+        '''Constructor.
+
+            Args:
+                :param trussRise: (float) rise of the truss.
+                :param trussModule: (float) width of each truss.
+                :param trussSpan: (float) span of each truss.
+                :param horizDirectionVector: direction of the chords.
+                :param vertDirectionVector: direction of the vertical studs.
+        '''
+        super(FanTruss,self).__init__(trussRise,trussModule, trussSpan, horizDirectionVector, vertDirectionVector)
+
+    def computeJointPositions(self):
+        '''Compute the positions of the truss joints.'''
+        lowerChordX= list()
+        for i in range(0,int(self.numberOfModules+1)):
+            lowerChordX.append(i)
+        upperChordX= list()
+        for i in range(0,int(self.numberOfModules)):
+            upperChordX.append(i)
+            upperChordX.append(i+0.5)
+        upperChordX.append(self.numberOfModules)
+
+        for x in lowerChordX:
+            self.lowerChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector)
+        for x in upperChordX:
+            self.upperChordJointsPos.append(self.moduleWidth*x*self.horizDirectionVector+self.rise*self.vertDirectionVector)
+        
+    def createPostsGeometry(self):
+        '''Creates the geometry of the Truss.'''
+        super(FanTruss,self).createPostsGeometry()
+        #Add intermediate posts.
+        lines= self.preprocessor.getMultiBlockTopology.getLines # Line container
+        for i in range(1,len(self.lowerChordPoints)-1):
+            self.posts.append(lines.newLine(self.lowerChordPoints[i].tag,self.upperChordPoints[2*i].tag))
+
+    def createDiagonalsGeometry(self):
+        '''Creates the geometry of the truss diagonals.'''
+        lines= self.preprocessor.getMultiBlockTopology.getLines # Line container.
+        for i in range(0,len(self.lowerChordPoints)-1):
+            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i].tag,self.upperChordPoints[2*i+1].tag))
+            self.diagonalLines.append(lines.newLine(self.lowerChordPoints[i+1].tag,self.upperChordPoints[2*i+1].tag))
+        for l in self.diagonalLines:
+            l.nDiv= 1
