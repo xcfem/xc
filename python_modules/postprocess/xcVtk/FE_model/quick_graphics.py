3498fb5bc59c913e5c0f0c7975f04cffc0c23c19
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-05-21 18:31:32 +0200

    Update print to Python 3

--- python_modules/postprocess/xcVtk/FE_model/quick_graphics.py
+++ python_modules/postprocess/xcVtk/FE_model/quick_graphics.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
+from __future__ import print_function
 
 '''Providing the user with a quick and easy way to 
    display results (internal forces, displacements) of an user-defined
