19b079cf6a49e558b939c2af61b973dff4154599
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-05-31 10:49:35 +0200

    Add another test to assure that results are correct.

--- verif/tests/utility/rcond.py
+++ verif/tests/utility/rcond.py
@@ -12,29 +12,42 @@ __email__= "l.pereztato@gmail.com"
 import xc_base
 import geom
 import xc
+import numpy as np
 
 data= [[1,1],[1,0]]
 
 testMatrix= xc.Matrix(data)
+npMatrix= np.array(data)
+npInverse= np.linalg.inv(npMatrix)
 
 rcond= testMatrix.RCond()
 oneNorm= testMatrix.OneNorm()
+npNorm= np.linalg.norm(npMatrix,1)
 invMatrix= testMatrix.getInverse()
 invOneNorm= invMatrix.OneNorm()
+invNpMatrixNorm= np.linalg.norm(npInverse,1)
 rcondTheor= 1.0/(oneNorm*invOneNorm)
+rcondFrobenius= np.linalg.cond(npMatrix,'fro')
+rcondNumpy= 1.0/(npNorm*invNpMatrixNorm)
 ratio1= (rcond-rcondTheor)/rcondTheor
+ratio2= (rcond-rcondNumpy)/rcondNumpy
 
 # print('data= ',testMatrix)
 # print('oneNorm= ',oneNorm)
+# print('npNorm= ',npNorm)
 # print('invMatrix= ',invMatrix)
+# print('npInverse= ',npInverse)
 # print('invOneNorm= ',invOneNorm)
+# print('invNpMatrixNorm= ',invNpMatrixNorm)
 # print('rcondTheor= ', rcondTheor)
+# print('rcondFrobenius= ', rcondFrobenius)
+# print('rcondNumpy= ', rcondNumpy)
 # print('rcond= ', rcond)
 
 import os
 from miscUtils import LogMessages as lmsg
 fname= os.path.basename(__file__)
-if (abs(ratio1)<1e-5):
+if (abs(ratio1)<1e-5) and (abs(ratio2)<1e-5):
   print("test ",fname,": ok.")
 else:
   lmsg.error(fname+' ERROR.')
