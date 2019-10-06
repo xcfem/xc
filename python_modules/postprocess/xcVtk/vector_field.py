4dc18b05f15ed72460a82adfe973a3902aa1650a
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-10-06 20:08:43 +0200

    Add method to populate from pair lists

--- python_modules/postprocess/xcVtk/vector_field.py
+++ python_modules/postprocess/xcVtk/vector_field.py
@@ -17,42 +17,52 @@ from postprocess.xcVtk import field_base as fb
 from postprocess.xcVtk import vector_field_data as vfd
 
 class VectorField(fb.FieldBase):
-  '''Vector field defined at points.'''
-  def __init__(self,name,fUnitConv,scaleFactor,showPushing= True,symType=vtk.vtkArrowSource()):
-    super(VectorField,self).__init__(name,fUnitConv)
-    self.data= vfd.VectorFieldData(name,3,scaleFactor)
-    self.showPushing= showPushing #If true vector push else pulls from point
-    self.symType=symType
+    '''Vector field defined at points.'''
+    def __init__(self,name,fUnitConv,scaleFactor,showPushing= True,symType=vtk.vtkArrowSource()):
+        super(VectorField,self).__init__(name,fUnitConv)
+        self.data= vfd.VectorFieldData(name,3,scaleFactor)
+        self.showPushing= showPushing #If true vector push else pulls from point
+        self.symType=symType
 
-  def setupMapper(self):
-    self.data.setupGlyph(self.fUnitConv,self.symType)
-    self.mapper = vtk.vtkPolyDataMapper()
-    self.mapper.SetInputConnection(self.data.glyph.GetOutputPort())
-    self.mapper.SetScalarModeToUsePointFieldData()
-    self.mapper.SetColorModeToMapScalars()
-    self.mapper.ScalarVisibilityOn()
-    #self.mapper.SetScalarRange(self.glyph.GetOutputDataObject(0).GetPointData().GetArray(self.lenghtsName).GetRange())
-    self.mapper.SetScalarRange(self.data.lengths.GetRange())
-    self.mapper.SelectColorArray(self.data.lenghtsName)
-    self.creaLookUpTable()
-    self.mapper.SetLookupTable(self.lookUpTable)
-    return self.mapper
+    def setupMapper(self):
+        self.data.setupGlyph(self.fUnitConv,self.symType)
+        self.mapper = vtk.vtkPolyDataMapper()
+        self.mapper.SetInputConnection(self.data.glyph.GetOutputPort())
+        self.mapper.SetScalarModeToUsePointFieldData()
+        self.mapper.SetColorModeToMapScalars()
+        self.mapper.ScalarVisibilityOn()
+        #self.mapper.SetScalarRange(self.glyph.GetOutputDataObject(0).GetPointData().GetArray(self.lenghtsName).GetRange())
+        self.mapper.SetScalarRange(self.data.lengths.GetRange())
+        self.mapper.SelectColorArray(self.data.lenghtsName)
+        self.creaLookUpTable()
+        self.mapper.SetLookupTable(self.lookUpTable)
+        return self.mapper
 
-  def setupActor(self):
-    self.setupMapper()
-    self.actor = vtk.vtkActor()
-    self.actor.SetMapper(self.mapper)
-    return self.actor
+    def setupActor(self):
+        self.setupMapper()
+        self.actor = vtk.vtkActor()
+        self.actor.SetMapper(self.mapper)
+        return self.actor
 
-  def addToDisplay(self, recordDisplay, orientation= 'H'):
-    # Adds the vector field to the display.
-    nTuples= self.data.getNumberOfTuples()
-    if(nTuples>0):
-      self.setupActor()
-      recordDisplay.renderer.AddActor(self.actor)
-      self.creaColorScaleBar(orientation)
-      # mapper2D= vtk.vtkPolyDataMapper2D()
-      # self.scalarBar.SetMapper(mapper2D)
-      recordDisplay.renderer.AddActor2D(self.scalarBar)
-    else:
-      lmsg.warning('vector field: no vectors to display. Command ignored.')
+    def populateFromPairList(self,pairList):
+        ''' Adds vector data from the pair list
+            argument. '''
+        for pair in pairList:
+            p= pair[0]
+            v= pair[1]
+            self.data.insertNextVector(v[0],v[1],v[2])
+            self.data.insertNextPair(p[0],p[1],p[2],v[0],v[1],v[2],self.fUnitConv,self.showPushing)
+
+
+    def addToDisplay(self, recordDisplay, orientation= 'H'):
+        # Adds the vector field to the display.
+        nTuples= self.data.getNumberOfTuples()
+        if(nTuples>0):
+          self.setupActor()
+          recordDisplay.renderer.AddActor(self.actor)
+          self.creaColorScaleBar(orientation)
+          # mapper2D= vtk.vtkPolyDataMapper2D()
+          # self.scalarBar.SetMapper(mapper2D)
+          recordDisplay.renderer.AddActor2D(self.scalarBar)
+        else:
+          lmsg.warning('vector field: no vectors to display. Command ignored.')
