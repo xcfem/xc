9e9ea99fc6223071e66d67b0371dd3b5a79b8fa7
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-10-17 19:58:02 +0200

    Add method to compute cracking moment strength

--- python_modules/materials/tm5_809_3/tm5_809_3_materials.py
+++ python_modules/materials/tm5_809_3/tm5_809_3_materials.py
@@ -18,13 +18,89 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@ciccp.es, ana.ortega@ciccp.es "
 
+toPascal= ACI_materials.toPascal #Conversion from Pa to lb/inch2
+fromPascal= ACI_materials.fromPascal #Conversion from lb/inch2 to Pa
+
+class Mortar(object):
+    ''' Mortar according to TM 5-809-3.
+
+    :ivar fm: compressive strenght.
+    '''
+    def __init__(self,fm):
+        '''
+        Constructor.
+
+        :param fm: compressive strength.
+        '''
+        self.fm= fm
+    def Em(self):
+        '''Modulus of elasticity.'''
+        return 1000.0*self.fm
+    def Ev(self):
+        '''Elastic modulus.'''
+        return 400.0*self.fm
+    def fr(self):
+        '''Modulus of rupture (see TM 5-809-3 page 5-4).
+        '''
+        fmlb_inch2= abs(self.fm*fromPascal) #Pa -> lb/inch2
+        return 2.5*(math.sqrt(fmlb_inch2))*toPascal #lb/inch2 -> Pa
+
+sMortar= Mortar(1350.0*toPascal)
+
 class CMUWall(object):
     """ Masonry wall 
 
     :ivar thickness: nominal wall thickness.
     :ivar groutedCellsSpacing: spacing of grouted cells.
-    """    
-    def __init__(self,thickness, spacing):
+    """
+    # Interpolation of the equivalent thickness
+    xT= [6,8,10,12] # nominal wall thickness (inches)
+    yT= [0.0,16.0,24.0,32.0,40.0,48.0,56.0,64.0,72.0,1e6] # spacing inches
+    zT= [[5.62, 7.62, 9.62, 11.62],
+        [3.7, 4.9, 5.98, 7.04],
+        [3.13, 4.1, 4.91, 5.7],
+        [2.85, 3.7, 4.37, 5.02],
+        [2.68, 3.46, 4.05, 4.62],
+        [2.57, 3.3, 3.83, 4.35],
+        [2.49, 3.19, 3.67, 4.16],
+        [2.42, 3.1, 3.56, 4.01],
+        [2.38, 3.03, 3.47, 3.90],
+        [2.00, 2.50, 2.75, 3.00]]
+    fEquivalentThickness= scipy.interpolate.interp2d(xT,yT,zT)
+    # Interpolation of the effective area
+    xA= [6,8,10,12] # nominal wall thickness (inches)
+    yA= [0.0,16.0,24.0,32.0,40.0,48.0,56.0,64.0,72.0,1e6] # spacing inches
+    zA= [[68.0, 92.0, 116.0, 140.0],
+       [44.0, 59.0, 72.0, 85.0],
+       [38.0, 49.0, 59.0, 68.0],
+       [34.0, 44.0, 52.0, 60.0],
+       [32.0, 42.0, 48.0, 55.0],
+       [31.0, 40.0, 46.0, 52.0],
+       [30.0, 38.0, 44.0, 50.0],
+       [29.0, 37.0, 43.0, 48.0],
+       [28.0, 36.0, 42.0, 47.0],
+       [24.0, 30.0, 33.0, 36.0]]
+    fEffectiveArea= scipy.interpolate.interp2d(xA,yA,zA)
+    # Interpolation of the gross moment of inertia
+    xI= [6,8,10,12] # nominal wall thickness (inches)
+    yI= [8.0, 16.0, 24.0, 32.0, 40.0, 48.0] # effective width
+    zI= [[119.0, 296.0, 594.0, 1047.0],
+        [204.0, 496.0, 959.0, 1640.0],
+        [290.0, 702.0, 1337.0, 2260.0],
+        [377.0, 907.0, 1714.0, 2879.0],
+        [0.0, 1113.0, 2092.0, 3499.0],
+        [0.0, 1319.0, 2470.0, 4119.0]] # gross moment of inertia in**4
+    fInertia= scipy.interpolate.interp2d(xI,yI,zI)
+    xM= xI # nominal wall thickness (inches)
+    yM= yI # effective width
+    zM= [[323.0, 593.0, 946.0, 1379.0],
+        [554.0, 995.0, 1525.0, 2160.0],
+        [791.0, 1409.0, 2126.0, 2976.0],
+        [1027.0, 1822.0, 2727.0, 3792.0],
+        [0.0, 2235.0, 3328.0, 4608.0],
+        [0.0, 2648.0, 3929.0, 5424.0]]
+    fMoment=  scipy.interpolate.interp2d(xM,yM,zM)
+    def __init__(self,thickness, spacing, mortar= sMortar):
         '''
         Constructor.
 
@@ -33,23 +109,13 @@ class CMUWall(object):
         '''
         self.thickness= thickness
         self.groutedCellsSpacing= spacing
+        self.mortar= mortar
     
     def getEquivalentWallThickness(self):
         ''' Return the equivalent wall thickness according to
             table 5-2 of TM 5-809-3.'''
-        x= [6,8,10,12] # nominal wall thickness (inches)
-        y= [0.00,16.00,24.00,32.00,40.00,48.00,56.00,64.00] # spacing inches
-        z= [[5.62, 7.62, 9.62, 11.62],
-        [3.7, 4.9, 5.98, 7.04],
-        [3.13, 4.1, 4.91, 5.7],
-        [2.85, 3.7, 4.37, 5.02],
-        [2.68, 3.46, 4.05, 4.62],
-        [2.57, 3.3, 3.83, 4.35],
-        [2.49, 3.19, 3.67, 4.16],
-        [2.42, 3.1, 3.56, 4.01]]
         sp_inch= self.groutedCellsSpacing/0.0254
         th_inches= self.thickness/0.0254
-        fEquivalentThickness= scipy.interpolate.interp2d(x,y,z)
         tol= 1e-3
         if(th_inches<=6.0):
             if(th_inches<6.0-1e3):
@@ -61,5 +127,49 @@ class CMUWall(object):
                 lmsg.error('thickness: '+str(self.thickness)+' out of range (too thick).')
             else:
                 th_inches= 12.0
-        return fEquivalentThickness(th_inches,sp_inch)[0]*0.0254
+        return self.fEquivalentThickness(th_inches,sp_inch)[0]*0.0254
+    def getEffectiveArea(self):
+        ''' Return the effective area according to
+            table 5-3 of TM 5-809-3.'''
+        sp_inch= self.groutedCellsSpacing/0.0254
+        th_inches= self.thickness/0.0254
+        tol= 1e-3
+        if(th_inches<=6.0):
+            if(th_inches<6.0-1e3):
+                lmsg.error('thickness: '+str(self.thickness)+' out of range (too thin).')
+            else:
+                th_inches= 6.0
+        elif(th_inches>=12.0):
+            if(th_inches>12.0+tol):
+                lmsg.error('thickness: '+str(self.thickness)+' out of range (too thick).')
+            else:
+                th_inches= 12.0
+        return self.fEffectiveArea(th_inches,sp_inch)[0]*0.0254**2/0.3048 #in/ft->m2/m
+    def getEffectiveWidth(self):
+        '''Return the effective width of the fabric
+           according to remark 2 on table 5-4
+           of TM 5-809-3.'''
+        retval= self.groutedCellsSpacing
+        retval= min(6*self.thickness,retval)
+        return min(retval,48.0*0.0254)
+        
+    def getGrossMomentOfInertia(self):
+        '''Return the gross moment of inertia
+           according to table 5-4 of TM 5-809-3.
+        '''
+        th_inches= self.thickness/0.0254
+        b_inch= self.getEffectiveWidth()/0.0254
+        return self.fInertia(th_inches,b_inch)[0]*0.0254**4 #in**4->m**4
+
+    def getSectionModulus(self):
+        ''' Return the section modulus
+            (see TM 5-809-3 page 5-4).'''
+        return  2.0*self.getGrossMomentOfInertia()/self.thickness
+    def getCrackingMoment(self):
+        '''Return the cracking moment of the fabric
+           according to equation 5-7 of  TM 5-809-3.'''
+        th_inches= self.thickness/0.0254
+        b_inch= self.getEffectiveWidth()/0.0254
+        return self.fMoment(th_inches,b_inch)[0]/8.85 #lbf.ft-> N.m
+        
 
--- verif/tests/materials/tm5_809_3/test_equivalent_wall_thickness.py
+++ verif/tests/materials/tm5_809_3/test_equivalent_wall_thickness.py
@@ -6,6 +6,7 @@
 from __future__ import division
 from __future__ import print_function
 
+import math
 from materials.tm5_809_3 import tm5_809_3_materials
 
 __author__= "Luis C. Pérez Tato (LCPT)"
@@ -14,30 +15,67 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
+ratios=[]
 cmuWall6= tm5_809_3_materials.CMUWall(thickness= 6*0.0254,spacing= 0.0)
 eqThickness6= cmuWall6.getEquivalentWallThickness()
-cmuWall12= tm5_809_3_materials.CMUWall(thickness= 12*0.0254,spacing= 64.0*0.0254)
+effArea6= cmuWall6.getEffectiveArea()
+cmuWall12= tm5_809_3_materials.CMUWall(thickness= 12*0.0254,spacing= 72.0*0.0254)
 eqThickness12= cmuWall12.getEquivalentWallThickness()
+effArea12= cmuWall12.getEffectiveArea()
 cmuWall8= tm5_809_3_materials.CMUWall(thickness= 8*0.0254,spacing= 40*0.0254)
 eqThickness8= cmuWall8.getEquivalentWallThickness()
+effArea8= cmuWall8.getEffectiveArea()
+b12= cmuWall12.getEffectiveWidth()
+Ig12= cmuWall12.getGrossMomentOfInertia()
+fr= cmuWall12.mortar.fr()*tm5_809_3_materials.fromPascal
+fr_ref= 2.5*math.sqrt(1350)
+Mcr12= cmuWall12.getCrackingMoment()
+Mcr12_ref= 5424
+b8= cmuWall8.getEffectiveWidth()
+Ig8= cmuWall8.getGrossMomentOfInertia()
+Mcr8= cmuWall8.getCrackingMoment()
+Mcr8_ref= 2235
 
-ratio1= (eqThickness6/0.0254-5.62)/5.62
-ratio2= (eqThickness12/0.0254-4.01)/4.01
-ratio3= (eqThickness8/0.0254-3.46)/3.46
+
+ratios.append((eqThickness6/0.0254-5.62)/5.62)
+ratios.append((effArea6/0.0254**2*0.3048-68)/68)
+ratios.append((eqThickness12/0.0254-3.9)/3.9)
+ratios.append((effArea12/0.0254**2*0.3048-47)/47)
+ratios.append((eqThickness8/0.0254-3.46)/3.46)
+ratios.append((effArea8/0.0254**2*0.3048-42)/42)
+ratios.append((Ig12/0.0254**4-4119)/4119)
+ratios.append(abs(fr-fr_ref)/fr_ref)
+ratios.append(abs(Mcr8*8.85-Mcr8_ref)/Mcr8_ref)
+ratios.append(abs(Mcr12*8.85-Mcr12_ref)/Mcr12_ref)
 
 '''
 print('eqThickness6= ', eqThickness6/0.0254, ' in')
-print('ratio1= ', ratio1)
+print('ratio= ', ratios[0])
 print('eqThickness12= ', eqThickness12/0.0254, ' in')
-print('ratio2= ', ratio2)
+print('ratio= ', ratios[2])
 print('eqThickness8= ', eqThickness8/0.0254, ' in')
-print('ratio3= ', ratio3)
+print('ratio= ', ratios[3])
+print(effArea12/0.0254**2*0.3048)
+print(effArea8/0.0254**2*0.3048)
+print('b12= ',b12/0.0254,' in')
+print('Ig12= ',Ig12/(0.0254**4),' in4')
+print('fr= ',fr,' psi')
+print('fr_ref= ',fr_ref,' psi')
+
+print('b8= ',b8/0.0254,' in')
+print('Ig8= ',Ig8/(0.0254**4),' in4')
+print('Mcr8= ',Mcr8*8.85,' lb-in')
+print('Mcr8_ref= ',Mcr12_ref,' lb-in')
+
+print('Mcr12= ',Mcr12*8.85,' lb-in')
+print('Mcr12_ref= ',Mcr12_ref,' lb-in')
 '''
 
 import os
 from miscUtils import LogMessages as lmsg
 fname= os.path.basename(__file__)
-if (abs(ratio1)<1e-15 and abs(ratio1)<1e-15 and abs(ratio1)<1e-15):
+if (abs(max(ratios))<1e-15 and abs(min(ratios))<1e-15):
   print("test ",fname,": ok.")
 else:
+  print(ratios)
   lmsg.error(fname+' ERROR.')
