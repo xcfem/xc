4e4c71be6c790a18b4fae148a4e41e63c02eb5f5
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-10-29 19:32:42 +0100

    Remove zombie code

--- python_modules/postprocess/xcVtk/CAD_model/vtk_load_strprop_kpts.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-def VtkLoadStrPropKPts(setName, nmbProp, nmbContndr):
-  nmbContndr= vtk.vtkShStrArray()
-  eSet= preprocessor.getSets.getSet(setName)
-  eSet.numerate()
-  pnts= eSet.getPoints()
-  for p in pnts:
-    nmbContndr.insert_value(p.getIdx,deref(nmbProp)) # Asignamos etiqueta  
-  ugrid.GetPointData().SetStrings(nmbContndr)
--- python_modules/postprocess/xcVtk/vtk_define_id_filter.py
+++
@@ -1,7 +0,0 @@
-def VtkDefineIdFilter(nmbIdFilter):
-  # vtkIdFilter genera escalares a partir del identificador
-  nmbIdFilter= vtk.vtkIdFilter()
-  nmbIdFilter.SetInput(ugrid)
-  nmbIdFilter.CellIdsOff()
-  nmbIdFilter.PointIdsOff()
-
