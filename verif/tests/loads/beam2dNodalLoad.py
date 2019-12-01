9aaf67559aab60943f2801accec9cbaf5850b4ff
Luis C. Pérez Tato <l.pereztato@gmail.com>
2019-12-01 16:15:13 +0100

    Code cosmetics

--- verif/tests/loads/beam2dNodalLoad.py
+++ verif/tests/loads/beam2dNodalLoad.py
@@ -5,6 +5,9 @@
 # ISBN: 84-7790-336-0
 # url={https://books.google.ch/books?id=j88yAAAACAAJ},
 
+from __future__ import print_function
+from __future__ import division
+
 import xc_base
 import geom
 import xc
@@ -18,6 +21,15 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
+# Problem type
+feProblem= xc.FEProblem()
+preprocessor=  feProblem.getPreprocessor   
+nodes= preprocessor.getNodeHandler
+modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+
+
+    
+# Materials definition
 E= 2e6 # Elastic modulus
 L= 20 # Bar length.
 h= 0.30 # Beam cross-section depth.
@@ -25,26 +37,18 @@ b= 0.2 # Beam cross-section width.
 A= b*h # Cross section area.
 I= b*h**3/12 # Inertia of the beam section in inches to the fourth power.
 x= 0.5 # Relative abscissae where the punctual load is applied.
-P= 1e3 # punctual load.
+scc= typical_materials.defElasticSection2d(preprocessor, "scc",A,E,I)
 
-# Problem type
-feProblem= xc.FEProblem()
-preprocessor=  feProblem.getPreprocessor   
-nodes= preprocessor.getNodeHandler
-modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+# Mesh
 
-nodes.defaultTag= 1 #First node number.
+## Node definition
 nod1= nodes.newNodeXY(0,0)
 nod2= nodes.newNodeXY(L,0.0)
 
 # Geometric transformation(s)
 lin= modelSpace.newLinearCrdTransf("lin")
-    
-# Materials definition
-scc= typical_materials.defElasticSection2d(preprocessor, "scc",A,E,I)
 
-
-# Elements definition
+## Element definition
 elements= preprocessor.getElementHandler
 elements.defaultTransformation= "lin"
 elements.defaultMaterial= "scc"
@@ -58,18 +62,18 @@ constraints= preprocessor.getBoundaryCondHandler
 modelSpace.fixNode000(nod1.tag)
 
 
-# Loads definition
+# Load definition
+P= 1e3 # punctual load.
 loadHandler= preprocessor.getLoadHandler
 lPatterns= loadHandler.getLoadPatterns
-#Load modulation.
+## Load modulation.
 ts= lPatterns.newTimeSeries("constant_ts","ts")
 lPatterns.currentTimeSeries= "ts"
-#Load case definition
+## Load case definition
 lp0= lPatterns.newLoadPattern("default","0")
 lPatterns.currentLoadPattern= "0"
-#lp0.newNodalLoad(2,xc.Vector([0,-P,0]))
 nod2.newLoad(xc.Vector([0,-P,0]))
-#We add the load case to domain.
+## We add the load case to domain.
 lPatterns.addToDomain(lp0.name)
 
 # Solution
@@ -91,6 +95,6 @@ import os
 from miscUtils import LogMessages as lmsg
 fname= os.path.basename(__file__)
 if abs(ratio1)<1e-12:
-  print "test ",fname,": ok."
+  print("test ",fname,": ok.")
 else:
   lmsg.error(fname+' ERROR.')
