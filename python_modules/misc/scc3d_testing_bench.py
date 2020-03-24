aec5ed04a1f937ffb2bb3e8740d84f6afecd944e
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-03-24 14:29:32 +0100

    Code cosmetics

--- python_modules/misc/scc3d_testing_bench.py
+++ python_modules/misc/scc3d_testing_bench.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+
 import xc_base
 import geom
 import xc
 from model import predefined_spaces
 
 def sectionModel(preprocessor,sectionName):
-  ''' Defines a model to test a 3D fiber section.'''
-  nodes= preprocessor.getNodeHandler
+    ''' Defines a model to test a 3D fiber section.'''
+    nodes= preprocessor.getNodeHandler
 
-  modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
-  nodes.defaultTag= 1 # Next node tag will be 1.
-  nodA= nodes.newNodeXYZ(1,0,0)
-  nodB= nodes.newNodeXYZ(1,0,0)
+    modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
+    nodes.defaultTag= 1 # Next node tag will be 1.
+    nodA= nodes.newNodeXYZ(1,0,0)
+    nodB= nodes.newNodeXYZ(1,0,0)
 
-  elementos= preprocessor.getElementHandler
-  elementos.dimElem= 1
-  elementos.defaultMaterial= sectionName
-  elementos.defaultTag= 1 #Tag for the next element.
-  zls= elementos.newElement("ZeroLengthSection",xc.ID([nodA.tag,nodB.tag]))
-  return zls
+    elementos= preprocessor.getElementHandler
+    elementos.dimElem= 1
+    elementos.defaultMaterial= sectionName
+    elementos.defaultTag= 1 #Tag for the next element.
+    zls= elementos.newElement("ZeroLengthSection",xc.ID([nodA.tag,nodB.tag]))
+    return zls
 
 
