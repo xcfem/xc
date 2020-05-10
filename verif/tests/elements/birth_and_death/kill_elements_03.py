a74df214b0d8f19ad6f03b212fdd6d982a2bc3eb
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-05-10 15:42:00 +0200

    Add verification death test for truss elements

---
+++ verif/tests/elements/birth_and_death/kill_elements_03.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+# Home made test.
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
+__copyright__= "Copyright 2020, LCPT and AOO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import math
+import xc_base
+import geom
+import xc
+from model import predefined_spaces
+from materials import typical_materials
+
+A= 1.0 # Cross-section area (in2)
+E= 30e6 # Young modulus (psi)
+l= 10 # Bar length in inches
+F= 1000 # Force magnitude
+
+feProblem= xc.FEProblem()
+preprocessor=  feProblem.getPreprocessor
+nodes= preprocessor.getNodeHandler
+
+# Mesh
+modelSpace= predefined_spaces.SolidMechanics2D(nodes)
+n1= nodes.newNodeXY(0,0)
+n2= nodes.newNodeXY(l,0)
+
+elast= typical_materials.defElasticMaterial(preprocessor, "elast",E)
+
+elements= preprocessor.getElementHandler
+elements.dimElem= 2 #Bars defined in a two dimensional space.
+elements.defaultMaterial= "elast"
+trussA= elements.newElement("Truss",xc.ID([n1.tag,n2.tag]))
+trussA.sectionArea= A
+trussB= elements.newElement("Truss",xc.ID([n1.tag,n2.tag]))
+trussB.sectionArea= A
+
+# Constraints
+constraints= preprocessor.getBoundaryCondHandler
+#Constrain the displacement of node 1.
+spc1= constraints.newSPConstraint(n1.tag,0,0.0)
+spc2= constraints.newSPConstraint(n1.tag,1,0.0)
+#Constrain the displacement of node 2 in Y axis (gdl 1).
+spc5= constraints.newSPConstraint(n2.tag,1,0.0)
+
+# Loads
+loadHandler= preprocessor.getLoadHandler
+## Load pattern container:
+lPatterns= loadHandler.getLoadPatterns
+## time series for the load pattern:
+ts= lPatterns.newTimeSeries("constant_ts","ts")
+lPatterns.currentTimeSeries= "ts"
+## Load case definition
+lp0= lPatterns.newLoadPattern("default","0")
+lp0.newNodalLoad(n2.tag,xc.Vector([F,0]))
+
+## Add the load pattern to the domain.
+lPatterns.addToDomain(lp0.name)
+
+# Solution
+result= modelSpace.analyze(calculateNodalReactions= True)
+
+# Results
+ux_I= n2.getDisp[0]
+ux_refI= F*l/(E*2*A) # Both elements are active
+R1_I= n1.getReaction[0]
+R2_I= n2.getReaction[0]
+NA_I= trussA.getN1
+NB_I= trussB.getN1
+halfF= F/2.0
+
+ratio0= (ux_I-ux_refI)/ux_refI
+ratio1= (R1_I+F)/F
+ratio2= (R2_I)
+ratio3= (NA_I-halfF)/halfF
+ratio4= (NB_I-halfF)/halfF
+
+toKill= preprocessor.getSets.defSet("to_kill")
+toKill.getElements.append(trussB)
+toKill.killElements() # deactivate B truss
+
+mesh= feProblem.getDomain.getMesh
+mesh.setDeadSRF(0.0)
+mesh.freezeDeadNodes('locked') # Constraint inactive nodes.
+
+# Solve with dead element.
+result= modelSpace.analyze(calculateNodalReactions= True)
+
+ux_II= n2.getDisp[0]
+ux_refII=2.0*ux_refI # Only one active element
+R1_II= n1.getReaction[0]
+R2_II= n2.getReaction[0]
+NA_II= trussA.getN1
+NB_II= trussB.getN1
+
+ratio5= (ux_II-ux_refII)/ux_refII
+ratio6= (R1_II+F)/F
+ratio7= (R2_II)
+ratio8= (NA_II-F)/F
+ratio9= (NB_II)
+
+ratios= [ratio0,ratio1,ratio2,ratio3,ratio4,ratio5,ratio6,ratio7,ratio8,ratio9]
+
+error= 0.0
+for r in ratios:
+    error+= r**2
+error= math.sqrt(error)
+
+print("ux_I= ",ux_I)
+print("ux_refI= ",ux_refI)
+print("ratio0= ",ratio0)
+print("R1_I= ",R1_I)
+print("R2_I= ",R2_I)
+print("ratio1= ",ratio1)
+print("ratio2= ",ratio2)
+print("NA_I= ",NA_I)
+print("NB_I= ",NB_I)
+print("ratio3= ",ratio3)
+print("ratio4= ",ratio4)
+print("ux_II= ",ux_II)
+print("ux_refII= ",ux_refII)
+print("ratio5= ",ratio5)
+print("R1_II= ",R1_II)
+print("R2_II= ",R2_II)
+print("ratio6= ",ratio6)
+print("ratio7= ",ratio7)
+print("NA_II= ",NA_II)
+print("ratio8= ",ratio8)
+print("NB_II= ",NB_II)
+print("ratio9= ",ratio9)
+print("error= ",error)
+'''
+'''
+
+import os
+from misc_utils import log_messages as lmsg
+fname= os.path.basename(__file__)
+if error<1e-7:
+  print("test ",fname,": ok.")
+else:
+  lmsg.error(fname+' ERROR.')
+
+
