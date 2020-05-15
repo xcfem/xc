ef7f99fc8d3488bb8c233221adf596e2083b6f14
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-05-15 19:37:17 +0200

    Code update

--- verif/tests/elements/beam_column/cantilever3d_01.py
+++ verif/tests/elements/beam_column/cantilever3d_01.py
@@ -41,8 +41,8 @@ nodes= preprocessor.getNodeHandler
 modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
 
 nodes.defaultTag= 1 #First node number.
-nodes.newNodeXYZ(0,0.0,0.0)
-nodes.newNodeXYZ(L,0.0,0.0)
+n1= nodes.newNodeXYZ(0,0.0,0.0)
+n2= nodes.newNodeXYZ(L,0.0,0.0)
 
 lin= modelSpace.newLinearCrdTransf("lin",xc.Vector([0,1,0]))
 
@@ -56,9 +56,9 @@ elements= preprocessor.getElementHandler
 elements.defaultTransformation= "lin"
 elements.defaultMaterial= "section"
 elements.defaultTag= 1 #Tag for next element.
-beam3d= elements.newElement("ElasticBeam3d",xc.ID([1,2]))
+beam3d= elements.newElement("ElasticBeam3d",xc.ID([n1.tag,n2.tag]))
 
-modelSpace.fixNode000_000(1)
+modelSpace.fixNode000_000(n1.tag)
 
 loadHandler= preprocessor.getLoadHandler
 lPatterns= loadHandler.getLoadPatterns
@@ -67,7 +67,7 @@ ts= lPatterns.newTimeSeries("constant_ts","ts")
 lPatterns.currentTimeSeries= "ts"
 #Load case definition
 lp0= lPatterns.newLoadPattern("default","0")
-lp0.newNodalLoad(2,xc.Vector([F,0,0,0,0,0]))
+lp0.newNodalLoad(n2.tag,xc.Vector([F,0,0,0,0,0]))
 #We add the load case to domain.
 lPatterns.addToDomain(lp0.name)
 
@@ -75,8 +75,8 @@ lPatterns.addToDomain(lp0.name)
 analysis= predefined_solutions.simple_static_linear(feProblem)
 result= analysis.analyze(1)
 
-delta= nodes.getNode(2).getDisp[0] #x displacement of node 2.
-elements.getElement(1).getResistingForce()
+delta= n2.getDisp[0] #x displacement of node 2.
+beam3d.getResistingForce()
 N1= elements.getElement(1).getN1
 
 deltateor= (F*L/(E*A))
