712081a0b828f1fef472e59fba7abdc968178c8f
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-06-08 19:24:18 +0200

    Update code

--- verif/tests/materials/ehe/test_crack_control_01.py
+++ verif/tests/materials/ehe/test_crack_control_01.py
@@ -2,6 +2,8 @@
 ''' Reinforced concrete section. Crack control verification test.
    Results are compared with those of the «Prontuario informático del hormigón armado». '''
 
+from __future__ import division
+from __future__ import print_function
 
 import xc_base
 import geom
@@ -13,6 +15,7 @@ from solution import predefined_solutions # Solution procedure
 from materials.ehe import EHE_materials
 from materials.ehe import EHE_limit_state_checking
 from model import predefined_spaces
+from materials.sections.fiber_section import def_simple_RC_section
 
 __author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (A_OO)"
 __copyright__= "Copyright 2015, LCPT and AO_O"
@@ -23,49 +26,37 @@ __email__= "l.pereztato@gmail.com ana.ortega.ort@gmal.com"
 width= 1.0 # Section width expressed in meters.
 depth= 0.25 # Section depth expressed in meters.
 cover= 0.035 # Concrete cover expressed in meters.
-areaFi12=1.13e-4
+rebarDiam= 12e-3 # Rebar diameter.
+rebarArea= 1.13e-4 # Rebar area.
+numOfRebars= 7 # Number of rebars.
 
 NDato= 0.0 # Axial force for crack control checking.
 MyDato= 40e3 # Bending moment force for crack control checking.
 
 feProblem= xc.FEProblem()
 preprocessor=  feProblem.getPreprocessor
-# Materials definition
-concrMatTag25= EHE_materials.HA25.defDiagK(preprocessor)
-tagB500S= EHE_materials.B500S.defDiagK(preprocessor)
 
-geomSecHA= preprocessor.getMaterialHandler.newSectionGeometry("geomSecHA")
-regions= geomSecHA.getRegions
-concrete= regions.newQuadRegion(EHE_materials.HA25.nmbDiagK)
-concrete.nDivIJ= 10
-concrete.nDivJK= 10
-concrete.pMin= geom.Pos2d(-width/2.0,-depth/2.0)
-concrete.pMax= geom.Pos2d(width/2.0,depth/2.0)
-reinforcement= geomSecHA.getReinfLayers
-reinforcementSup= reinforcement.newStraightReinfLayer(EHE_materials.B500S.nmbDiagK)
-reinforcementSup.numReinfBars= 7
-reinforcementSup.barArea= areaFi12
-reinforcementSup.p1= geom.Pos2d(cover-width/2.0,depth/2.0-cover) # top layer (cara +).
-reinforcementSup.p2= geom.Pos2d(width/2.0-cover,depth/2.0-cover)
+# Material definition
+concr= EHE_materials.HA25
+steel= EHE_materials.B500S
 
-materialHandler= preprocessor.getMaterialHandler
-secHA= materialHandler.newMaterial("fiber_section_3d","secHA")
-fiberSectionRepr= secHA.getFiberSectionRepr()
-fiberSectionRepr.setGeomNamed("geomSecHA")
-secHA.setupFibers()
-#fibras= secHA.getFibers()
+# Reinforcement
+lowerRow= def_simple_RC_section.ReinfRow(areaRebar= rebarArea, width= width, nRebars= numOfRebars, nominalCover= cover-rebarDiam/2.0, nominalLatCover= cover)
+# Section geometry
+section= def_simple_RC_section.RCRectangularSection(name='test', width= width, depth= depth, concrType= concr, reinfSteelType= steel)
+section.positvRebarRows= def_simple_RC_section.LongReinfLayers([lowerRow])
+section.negatvRebarRows= def_simple_RC_section.LongReinfLayers([])
 
+section.defRCSection(preprocessor,matDiagType= 'k')
+scc3d_testing_bench.sectionModel(preprocessor, section.sectionName)
 
-scc3d_testing_bench.sectionModel(preprocessor, "secHA")
 # Constraints
 modelSpace= predefined_spaces.getStructuralMechanics3DSpace(preprocessor)
 modelSpace.fixNode000_000(1)
 modelSpace.fixNodeF00_0FF(2)
 
 # Loads definition
-loadHandler= preprocessor.getLoadHandler
-
-lPatterns= loadHandler.getLoadPatterns
+lPatterns= preprocessor.getLoadHandler.getLoadPatterns
 
 #Load modulation.
 ts= lPatterns.newTimeSeries("constant_ts","ts")
@@ -77,34 +68,31 @@ lp0.newNodalLoad(2,xc.Vector([NDato,0,0,0,MyDato,0]))
 #We add the load case to domain.
 lPatterns.addToDomain(lp0.name)
 
-
-
 # Solution procedure
 analysis= predefined_solutions.simple_newton_raphson(feProblem)
 analOk= analysis.analyze(10)
 
-
 secHAParamsFis= EHE_limit_state_checking.CrackControl('SLS_crack')
-
-
 
 elements= preprocessor.getElementHandler
 ele1= elements.getElement(1)
 scc= ele1.getSection()
 secHAParamsFis.computeWk(scc,EHE_materials.HA25.matTagK,EHE_materials.B500S.matTagK,EHE_materials.HA25.fctm())
 
-ratio1= ((secHAParamsFis.Wk-0.21e-3)/0.21e-3)
-
+ratio1= ((secHAParamsFis.Wk-0.226243929766e-3)/0.226243929766e-3)
+ratio2= ((secHAParamsFis.Wk-0.21e-3)/0.21e-3)
 
 '''
 secHAParamsFis.printParams()
-print "ratio1= ",ratio1
+print("Wk= ", secHAParamsFis.Wk*1e3,"mm")
+print("ratio1= ", ratio1)
+print("ratio2= ", ratio2)
 '''
 
 import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if (abs(ratio1)<0.1):
-  print "test ",fname,": ok."
+if ((abs(ratio1)<1e-10) and abs(ratio2<0.1)):
+  print("test ",fname,": ok.")
 else:
   lmsg.error(fname+' ERROR.')
