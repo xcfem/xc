b2d4b53b720b7f2d9a408b51d813353d522a3806
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-07-10 16:47:26 +0200

    Code cosmetics

--- verif/tests/solution/eigenvalues/cantilever_eigenmodes_04.py
+++ verif/tests/solution/eigenvalues/cantilever_eigenmodes_04.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Tomado del example B46 del SOLVIA Verification Manual
+''' SOLVIA Verification Manual. Example B46.''' 
 
 from __future__ import print_function
 from __future__ import division
