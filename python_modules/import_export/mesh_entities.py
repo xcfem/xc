282fcf92c7bf554accc171f565dc7a80af1cb973
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-09-07 13:15:16 +0200

    Code cosmetics

--- python_modules/import_export/mesh_entities.py
+++ python_modules/import_export/mesh_entities.py
@@ -5,21 +5,21 @@ from import_export import basic_entities as be
 from misc_utils import log_messages as lmsg
 
 class MaterialRecord(object):
-  def __init__(self,name,typo,thermalExp,rho,E,nu,G,logDec,specHeat,thermalCond):
-    self.name= name
-    self.typo= typo
-    self.thermalExp= thermalExp
-    self.rho= rho
-    self.E= E
-    self.nu= nu
-    self.G= G
-    self.logDec= logDec
-    self.specHeat= specHeat
-    self.thermalCond= thermalCond
+    def __init__(self,name,typo,thermalExp,rho,E,nu,G,logDec,specHeat,thermalCond):
+        self.name= name
+        self.typo= typo
+        self.thermalExp= thermalExp
+        self.rho= rho
+        self.E= E
+        self.nu= nu
+        self.G= G
+        self.logDec= logDec
+        self.specHeat= specHeat
+        self.thermalCond= thermalCond
 
 class MaterialDict(dict):
-  def append(self,mat):
-    self[mat.name]= mat
+    def append(self,mat):
+        self[mat.name]= mat
 
 class NodeRecord(object):
     '''Node of a finite element mesh'''
@@ -39,11 +39,11 @@ class NodeRecord(object):
     def __str__(self):
         return str(self.id)+' '+str(self.coords)
     def getX(self):
-         return self.coords[0]
+        return self.coords[0]
     def getY(self):
-         return self.coords[1]
+        return self.coords[1]
     def getZ(self):
-         return self.coords[2]
+        return self.coords[2]
     def getStrXCCommand(self,nodeHandlerName):
         strId= str(self.id)
         strCommand= '.newNodeIDXYZ(' + strId + ',' + self.coords[0] + ',' + self.coords[1] +','+ self.coords[2]+')'
@@ -53,50 +53,50 @@ class NodeRecord(object):
         msp.add_point((self.coords[0], self.coords[1], self.coords[2]), dxfattribs={'layer': layerName})
 
 class NodeDict(dict):
-  ''' Node container.'''
-  def append(self,id,x,y,z):
-    self[id]= NodeRecord(int(id),[x,y,z])
-  def getName(self):
-    return 'nodes'
-  def readFromDATFile(self,lines,begin,end):
-    self.clear()
-    for i in range(begin,end):
-      line= lines[i]
-      id, x, y ,z = line.split()
-      self.append(id,x,y,z)
-  def readFromUMesh(self,umesh):
-    nodes= umesh.getCoordinatesAndOwner()
-    self.clear()
-    id= 0
-    for n in nodes:
-      pos= n.getInitialPos3d
-      self.append(id,pos.x,pos.y,pos.z)
-      id+= 1
-  def readFromXCSet(self,xcSet):
-    nodeSet= xcSet.nodes
-    for n in nodeSet:
-      pos= n.getInitialPos3d
-      self.append(n.tag, pos.x, pos.y, pos.z)
-  def writeDxf(self,drawing):
-    '''Write the node positions in dxf file.'''
-    layerName= self.getName()
-    drawing.add_layer(layerName)
-    for key in self:
-      self[key].writeDxf(drawing,layerName)
-  def writeToXCFile(self,f,xcImportExportData):
-    ''' Write the XC commands that define nodes.'''
-    for key in self:
-      strCommand= self[key].getStrXCCommand(xcImportExportData.nodeHandlerName)
-      f.write(strCommand+'\n')
-  def getTags(self):
-    retval= list()
-    for key in self:
-      retval.append(self[key].id)
-    return retval
-  def __str__(self):
-    retval= ''
-    for key in self:
-      retval+= str(self[key]) + '\n'
+    ''' Node container.'''
+    def append(self,id,x,y,z):
+        self[id]= NodeRecord(int(id),[x,y,z])
+    def getName(self):
+        return 'nodes'
+    def readFromDATFile(self,lines,begin,end):
+        self.clear()
+        for i in range(begin,end):
+            line= lines[i]
+            id, x, y ,z = line.split()
+            self.append(id,x,y,z)
+    def readFromUMesh(self,umesh):
+        nodes= umesh.getCoordinatesAndOwner()
+        self.clear()
+        id= 0
+        for n in nodes:
+            pos= n.getInitialPos3d
+            self.append(id,pos.x,pos.y,pos.z)
+            id+= 1
+    def readFromXCSet(self,xcSet):
+        nodeSet= xcSet.nodes
+        for n in nodeSet:
+          pos= n.getInitialPos3d
+          self.append(n.tag, pos.x, pos.y, pos.z)
+    def writeDxf(self,drawing):
+        '''Write the node positions in dxf file.'''
+        layerName= self.getName()
+        drawing.add_layer(layerName)
+        for key in self:
+            self[key].writeDxf(drawing,layerName)
+    def writeToXCFile(self,f,xcImportExportData):
+        ''' Write the XC commands that define nodes.'''
+        for key in self:
+            strCommand= self[key].getStrXCCommand(xcImportExportData.nodeHandlerName)
+            f.write(strCommand+'\n')
+    def getTags(self):
+        retval= list()
+        for key in self:
+            retval.append(self[key].id)
+        return retval
+    def __str__(self):
+        retval= ''
+        for key in self:
+            retval+= str(self[key]) + '\n'
 
 class CellRecord(object):
     _ids= count(0) # counter
@@ -162,167 +162,166 @@ class CellRecord(object):
                 msp.add_line(ptA, ptB, dxfattribs={'layer': layerName})
 
 class CellDict(dict):
-  '''Cell container.'''
-  def append(self,cell):
-    self[cell.id]= cell
-  def readFromDATFile(self,lines,begin,end):
-    self.clear()
-    for i in range(begin,end):
-      line= lines[i]
-      lst= line.split()
-      sz= len(lst)
-      id= int(lst[0])
-      type= int(lst[1])
-      nodeIds= list()
-      for j in range(2,sz):
-        nodeIds.append(int(lst[j]))
-      self[id]= CellRecord(id, type, nodeIds)
-  def readFromUMesh(self,umesh):
-    for i in range(0,umesh.getNumberOfCells()):
-      self.append(CellRecord(umesh.getTypeOfCell(i), umesh.getNodeIdsOfCell(i)))
-  def readFromXCSet(self,xcSet):
-    elemSet= xcSet.elements
-    for e in elemSet:
-      nodes= e.getNodes.getExternalNodes
-      numNodes= len(nodes)
-      if(numNodes==4):
-        tagNodes= [nodes[0],nodes[1],nodes[2],nodes[3]]
-        thickness= e.getPhysicalProperties.getVectorMaterials[0].h
-        cell= CellRecord(e.tag,str(e.tag),tagNodes,thickness)
-      elif(numNodes==2):
-        tagNodes= [nodes[0],nodes[1]]
-        cell= CellRecord(e.tag,str(e.tag),tagNodes,0.0)
-      self.append(cell)        
-  def writeDxf(self,nodeDict,drawing):
-    '''Write the cells in dxf file.'''
-    layerName= 'cells'
-    drawing.add_layer(layerName)
-    for key in self:
-      self[key].writeDxf(nodeDict,drawing,layerName)
-  def writeToXCFile(self,f,xcImportExportData):
-    '''Write the XC commands that define the cells (elements).'''
-    for key in self:
-      cell= self[key]
-      type= xcImportExportData.convertCellType(cell.cellType)
-      if(type!=None):
-        strCommand= self[key].getStrXCCommand(xcImportExportData)
-        f.write(strCommand+'\n')
-  def getTags(self):
-    retval= list()
-    for key in self:
-      retval.append(self[key].id)
-    return retval
-  def __str__(self):
-    retval= ''
-    for key in self:
-      retval+= str(self[key]) + '\n'
-
-
+    '''Cell container.'''
+    def append(self,cell):
+        self[cell.id]= cell
+    def readFromDATFile(self,lines,begin,end):
+        self.clear()
+        for i in range(begin,end):
+          line= lines[i]
+          lst= line.split()
+          sz= len(lst)
+          id= int(lst[0])
+          type= int(lst[1])
+          nodeIds= list()
+          for j in range(2,sz):
+            nodeIds.append(int(lst[j]))
+          self[id]= CellRecord(id, type, nodeIds)
+    def readFromUMesh(self,umesh):
+        for i in range(0,umesh.getNumberOfCells()):
+          self.append(CellRecord(umesh.getTypeOfCell(i), umesh.getNodeIdsOfCell(i)))
+    def readFromXCSet(self,xcSet):
+        elemSet= xcSet.elements
+        for e in elemSet:
+          nodes= e.getNodes.getExternalNodes
+          numNodes= len(nodes)
+          if(numNodes==4):
+            tagNodes= [nodes[0],nodes[1],nodes[2],nodes[3]]
+            thickness= e.getPhysicalProperties.getVectorMaterials[0].h
+            cell= CellRecord(e.tag,str(e.tag),tagNodes,thickness)
+          elif(numNodes==2):
+            tagNodes= [nodes[0],nodes[1]]
+            cell= CellRecord(e.tag,str(e.tag),tagNodes,0.0)
+          self.append(cell)        
+    def writeDxf(self,nodeDict,drawing):
+        '''Write the cells in dxf file.'''
+        layerName= 'cells'
+        drawing.add_layer(layerName)
+        for key in self:
+          self[key].writeDxf(nodeDict,drawing,layerName)
+    def writeToXCFile(self,f,xcImportExportData):
+        '''Write the XC commands that define the cells (elements).'''
+        for key in self:
+          cell= self[key]
+          type= xcImportExportData.convertCellType(cell.cellType)
+          if(type!=None):
+            strCommand= self[key].getStrXCCommand(xcImportExportData)
+            f.write(strCommand+'\n')
+    def getTags(self):
+        retval= list()
+        for key in self:
+          retval.append(self[key].id)
+        return retval
+    def __str__(self):
+        retval= ''
+        for key in self:
+          retval+= str(self[key]) + '\n'
 
 class NodeSupportRecord(be.SupportRecord):
-  ''' Constraints for node displacements'''
-  def __init__(self, id, nodeId, xComp= be.ComponentSupportRecord(), yComp= be.ComponentSupportRecord(), zComp= be.ComponentSupportRecord(), rxComp= be.ComponentSupportRecord('Free'), ryComp= be.ComponentSupportRecord('Free'), rzComp= be.ComponentSupportRecord('Free')):
-    super(NodeSupportRecord,self).__init__(id,xComp,yComp,zComp,rxComp,ryComp,rzComp)
-    self.nodeId= nodeId
-  def __str__(self):
-    return str(self.id) + ' nodeId: ' + str(self.nodeId) + ' ' + self.getStrConstraints()
+    ''' Constraints for node displacements.'''
+    def __init__(self, id, nodeId, xComp= be.ComponentSupportRecord(), yComp= be.ComponentSupportRecord(), zComp= be.ComponentSupportRecord(), rxComp= be.ComponentSupportRecord('Free'), ryComp= be.ComponentSupportRecord('Free'), rzComp= be.ComponentSupportRecord('Free')):
+        super(NodeSupportRecord,self).__init__(id,xComp,yComp,zComp,rxComp,ryComp,rzComp)
+        self.nodeId= nodeId
+    def __str__(self):
+        return str(self.id) + ' nodeId: ' + str(self.nodeId) + ' ' + self.getStrConstraints()
 
 def getConstraintsByNode(domain):
-  retval= defaultdict(list)
-  spIter= domain.getConstraints.getSPs
-  sp= spIter.next()
-  while sp:
-    retval[sp.nodeTag].append(sp)
+    retval= defaultdict(list)
+    spIter= domain.getConstraints.getSPs
     sp= spIter.next()
-  return retval
+    while sp:
+        retval[sp.nodeTag].append(sp)
+        sp= spIter.next()
+    return retval
 
 class NodeSupportDict(dict):
   def append(self, ns):
-    if (ns.nodeId in self):
-      lmsg.warning('support for node: '+ns.nodeId+' redefined.')
-    self[ns.nodeId]= ns
+      if (ns.nodeId in self):
+          lmsg.warning('support for node: '+ns.nodeId+' redefined.')
+      self[ns.nodeId]= ns
   def readFromXCDomain(self,domain):
-    '''Read SP constraints from an XC domain.'''
-    spConstraintsByNode= getConstraintsByNode(domain)
-    supportId= 1
-    for tagNode in spConstraintsByNode:
-      constraintsNode= spConstraintsByNode[tagNode]
-      labels= ['Free','Free','Free','Free','Free','Free']
-      for sp in constraintsNode:
-        gdl= sp.getDOFNumber
-        dispValue= sp.getValue
-        if(dispValue == 0.0):
-          labels[gdl]= 'Rigid'
-        else:
-          lmsg.error('Error; imposed displacement constraints not implemented.')
-      nsr= NodeSupportRecord(supportId,tagNode)
-      nsr.setupFromComponentLabels(labels)
-      self.append(nsr)
-      supportId+= 1
+      '''Read SP constraints from an XC domain.'''
+      spConstraintsByNode= getConstraintsByNode(domain)
+      supportId= 1
+      for tagNode in spConstraintsByNode:
+          constraintsNode= spConstraintsByNode[tagNode]
+          labels= ['Free','Free','Free','Free','Free','Free']
+          for sp in constraintsNode:
+              gdl= sp.getDOFNumber
+              dispValue= sp.getValue
+              if(dispValue == 0.0):
+                  labels[gdl]= 'Rigid'
+              else:
+                  lmsg.error('Error; imposed displacement constraints not implemented.')
+          nsr= NodeSupportRecord(supportId,tagNode)
+          nsr.setupFromComponentLabels(labels)
+          self.append(nsr)
+          supportId+= 1
+        
   def getNodeTags(self):
-    '''Return dictionary keys in a list.'''
-    retval= list()
-    for key in self:
-      nodeTag= self[key].nodeId
-      retval.append(nodeTag)
-    return retval
+      '''Return dictionary keys in a list.'''
+      retval= list()
+      for key in self:
+        nodeTag= self[key].nodeId
+        retval.append(nodeTag)
+      return retval
 
 class MeshData(object):
 
-  def __init__(self):
-    self.name= None
-    self.numberOfCells= None
-    self.numberOfNodes= None
-    self.materials= MaterialDict()
-    self.nodes= NodeDict()
-    self.cells= CellDict()
-    self.nodeSupports= NodeSupportDict()
-    self.groups= []
+    def __init__(self):
+        self.name= None
+        self.numberOfCells= None
+        self.numberOfNodes= None
+        self.materials= MaterialDict()
+        self.nodes= NodeDict()
+        self.cells= CellDict()
+        self.nodeSupports= NodeSupportDict()
+        self.groups= []
 
-  def readFromDATFile(self,fName):
-    # Read the source mesh
-    f= open(fName,"r")
+    def readFromDATFile(self,fName):
+        # Read the source mesh
+        f= open(fName,"r")
 
-    lines= f.readlines()
-    line= lines[0]
-    nn,nc= line.split()
-    self.numberOfNodes= int(nn)
-    self.numberOfCells= int(nc)
+        lines= f.readlines()
+        line= lines[0]
+        nn,nc= line.split()
+        self.numberOfNodes= int(nn)
+        self.numberOfCells= int(nc)
 
-    beginNodes= 1
-    endNodes= self.numberOfNodes+1
+        beginNodes= 1
+        endNodes= self.numberOfNodes+1
 
-    self.nodes.readFromDATFile(lines,beginNodes,endNodes)
+        self.nodes.readFromDATFile(lines,beginNodes,endNodes)
 
-    beginCells= endNodes
-    endCells= beginCells+self.numberOfCells
-    self.cells.readFromDATFile(lines,beginCells,endCells)
+        beginCells= endNodes
+        endCells= beginCells+self.numberOfCells
+        self.cells.readFromDATFile(lines,beginCells,endCells)
 
-  def readFromXCSet(self,xcSet):
-    '''Read nodes and elements from an XC set.'''
-    self.nodes.readFromXCSet(xcSet)
-    self.cells.readFromXCSet(xcSet)
-    self.nodeSupports.readFromXCDomain(xcSet.getPreprocessor.getDomain)
-  def writeDxf(self,drawing):
-    '''Write mesh in a DXF file.'''
-    self.nodes.writeDxf(drawing)
-    self.cells.writeDxf(self.nodes,drawing)
-    #groups have no representation in DXF files.
-  def writeDxfFile(self,fileName):
-    '''Write mesh in a DXF file.'''
-    drawing= DXFEngine.drawing(fileName)
-    self.writeDxf(drawing)
-    drawing.save()
-  def writeToXCFile(self,xcImportExportData):
-    '''Write the XC commands that define the mesh.'''
-    f= xcImportExportData.outputFile
-    self.nodes.writeToXCFile(f,xcImportExportData)
-    self.cells.writeToXCFile(f,xcImportExportData)
-    for g in self.groups:
-      g.writeToXCFile(xcImportExportData)
-  def __str__(self):
-    retval= "numberOfNodes= " +' '+str(self.numberOfNodes) + '\n'
-    retval+= "numberOfCells= " +' '+str(self.numberOfCells) + '\n'
-    retval+= str(self.nodes)
-    retval+= str(self.cells)
-    return retval
+    def readFromXCSet(self,xcSet):
+        '''Read nodes and elements from an XC set.'''
+        self.nodes.readFromXCSet(xcSet)
+        self.cells.readFromXCSet(xcSet)
+        self.nodeSupports.readFromXCDomain(xcSet.getPreprocessor.getDomain)
+    def writeDxf(self,drawing):
+        '''Write mesh in a DXF file.'''
+        self.nodes.writeDxf(drawing)
+        self.cells.writeDxf(self.nodes,drawing)
+        #groups have no representation in DXF files.
+    def writeDxfFile(self,fileName):
+        '''Write mesh in a DXF file.'''
+        drawing= ezdxf.new()
+        self.writeDxf(drawing)
+        drawing.saveas(fileName)    
+    def writeToXCFile(self,xcImportExportData):
+        '''Write the XC commands that define the mesh.'''
+        f= xcImportExportData.outputFile
+        self.nodes.writeToXCFile(f,xcImportExportData)
+        self.cells.writeToXCFile(f,xcImportExportData)
+        for g in self.groups:
+          g.writeToXCFile(xcImportExportData)
+    def __str__(self):
+        retval= "numberOfNodes= " +' '+str(self.numberOfNodes) + '\n'
+        retval+= "numberOfCells= " +' '+str(self.numberOfCells) + '\n'
+        retval+= str(self.nodes)
+        retval+= str(self.cells)
+        return retval
