a2569f0e1de410bc6d7f2ba9cba77816314f9271
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-09-16 11:43:29 +0200

    Script to install Gmsh from sources

---
+++ install/gmsh_code_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##########################################################
+# script for compiling gmsh on linux operating system
+# it should run on any linux which has all needed packages installed
+
+
+##########################################################
+# get current directory and create a new for building xc
+base_dir=`pwd`
+echo "Base dir: ${base_dir}"
+rm -rf build_gmsh
+mkdir build_gmsh
+
+##########################################################
+# get the number of physical CPUs
+physicalCpuCount=$(getconf _NPROCESSORS_ONLN)
+cpusToUse=$((physicalCpuCount-1))
+cpusToUse=$((cpusToUse>1 ? cpusToUse : 1))
+echo "Will use ${cpusToUse} CPUs"
+
+##########################################################
+# clone gmsh
+cd $base_dir
+git clone https://gitlab.onelab.info/gmsh/gmsh.git
+
+##########################################################
+# build gmsh
+cd $base_dir
+cd build_gmsh
+cmake -DENABLE_BUILD_DYNAMIC=1 -DENABLE_FLTK=1 -DENABLE_MPI=0 ../gmsh
+make -j 3
+sudo make install
