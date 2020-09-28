b7f826dfb116e5ac18658f614aa599d1256dbfcf
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-09-28 20:42:16 +0200

    Separate drawing of block local axes
    Allow to display the lines and surfaces of the model without
    displaying the local axes.

--- python_modules/postprocess/xcVtk/CAD_model/vtk_CAD_graphic.py
+++ python_modules/postprocess/xcVtk/CAD_model/vtk_CAD_graphic.py
@@ -17,6 +17,28 @@ from postprocess.xcVtk.fields import local_axes_vector_field as lavf
 
 class DisplaySettingsBlockTopo(vtk_graphic_base.DisplaySettings):
     ''' Define graphic output.'''
+
+    def defineLocalAxes(self, setToDisplay):
+        ''' Define the local axes of the blocks.
+
+        :param setToDisplay: set of elements to be displayed.
+        '''
+        lineAxesField= None
+        surfAxesField= None
+        numKPts= setToDisplay.getPoints.size
+        if(numKPts>0):
+            avgSize= setToDisplay.getEntities.getAverageSize()
+            vectorScale= avgSize/4.0
+            ## Draw lines local axes.
+            lineAxesField= lavf.LinesLocalAxesVectorField(setToDisplay.name+'_lineLocalAxes', scaleFactor= vectorScale)
+            lineAxesField.dumpVectors(setToDisplay)
+            ## Draw quad surfaces local axes.
+            surfAxesField= lavf.SurfacesLocalAxesVectorField(setToDisplay.name+'_surfLocalAxes', scaleFactor= vectorScale)
+            surfAxesField.dumpVectors(setToDisplay)
+        else:
+            lmsg.warning("Error when drawing set: '"+setToDisplay.name+"' it has not key points so I can't get set geometry (use fillDownwards?)")
+        return lineAxesField, surfAxesField
+        
     def defineMeshScene(self):
         ''' Define mesh scene on ouput display.'''
         self.gridRecord.uGrid= vtk.vtkUnstructuredGrid()
@@ -25,29 +47,16 @@ class DisplaySettingsBlockTopo(vtk_graphic_base.DisplaySettings):
         self.gridRecord.uGrid.name= setToDraw.name+'_grid'
         numKPts= setToDraw.getPoints.size
         if(numKPts>0):
-            cad_mesh.VtkCargaMalla(self.gridRecord)
+            cad_mesh.vtk_load_mesh_data(self.gridRecord)
             self.renderer= vtk.vtkRenderer()
             self.renderer.SetBackground(self.bgRComp,self.bgGComp,self.bgBComp)
             cad_mesh.VtkDefineActorKPoint(self.gridRecord,self.renderer,0.02)
             cad_mesh.VtkDefineActorCells(self.gridRecord,self.renderer,"wireframe")
-
-            # Draw local axes.
-            avgSize= setToDraw.getEntities.getAverageSize()
-            vectorScale= avgSize/4.0
-            ## Draw lines local axes.
-            lineAxesField= lavf.LinesLocalAxesVectorField(setToDraw.name+'_lineLocalAxes', scaleFactor= vectorScale)
-            lineAxesField.dumpVectors(setToDraw)
-            lineAxesField.addToDisplay(self)
-            ## Draw quad surfaces local axes.
-            surfAxesField= lavf.QuadSurfacesLocalAxesVectorField(setToDraw.name+'_surfLocalAxes', scaleFactor= vectorScale)
-            surfAxesField.dumpVectors(setToDraw)
-            surfAxesField.addToDisplay(self)
-            
             self.renderer.ResetCamera()
         else:
             lmsg.warning("Error when drawing set: '"+setToDraw.name+"' it has not key points so I can't get set geometry (use fillDownwards?)")
 
-        #Implementar dibujo de etiquetas.
+        #Implement drawing of labels.
         # if(entToLabel=="cells"):
         #   postprocess.xcVtk.cad_mesh.VtkDibujaIdsCells(self.gridRecord,setToDraw,cellTypeName,renderer)
         # elif(entToLabel=="points"):
@@ -63,7 +72,7 @@ class DisplaySettingsBlockTopo(vtk_graphic_base.DisplaySettings):
         self.setupGrid(setToDisplay)
         self.displayGrid(caption)
 
-    def displayBlocks(self, setToDisplay, caption= '',fileName= None):
+    def displayBlockEntities(self, setToDisplay, caption, fileName= None):
         ''' Display geometric entities (points, lines, surfaces and volumes)
 
         :param setToDisplay: set to be represented
@@ -74,5 +83,33 @@ class DisplaySettingsBlockTopo(vtk_graphic_base.DisplaySettings):
         self.defineMeshScene()
         self.displayScene(caption,fileName)
 
+    def displayLocalAxes(self, setToDisplay, caption, fileName= None):
+        '''Display the element local axes.
 
+        :param setToDisplay:   set of elements to be displayed (defaults to total set)
+        :param caption:        text to display in the graphic 
+        :param fileName: file name to store the image. If none -> window on screen.
+        '''
+        self.setupGrid(setToDisplay)
+        # Draw local axes.
+        lineAxesField, surfAxesField= self.defineLocalAxes(setToDisplay)
+        self.defineMeshScene()
+        if(lineAxesField):
+            lineAxesField.addToDisplay(self) # Draw lines local axes.
+        if(surfAxesField):
+            surfAxesField.addToDisplay(self) # Draw quad surfaces local axes.
+        self.displayScene(caption, fileName)
+
+    def displayBlocks(self, setToDisplay, displayLocalAxes= True, caption= '', fileName= None):
+        '''Display the element local axes.
+
+        :param setToDisplay:   set of elements to be displayed (defaults to total set)
+        :param displayLocalAxes: if true, show also local axes of entities.
+        :param caption:        text to display in the graphic 
+        :param fileName: file name to store the image. If none -> window on screen.
+        '''
+        if(displayLocalAxes):
+            self.displayLocalAxes(setToDisplay, caption, fileName)
+        else:
+            self.displayBlockEntities(setToDisplay, caption, fileName)
 
