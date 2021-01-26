d9e194875a7bf588330b330dc63ac3ac2a29fab0
Luis Claudio Pérez Tato <l.pereztato@gmail.com>
2021-01-26 13:13:45 +0100

    Update code

--- verif/tests/aux/sol_superlu.py
+++ verif/tests/aux/sol_superlu.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
-solu= feProblem.getSoluProc
-solCtrl= solu.getSoluControl
-solModels= solCtrl.getModelWrapperContainer
-sm= solModels.newModelWrapper("sm")
-cHandler= sm.newConstraintHandler("penalty_constraint_handler")
-cHandler.alphaSP= 1.0e15
-cHandler.alphaMP= 1.0e15
 
-numberer= sm.newNumberer("default_numberer")
-numberer.useAlgorithm("simple")
-# Con este solver no es necesario renumerar.
-solutionStrategies= solCtrl.getSolutionStrategyContainer
-solutionStrategy= solutionStrategies.newSolutionStrategy("solutionStrategy","sm")
+from solution import predefined_solutions
 
-solAlgo= solutionStrategy.newSolutionAlgorithm("linear_soln_algo")
-integ= solutionStrategy.newIntegrator("load_control_integrator",xc.Vector([]))
-soe= solutionStrategy.newSystemOfEqn("sparse_gen_col_lin_soe")
-solver= soe.newSolver("super_lu_solver")
+class SuperLU(predefined_solutions.SolutionProcedure):
+    ''' Return a linear static solution algorithm
+        with a penalty constraint handler and
+        a super_lu solver.
+    '''
+    def __init__(self, prb, name= None, maxNumIter= 1, convergenceTestTol= 1e-9, printFlag= 0, numSteps= 1):
+        ''' Constructor.
 
-analysis= solu.newAnalysis("static_analysis","solutionStrategy","")
-result= analysis.analyze(1)
+        :param prb: XC finite element problem.
+        :param name: identifier for the solution procedure.
+        :param maxNumIter: maximum number of iterations (defauts to 10)
+        :param convergenceTestTol: convergence tolerance (defaults to 1e-9)
+        :param printFlag: if not zero print convergence results on each step.
+        :param numSteps: number of steps to use in the analysis (useful only when loads are variable in time).
+        '''
+        super(SuperLU,self).__init__(name, maxNumIter, convergenceTestTol, printFlag, numSteps)
+        # Sparse solver doesn't need renumbering.
+        modelWrapperName= self.defineModelWrapper(prb, numberingMethod= 'simple')
+        self.defineConstraintHandler('penalty')
+        self.defineSolutionAlgorithm(solAlgType= 'linear_soln_algo', integratorType= 'load_control_integrator', convTestType= None)
+        self.defineSysOfEq(soeType= 'sparse_gen_col_lin_soe', solverType= 'super_lu_solver')
+        self.defineAnalysis('static_analysis')
+
+solProc= SuperLU(feProblem)
+result= solProc.solve()
