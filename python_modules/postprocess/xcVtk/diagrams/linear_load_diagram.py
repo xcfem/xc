c207dcfff0a43a01075d7b22de33a6ba590c6dc4
Ana <ana.ortega.ort@gmail.com>
2021-02-01 14:17:36 +0100

    Display of loads on linear elements adding all active load cases

--- python_modules/postprocess/xcVtk/diagrams/linear_load_diagram.py
+++ python_modules/postprocess/xcVtk/diagrams/linear_load_diagram.py
@@ -21,6 +21,9 @@ class LinearLoadDiagram(cd.ColoredDiagram):
 
     :ivar setToDisp: set to display.
     :ivar component: component to display.
+    :ivar dictActLoadVectors: dictionary that stores for each 
+          linear loaded element the sum of active loads on it
+          (defaults to None)
     '''
     def __init__(self,setToDisp,scale,fUnitConv,component):
         ''' Constructor.
@@ -31,83 +34,100 @@ class LinearLoadDiagram(cd.ColoredDiagram):
         super(LinearLoadDiagram,self).__init__(scale,fUnitConv)
         self.setToDisp=setToDisp
         self.component= component
+        self.dictActLoadVectors=None
 
-    def dumpElementalLoads(self,preprocessor,lp,indxDiagram):
-        ''' Iterate over loaded elements dumping its loads into the graphic.'''
-        lIter= lp.loads.getElementalLoadIter
-        eLoad= lIter.next()
-        eTagsSet= self.setToDisp.getElements.getTags()
-        while(eLoad):
-            tags= eLoad.elementTags
-            for i in range(0,len(tags)):
-                eTag= tags[i]
-                if eTag in eTagsSet:
-                    elem= preprocessor.getElementHandler.getElement(eTag)
-                    dim= elem.getDimension
-                    if(dim==1):
-                        vJ= elem.getJVector3d(True)
-                        vK= elem.getKVector3d(True)
-                        if(self.component=='axialComponent'):
-                            self.vDir= vJ
-                            indxDiagram= self.appendDataToDiagram(elem,indxDiagram,eLoad.axialComponent,eLoad.axialComponent)
-                        elif(self.component=='transComponent'):
-                            self.vDir= vJ
-                            indxDiagram= self.appendDataToDiagram(elem,indxDiagram,eLoad.transComponent,eLoad.transComponent)
-                        elif(self.component=='transYComponent'):
-                            self.vDir= vJ
-                            indxDiagram= self.appendDataToDiagram(elem,indxDiagram,eLoad.transYComponent,eLoad.transYComponent)
-                        elif(self.component=='transZComponent'):
-                            self.vDir= vK
-                            indxDiagram= self.appendDataToDiagram(elem,indxDiagram,eLoad.transZComponent,eLoad.transZComponent)
-                        elif(self.component=='xyzComponents'):
-                            vI= elem.getIVector3d(True)
-                            localForce= eLoad.getVector3dLocalForce() # Local components of the force.
-                            v= localForce.x*vI+localForce.y*vJ+localForce.z*vK # Global force vector.
-                            self.vDir= v.normalized() 
-                            value= v.getModulus()
-                            indxDiagram= self.appendDataToDiagram(elem,indxDiagram,value,value)
-                        else:    
-                            lmsg.error("LinearLoadDiagram :'"+self.component+"' unknown.")        
-            eLoad= lIter.next()
-        return indxDiagram
-        
+    def sumElementalLoads(self,actLP):
+        ''' Iterate over active load patterns and cumulate on elements their elemental loads.
+        Returns a dictionary that stores for each linear loaded element the sum of active 
+        loads on it
 
-    def getMaxAbsComp(self,preprocessor):
-        '''Return the maximum absolute value of the component.
-        It is used for calculating auto-scale parameter
+        :param actLP: list of active load patterns.
         '''
-        maxV= 0.0
-        activeLoadPatterns= preprocessor.getDomain.getConstraints.getLoadPatterns
-        if(len(activeLoadPatterns)<1):
-            lmsg.warning('No active load patterns.')
-        eTagsSet= self.setToDisp.getElements.getTags()
-        for lp in activeLoadPatterns: #Iterate over load patterns.
-            lIter= lp.data().loads.getElementalLoadIter
+        retval= dict()
+        preprocessor=actLP[0].getDomain.getPreprocessor     
+        for lp in actLP:
+            lIter= lp.loads.getElementalLoadIter
             eLoad= lIter.next()
+            eTagsSet= self.setToDisp.getElements.getTags()
             while(eLoad):
                 tags= eLoad.elementTags
                 for i in range(0,len(tags)):
                     eTag= tags[i]
-                    if eTag in eTagsSet: # if element in set.
-                        if(self.component=='axialComponent'):
-                            vComp= eLoad.axialComponent
-                        elif(self.component=='transComponent'):
-                            vComp= eLoad.transComponent
-                        elif(self.component=='transYComponent'):
-                            vComp=eLoad.transYComponent
-                        elif(self.component=='transZComponent'):
-                            vComp=eLoad.transZComponent
-                        elif(self.component=='xyzComponents'):
-                            hasLocalForce= getattr(eLoad, "getVector3dLocalForce", None)
-                            if(hasLocalForce):
-                                vComp= eLoad.getVector3dLocalForce().getModulus()
-                            else: # strain load or alike.
-                                vComp= 0.0
-                        else:
-                            lmsg.error("LinearLoadDiagram :'"+self.component+"' unknown.")
-                        maxV=max(abs(vComp),maxV)
+                    if eTag in eTagsSet:
+                        elem= preprocessor.getElementHandler.getElement(eTag)
+                        dim= elem.getDimension
+                        if(dim==1):
+                            if eTag in retval:
+                                retval[eTag]+=eLoad.getVector3dLocalForce()
+                            else:
+                                retval[eTag]=eLoad.getVector3dLocalForce()
                 eLoad= lIter.next()
-        return maxV
+        return retval
+       
+
+    def dumpElementalLoads(self,actLP,indxDiagram):
+        ''' Iterate over loaded elements dumping its loads into the graphic.
+
+        :param actLP: list of active load patterns.
+        '''
+        preprocessor=actLP[0].getDomain.getPreprocessor
+        if not self.dictActLoadVectors:
+            self.dictActLoadVectors=self.sumElementalLoads(actLP)
+        if(self.component=='axialComponent'):
+            for eTag in self.dictActLoadVectors.keys():
+                elem= preprocessor.getElementHandler.getElement(eTag)
+                self.vDir= elem.getJVector3d(True)
+                axialLoad=self.dictActLoadVectors[eTag].x
+                indxDiagram= self.appendDataToDiagram(elem,indxDiagram,axialLoad,axialLoad)
+        elif(self.component in ['transComponent','transYComponent']):  # transComponent only for 2D models
+            for eTag in self.dictActLoadVectors.keys():
+                elem= preprocessor.getElementHandler.getElement(eTag)
+                self.vDir= elem.getJVector3d(True)
+                transLoad=self.dictActLoadVectors[eTag].y
+                indxDiagram= self.appendDataToDiagram(elem,indxDiagram,transLoad,transLoad)
+        elif(self.component=='transZComponent'):  
+            for eTag in self.dictActLoadVectors.keys():
+                elem= preprocessor.getElementHandler.getElement(eTag)
+                self.vDir= elem.getKVector3d(True)
+                transZLoad=self.dictActLoadVectors[eTag].z
+                indxDiagram= self.appendDataToDiagram(elem,indxDiagram,transZLoad,transZLoad)
+        elif(self.component=='xyzComponents'):
+            for eTag in self.dictActLoadVectors.keys():
+                elem= preprocessor.getElementHandler.getElement(eTag)
+                vI= elem.getIVector3d(True)
+                vJ= elem.getJVector3d(True)
+                vK= elem.getKVector3d(True)
+                localForce=self.dictActLoadVectors[eTag]
+                v= localForce.x*vI+localForce.y*vJ+localForce.z*vK 
+                self.vDir= v.normalized()
+                totLoad= v.getModulus()
+                indxDiagram= self.appendDataToDiagram(elem,indxDiagram,totLoad,totLoad)
+        else:
+            lmsg.error("LinearLoadDiagram :'"+self.component+"' unknown.")
+        return indxDiagram
+ 
+    def getMaxAbsComp(self,preprocessor):
+        '''Return the maximum absolute value of the component.
+        It is used for calculating auto-scale parameter
+        '''
+        activeLoadPatterns= preprocessor.getDomain.getConstraints.getLoadPatterns
+        if(len(activeLoadPatterns)<1):
+            lmsg.warning('No active load patterns.')
+        actLP=[lp.data() for lp in activeLoadPatterns]
+        if not self.dictActLoadVectors:
+            self.dictActLoadVectors=self.sumElementalLoads(actLP)
+        eTags=[tag for tag in self.dictActLoadVectors.keys()]
+        if(self.component=='axialComponent'):
+            retval=max([abs(self.dictActLoadVectors[tag].x) for tag in eTags])
+        elif(self.component in ['transComponent','transYComponent']):  # transComponent only for 2D models
+            retval=max([abs(self.dictActLoadVectors[tag].y) for tag in eTags])
+        elif(self.component=='transZComponent'):  
+            retval=max([abs(self.dictActLoadVectors[tag].z) for tag in eTags])
+        elif(self.component=='xyzComponents'):
+            retval=max([abs(self.dictActLoadVectors[tag].getModulus()) for tag in eTags])
+        else:
+            lmsg.error("LinearLoadDiagram :'"+self.component+"' unknown.")
+        return retval
 
     def dumpLoads(self, preprocessor, indxDiagram):
         ''' Dump loads over elements.'''
@@ -115,8 +135,8 @@ class LinearLoadDiagram(cd.ColoredDiagram):
         if(len(activeLoadPatterns)<1):
             lmsg.warning('No active load patterns.')
         retval= 0
-        for lp in activeLoadPatterns: #Iterate over loaded elements.
-            retval+= self.dumpElementalLoads(preprocessor,lp.data(),indxDiagram)
+        actLP=[lp.data() for lp in activeLoadPatterns]
+        retval= self.dumpElementalLoads(actLP,indxDiagram)
         return retval
 
     def addDiagram(self,preprocessor):
