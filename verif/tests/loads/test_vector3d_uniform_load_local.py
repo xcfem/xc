8925d5bfa65609a12301c54a062a77fbfbe0f0a5
Luis Claudio Pérez Tato <l.pereztato@gmail.com>
2021-02-24 19:53:46 +0100

    Code cosmetics

--- verif/tests/loads/test_vector3d_uniform_load_local.py
+++ verif/tests/loads/test_vector3d_uniform_load_local.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
 # home made test
 # Horizontal cantilever under tension load at its end.
+
+from __future__ import print_function
 
 import xc_base
 import geom
