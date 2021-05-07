70c72b4ff5d6bb87af33423601aa6d60dc74a6bd
Ana <ana.ortega.ort@gmail.com>
2021-05-07 15:05:59 +0200

    Add results in LaTeX to test that analyses a stud in combined tension and shear according to AISC

--- verif/tests/materials/aci/test_aci_anchor_comb_tension_shear.py
+++ verif/tests/materials/aci/test_aci_anchor_comb_tension_shear.py
@@ -132,3 +132,85 @@ if((ratio1<1e-5) and (ratio2<1e-5) and (ratio3<1e-5) and (ratio4<1e-5) and (rati
   print('test '+fname+': ok.')
 else:
   lmsg.error(fname+' ERROR.')
+
+'''
+# Export results to LaTeX
+print('\\documentclass[12pt]{article}')
+print('\\begin{document}')
+
+print('The design of anchors below follows the methodology of the Guide to the Concrete Capacity Design (CCD) Method, reported by ACI Committee 349. \\\\ ')
+#                   DATA
+print('\\paragraph{Data}')
+print('\\begin{center}')
+print('\\begin{tabular}{p{8cm}r}')
+
+# Materials
+print('Anchor diameter & ',round(stud.diam*1e3,0), ' mm \\\\')
+print('Effective embedment depth of anchor & ',round(stud.hef*1e3,0), ' mm \\\\')
+
+print('Concrete $f_c^,$ = & ', round(abs(stud.concrete.fck)*1e-6,1), 'MPa \\\\')
+print('Steel $f_{uta}$ = & ',round(stud.getFuta()*1e-6,1), 'MPa \\\\')
+#Loads
+#print('Critical load combination & ',comb_expr,' \\\\')
+print('Applied factored external tension load N$_{ua}$= & ', round(Nua*1e-3,2), 'kN \\\\')
+print('Applied factored external shear load V$_{ua}$= & ', round(Vua*1e-3,2), 'kN \\\\')
+if stud.cast_in:
+    print('cast in place stud $\\rightarrow\\ k_c$= & 24 \\\\')
+else:
+    print('post-installed stud $\\rightarrow\\ k_c$= & 17 \\\\')
+
+print('\\end{tabular}')
+print('\\end{center}')
+
+
+print('\\paragraph{Nominal strength of the anchor in tension} \\emph{(art. D.5.1.2)} \\\\ ')
+print('$$N_{sa} = nA_{se}f_{uta} = ' ,round(stud.getAnchorArea(),6),'\\times',round(stud.steel.fmaxk()*1e-3,1),' = ',  round(stud.getNominalSteelStrengthTension()*1e-3,1) , ' kN $$')
+ 
+
+print('\\paragraph{Concrete breakout failure in tension} \\emph{(art. D.5.2.)}\\\\')
+
+print('Basic concrete breakout strength of a single anchor in tension in cracked concrete: ')
+
+print('$$N_b=k_c \\sqrt{f_c^,} h_{h_{ef}^{1.5}} = ',round(stud.getBasicConcreteBreakoutStrengthTension()*1e-3,1),' kN $$')
+cracking=True
+print('Nominal concrete breakout strength of a single anchor in tension: ')
+print('$$N_{cb} = \\frac{A_{Nc}}{A_{Nco}} \\Psi_{ed,N}\\Psi_{C,N}\\Psi_{cp} N_b = \\frac{',stud.getANc(),'}{',stud.getANco(),'}\\cdot',  stud.getFactorEdgeN(),'\\cdot',stud.getFactorCrackingN(cracking),'\\cdot',stud.getFactorModifN(cracking),'\\cdot',round(stud.getBasicConcreteBreakoutStrengthTension()*1e-3,1),'= ',round(stud.getConcrBreakoutStrengthTension(cracking)*1e-3,1),' kN     $$')
+
+print('\\paragraph{Pullout stregth of stud to check head of the stud} \\emph{(article D.5.3)}')
+print('$$A_{bearing} = ',Abearing,' m^2 $$')
+print('$$N_{pn} = ',round(stud.getPulloutStrengthTension(Abearing)*1e-3,1),' kN $$')
+print('\\paragraph{Check ductility in tension}')
+print('$$N_{dd}=', round(Ndd*1e-3,1), 'kN$$  $$N_{ua}=', round(Nua*1e-3,1), ' kN $$')
+if Ndd >= Nua:
+    print('$$N_{dd} \\ge N_{ua} \\rightarrow\\ ductility\\ OK$$')
+else:
+    print('$$N_{dd} \\lt N_{ua} \\rightarrow\\ NO\\ ductility$$')
+
+print('\\paragraph{Nominal strength of the anchor in shear} \\emph{(art. D.6.1.2)}')
+print('$$V_{sa} = 0.6 A_{se} f_{uta} = ', round(stud.getSteelStrengthShear()*1e-3,1),  ' kN $$')
+print('\\paragraph{Concrete breakout failure in shear}\\emph{(art. D.6.2.)}')
+print('$$V_{cb}= \\frac{A_{Vc}}{A_{Vco}}\\Psi_{ed,V}\\Psi_{c,V}V_b = ',round(Vcb*1e-3,1),' kN $$')
+print('\\paragraph{Concrete pryout strength of anchor in shear} \\emph{(art. D.6.3.)}')
+print('$$ V_{cp} = k_{cp} N_{cb} = ', round(Vcp*1e-3,1),' kN $$')
+print('\\paragraph{Check ductility in shear}')
+print('$$V_{dd}=', round(Vdd*1e-3,1), ' kN$$  $$V_{ua}=',round(Vua*1e-3,1), ' kN$$')
+if Vdd >= Vua:
+    print('$$ V_{dd} \\ge V_{ua} \\rightarrow\\ ductility\\ OK $$')
+else:
+    print('$$ V_{dd} \\lt V_{ua} \\rightarrow\\ NO\\ ductility $$')
+print('\\paragraph{Check design strength of stud in shear}')
+print('$$V_{nd}=', round(Vnd*1e-3,1), ' kN$$  $$ V_{ua}=',round(Vua*1e-3,1), ' kN$$')
+if Vnd >= Vua:
+    print('$$V_{nd} \\ge V_{ua} \\rightarrow\\ design\\ strength\\ checking\\ OK$$')
+else:
+    print('$$V_{nd} \\lt V_{ua} \\rightarrow\\ change\\ stud\\ dimensions$$')
+if Vua <= 0.2*Vnd:
+    print('$$V_{ua} \\le 0.2V_{nd} \\rightarrow\\ full\\ strength\\ in\\ tension\\ is\\ permitted$$')
+if Nua <= 0.2*Nnd:
+    print('$$N_{ua} \\le 0.2N_{nd} \\rightarrow\\ full strength in shear is permitted$$')
+if Vua > 0.2*Vnd and Nua > 0.2*Nnd:
+    if Nua/Nnd + Vua/Vnd <= 1.2:
+        print('$$N_{ua}/N_{nd} + V_{ua}/V_{nd} = ',round(Nua/Nnd + Vua/Vnd,2), ' \\le 1.2$$')
+
+print('\\end{document}')
+'''
