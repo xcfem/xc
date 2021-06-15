7d229a3b25709ef8e0f3c113e72397833e25d802
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2021-06-15 09:29:43 +0200

    Add missing diameters

--- verif/tests/materials/tie_bars/test_tie_bars_01.py
+++ verif/tests/materials/tie_bars/test_tie_bars_01.py
@@ -10,10 +10,10 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
-tieBars= [tie_bars.M12, tie_bars.M16, tie_bars.M20, tie_bars.M24, tie_bars.M30, tie_bars.M36, tie_bars.M42, tie_bars.M48, tie_bars.M56, tie_bars.M64]
-yieldLoads= [30e3, 82e3, 127e3, 184e3, 292e3, 425e3, 583e3, 766e3, 1056e3, 1392e3]
+tieBars= [tie_bars.M12, tie_bars.M16, tie_bars.M20, tie_bars.M24, tie_bars.M30, tie_bars.M36, tie_bars.M42, tie_bars.M48, tie_bars.M56, tie_bars.M64, tie_bars.M76, tie_bars.M90, tie_bars.M100]
+yieldLoads= [30e3, 82e3, 127e3, 184e3, 292e3, 425e3, 583e3, 766e3, 1056e3, 1392e3,1999e3, 2879e3, 3605e3]
 
-ultimateLoads= [51e3, 104e3, 162e3, 233e3, 370e3, 539e3, 740e3, 972e3, 1340e3, 1766e3]
+ultimateLoads= [51e3, 104e3, 162e3, 233e3, 370e3, 539e3, 740e3, 972e3, 1340e3, 1766e3, 2537e3, 3654e3, 4575e3]
 
 ratio= 0.0
 for b, yLRef, uLRef in zip(tieBars, yieldLoads, ultimateLoads):
