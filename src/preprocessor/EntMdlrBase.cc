3de9ffee21f802cd8c51fd0fb0f8695f458b499e
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2021-11-29 20:41:47 +0100

    Code cosmetics

--- src/preprocessor/EntMdlrBase.cc
+++ src/preprocessor/EntMdlrBase.cc
@@ -96,14 +96,13 @@ size_t XC::EntMdlrBase::getTag(void) const
     try
       { retval= boost::lexical_cast<size_t>(tmp); }
     catch(boost::bad_lexical_cast &)
-        {
-	  retval= reinterpret_cast<size_t>(this); // and hope for the best.
-	  std::cerr << getClassName() << "::" << __FUNCTION__
-	            << "; ERROR: could not extract entity tag from its name: '"
-	            << getName() << "'. Weird results could follow."
-		    << " Returning: " << retval << std::endl;
-        }
-    
+      {
+	retval= reinterpret_cast<size_t>(this); // and hope for the best.
+	std::cerr << getClassName() << "::" << __FUNCTION__
+		  << "; ERROR: could not extract entity tag from its name: '"
+		  << getName() << "'. Weird results could follow."
+		  << " Returning: " << retval << std::endl;
+      }    
     return retval;
   }
 
