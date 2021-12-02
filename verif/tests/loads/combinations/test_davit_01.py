042625de1401960421b6e25b88f3fbcbf0c24dcc
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2021-12-02 14:55:45 +0100

    Move combination computation tests into the load folder

--- verif/tests/loads/combinations/test_davit_01.py
+++ verif/tests/loads/combinations/test_davit_01.py
@@ -2,6 +2,13 @@
 from __future__ import print_function
 from __future__ import division
 
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2014, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import yaml
 import xc_base
 import geom
 import xc
@@ -9,18 +16,12 @@ from solution import predefined_solutions
 from model import predefined_spaces
 from materials import typical_materials
 
-__author__= "Luis C. Pérez Tato (LCPT)"
-__copyright__= "Copyright 2014, LCPT"
-__license__= "GPL"
-__version__= "3.0"
-__email__= "l.pereztato@gmail.com"
-
 b= 0.4
 h= 0.8
 A= b*h
 E= 200000*9.81/1e-4 # Estimated concrete elastic modulus.
 nu= 0.3 # Poisson's ratio
-G= E/(2*(1+nu)) # Shear modulus
+G= E/(2.0*(1+nu)) # Shear modulus
 Iy= (1/12.0*h*b**3) # Cross section moment of inertia (m4)
 Iz= (1/12.0*b*h**3) # Cross section moment of inertia (m4)
 J= 0.721e-8 # Cross section torsion constant (m4)
@@ -41,11 +42,11 @@ feProblem= xc.FEProblem()
 preprocessor=  feProblem.getPreprocessor
 nodes= preprocessor.getNodeHandler
 modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
-nod1= nodes.newNodeXYZ(2.0,0.0,0.0)
-nod2= nodes.newNodeXYZ(2.0,0.0,4.0)
-nod3= nodes.newNodeXYZ(0.0,0.0,4.0)
-nod4= nodes.newNodeXYZ(5.0,0.0,4.0)
-
+nodes.defaultTag= 1 #First node number.
+nod= nodes.newNodeXYZ(2.0,0.0,0.0)
+nod= nodes.newNodeXYZ(2.0,0.0,4.0)
+nod= nodes.newNodeXYZ(0.0,0.0,4.0)
+nod= nodes.newNodeXYZ(5.0,0.0,4.0)
 
 lin= modelSpace.newLinearCrdTransf("lin",xc.Vector([0,1,0]))
     
@@ -57,17 +58,18 @@ scc= typical_materials.defElasticSection3d(preprocessor, "scc",A,E,G,Iz,Iy,J)
 elements= preprocessor.getElementHandler
 elements.defaultTransformation= lin.name
 elements.defaultMaterial= scc.name
-#  sintaxis: ElasticBeam3d[<tag>] 
-beam1= elements.newElement("ElasticBeam3d",xc.ID([nod1.tag,nod2.tag]))
+elements.defaultTag= 1
+#  sintaxis: ElasticBeam3d[<tag>]
+beam1= elements.newElement("ElasticBeam3d",xc.ID([1,2]))
 beam1.rho= densHorm*A
-beam2= elements.newElement("ElasticBeam3d",xc.ID([nod3.tag,nod2.tag])) 
+beam2= elements.newElement("ElasticBeam3d",xc.ID([3,2])) 
 beam2.rho= densHorm*A
-beam3= elements.newElement("ElasticBeam3d",xc.ID([nod2.tag,nod4.tag])) 
+beam3= elements.newElement("ElasticBeam3d",xc.ID([2,4])) 
 beam3.rho= densHorm*A
 
 # Constraints
 
-modelSpace.fixNode000_000(nod1.tag)
+modelSpace.fixNode000_000(1)
 
 # Loads definition
 loadHandler= preprocessor.getLoadHandler
@@ -97,6 +99,7 @@ beam2.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-22e3]))
 beam3.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-22e3]))
 
 lPatterns.currentLoadPattern= "SC"
+
 beam3.vector3dPointByRelDistLoadGlobal(0.99,xc.Vector([0.0,0.0,-100e3]))
 
 lPatterns.currentLoadPattern= "VT"
@@ -107,84 +110,64 @@ beam2.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-10e3]))
 beam3.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-10e3]))
 
 # Combinaciones
-combs= loadHandler.getLoadCombinations
+## Read load combinations.
 import os
 pth= os.path.dirname(__file__)
 #print("pth= ", pth)
 if(not pth):
-  pth= "."
-exec(open(pth+"/../aux/def_hip_elu.py").read())
+    pth= "."
+fName= pth+"/../../aux/def_hip_elu.yaml"
+with open(fName) as file:
+    try:
+        combData= yaml.safe_load(file)
+    except yaml.YAMLError as exception:
+        print(exception)
+
+combs= loadHandler.getLoadCombinations
+for combName in combData:
+    combExpr= combData[combName]
+    comb= combs.newLoadCombination(combName,combExpr)
+    
+NMin1= 6.023e23
+NMin2= 6.023e23
+
+def procesResultVerif(nmbComb):
+    elements= preprocessor.getElementHandler
+    elem1= elements.getElement(1)
+    elem1.getResistingForce()
+    N1= elem1.getN1
+    global NMin1
+    NMin1=min(NMin1,N1)
+    global NMin2
+    NMin2=min(NMin2,elem1.getN2)
 
 
+# Solve.
+solProc= predefined_solutions.SimpleStaticLinear(feProblem)
 
-def solveStaticLinearComb(comb):
-  preprocessor.resetLoadCase()
-  previa= comb.getCombPrevia()
-  if(previa!=None):
-    previousName= previa.getName
-    tagPrevia= previa.tag
-    ''' 
-    print("previousName= ",previousName)
-    print("tag= ",comb.tag)
-    print("tagPrevia= ",tagPrevia)
-    print("descomp previa= ",getComponentsCombPrevia)
-    print("resto sobre previa= ",getComponentsRestoSobrePrevia)
-    '''
-    tagSave= tagPrevia*100
-    if(tagPrevia>0):
-      db.restore(tagSave)
+for key in combs.getKeys():
+  analOk= solProc.solveComb(key)
+  procesResultVerif(key)
 
-    comb.addToDomain()
-    analysis= predefined_solutions.simple_static_linear(feProblem)
-    result= analysis.analyze(1)
-    comb.removeFromDomain()
-
-
-def procesResultVerif(comb):
-  tagSave= comb.tag*100
-  db.save(tagSave)
-  elements= preprocessor.getElementHandler
-  beam1.getResistingForce()
-  global NMin1
-  NMin1=min(NMin1,beam1.getN1)
-  global NMin2
-  NMin2=min(NMin2,beam1.getN2)
-  ''' 
-    print("tagComb= ",comb.tag)
-    print("nmbComb= ",comb.getName)
-    print("N1= ",(beam1.getN1/1e3))
-    print("N2= ",(beam1.getN2/1e3))
-  '''
-
-
-NMin1= 1e9
-NMin2= 1e9
-
-import os
-os.system("rm -r -f /tmp/test_pescante_02.db")
-db= feProblem.newDatabase("BerkeleyDB","/tmp/test_pescante_02.db")
-
-previousName="" 
-tagPrevia= 0
-for c in combs.getKeys():
-  solveStaticLinearComb(combs[c])
-  procesResultVerif(combs[c])
-          
-ratio1= ((NMin1+440.7e3)/440.7e3)
-ratio2= ((NMin2+397.5e3)/397.5e3)
+NMin1Teor= 440.7e3
+NMin2Teor= 397.5e3
+ratio1= ((NMin1+NMin1Teor)/NMin1Teor)
+ratio2= ((NMin2+NMin2Teor)/NMin2Teor)
 
 ''' 
-print("NMin1= ",NMin1)
+print("NMin1= ",NMin1/1e3)
+print("NMin1Teor= ",NMin1Teor/1e3)
 print("ratio1= ",ratio1)
-print("NMin2= ",NMin2)
+print("NMin2= ",NMin2/1e3)
+print("NMin2Teor= ",NMin2Teor/1e3)
 print("ratio2= ",ratio2)
-'''
+ '''
 
 
+import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
 if (abs(ratio1)<1e-8) & (abs(ratio2)<1e-8):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
-os.system("rm -rf /tmp/test_pescante_02.db") # Your garbage you clean it
--- verif/tests/loads/combinations/test_davit_02.py
+++ verif/tests/loads/combinations/test_davit_02.py
@@ -2,6 +2,14 @@
 from __future__ import print_function
 from __future__ import division
 
+import yaml
+import xc_base
+import geom
+import xc
+from solution import predefined_solutions
+from model import predefined_spaces
+from materials import typical_materials
+
 __author__= "Luis C. Pérez Tato (LCPT)"
 __copyright__= "Copyright 2014, LCPT"
 __license__= "GPL"
@@ -13,18 +21,12 @@ h= 0.8
 A= b*h
 E= 200000*9.81/1e-4 # Estimated concrete elastic modulus.
 nu= 0.3 # Poisson's ratio
-G= E/(2.0*(1+nu)) # Shear modulus
+G= E/(2*(1+nu)) # Shear modulus
 Iy= (1/12.0*h*b**3) # Cross section moment of inertia (m4)
 Iz= (1/12.0*b*h**3) # Cross section moment of inertia (m4)
 J= 0.721e-8 # Cross section torsion constant (m4)
 densHorm= 2500 # Reinforced concrete density.
 
-import xc_base
-import geom
-import xc
-from solution import predefined_solutions
-from model import predefined_spaces
-from materials import typical_materials
 
 #
 #     3   2     4
@@ -40,11 +42,11 @@ feProblem= xc.FEProblem()
 preprocessor=  feProblem.getPreprocessor
 nodes= preprocessor.getNodeHandler
 modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
-nodes.defaultTag= 1 #First node number.
-nod= nodes.newNodeXYZ(2.0,0.0,0.0)
-nod= nodes.newNodeXYZ(2.0,0.0,4.0)
-nod= nodes.newNodeXYZ(0.0,0.0,4.0)
-nod= nodes.newNodeXYZ(5.0,0.0,4.0)
+nod1= nodes.newNodeXYZ(2.0,0.0,0.0)
+nod2= nodes.newNodeXYZ(2.0,0.0,4.0)
+nod3= nodes.newNodeXYZ(0.0,0.0,4.0)
+nod4= nodes.newNodeXYZ(5.0,0.0,4.0)
+
 
 lin= modelSpace.newLinearCrdTransf("lin",xc.Vector([0,1,0]))
     
@@ -56,18 +58,17 @@ scc= typical_materials.defElasticSection3d(preprocessor, "scc",A,E,G,Iz,Iy,J)
 elements= preprocessor.getElementHandler
 elements.defaultTransformation= lin.name
 elements.defaultMaterial= scc.name
-elements.defaultTag= 1
-#  sintaxis: ElasticBeam3d[<tag>]
-beam1= elements.newElement("ElasticBeam3d",xc.ID([1,2]))
+#  sintaxis: ElasticBeam3d[<tag>] 
+beam1= elements.newElement("ElasticBeam3d",xc.ID([nod1.tag,nod2.tag]))
 beam1.rho= densHorm*A
-beam2= elements.newElement("ElasticBeam3d",xc.ID([3,2])) 
+beam2= elements.newElement("ElasticBeam3d",xc.ID([nod3.tag,nod2.tag])) 
 beam2.rho= densHorm*A
-beam3= elements.newElement("ElasticBeam3d",xc.ID([2,4])) 
+beam3= elements.newElement("ElasticBeam3d",xc.ID([nod2.tag,nod4.tag])) 
 beam3.rho= densHorm*A
 
 # Constraints
 
-modelSpace.fixNode000_000(1)
+modelSpace.fixNode000_000(nod1.tag)
 
 # Loads definition
 loadHandler= preprocessor.getLoadHandler
@@ -97,7 +98,6 @@ beam2.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-22e3]))
 beam3.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-22e3]))
 
 lPatterns.currentLoadPattern= "SC"
-
 beam3.vector3dPointByRelDistLoadGlobal(0.99,xc.Vector([0.0,0.0,-100e3]))
 
 lPatterns.currentLoadPattern= "VT"
@@ -108,58 +108,92 @@ beam2.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-10e3]))
 beam3.vector3dUniformLoadGlobal(xc.Vector([0.0,0.0,-10e3]))
 
 # Combinaciones
-combs= loadHandler.getLoadCombinations
 import os
 pth= os.path.dirname(__file__)
 #print("pth= ", pth)
 if(not pth):
-  pth= "."
-exec(open(pth+"/../aux/def_hip_elu.py").read())
+    pth= "."
+fName= pth+"/../../aux/def_hip_elu.yaml"
+with open(fName) as file:
+    try:
+        combData= yaml.safe_load(file)
+    except yaml.YAMLError as exception:
+        print(exception)
+        
+combs= loadHandler.getLoadCombinations
+for combName in combData:
+    combExpr= combData[combName]
+    comb= combs.newLoadCombination(combName,combExpr)
 
-NMin1= 6.023e23
-NMin2= 6.023e23
 
-def procesResultVerif(nmbComb):
+def solveStaticLinearComb(comb):
+  preprocessor.resetLoadCase()
+  previa= comb.getCombPrevia()
+  if(previa!=None):
+    previousName= previa.getName
+    tagPrevia= previa.tag
+    ''' 
+    print("previousName= ",previousName)
+    print("tag= ",comb.tag)
+    print("tagPrevia= ",tagPrevia)
+    print("descomp previa= ",getComponentsCombPrevia)
+    print("resto sobre previa= ",getComponentsRestoSobrePrevia)
+    '''
+    tagSave= tagPrevia*100
+    if(tagPrevia>0):
+      db.restore(tagSave)
+
+    comb.addToDomain()
+    analysis= predefined_solutions.simple_static_linear(feProblem)
+    result= analysis.analyze(1)
+    comb.removeFromDomain()
+
+
+def procesResultVerif(comb):
+  tagSave= comb.tag*100
+  db.save(tagSave)
   elements= preprocessor.getElementHandler
-  elem1= elements.getElement(1)
-  elem1.getResistingForce()
-  N1= elem1.getN1
+  beam1.getResistingForce()
   global NMin1
-  NMin1=min(NMin1,N1)
+  NMin1=min(NMin1,beam1.getN1)
   global NMin2
-  NMin2=min(NMin2,elem1.getN2)
-
+  NMin2=min(NMin2,beam1.getN2)
+  ''' 
+    print("tagComb= ",comb.tag)
+    print("nmbComb= ",comb.getName)
+    print("N1= ",(beam1.getN1/1e3))
+    print("N2= ",(beam1.getN2/1e3))
+  '''
 
 
 NMin1= 1e9
 NMin2= 1e9
 
-# Solve.
-solProc= predefined_solutions.SimpleStaticLinear(feProblem)
+import os
+os.system("rm -r -f /tmp/test_pescante_02.db")
+db= feProblem.newDatabase("BerkeleyDB","/tmp/test_pescante_02.db")
 
-for key in combs.getKeys():
-  analOk= solProc.solveComb(key)
-  procesResultVerif(key)
-
-NMin1Teor= 440.7e3
-NMin2Teor= 397.5e3
-ratio1= ((NMin1+NMin1Teor)/NMin1Teor)
-ratio2= ((NMin2+NMin2Teor)/NMin2Teor)
+previousName="" 
+tagPrevia= 0
+for c in combs.getKeys():
+  solveStaticLinearComb(combs[c])
+  procesResultVerif(combs[c])
+          
+ratio1= ((NMin1+440.7e3)/440.7e3)
+ratio2= ((NMin2+397.5e3)/397.5e3)
 
 ''' 
-print("NMin1= ",NMin1/1e3)
-print("NMin1Teor= ",NMin1Teor/1e3)
+print("NMin1= ",NMin1)
 print("ratio1= ",ratio1)
-print("NMin2= ",NMin2/1e3)
-print("NMin2Teor= ",NMin2Teor/1e3)
+print("NMin2= ",NMin2)
 print("ratio2= ",ratio2)
- '''
+'''
 
 
-import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
 if (abs(ratio1)<1e-8) & (abs(ratio2)<1e-8):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
+os.system("rm -rf /tmp/test_pescante_02.db") # Your garbage you clean it
