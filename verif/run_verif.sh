630aa2c5677c39491d8c4830fe4b08107a213234
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2021-12-14 10:43:59 +0100

    Add copyright notice

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+# 
+#   Copyright (C)  Luis C. Pérez Tato
+# 
+#   XC utils is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or 
+#   (at your option) any later version.
+# 
+#   This software is distributed in the hope that it will be useful, but 
+#   WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.  
+# 
+#  You should have received a copy of the GNU General Public License 
+#  along with this program.
+#  If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------------
+
 ERT="\\033[1;32m"
 NORMAL="\\033[0;39m"
 ROUGE="\\033[1;31m"
