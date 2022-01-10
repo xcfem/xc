1ccaa09bd64d88a1b974b740f5d544419ce5bedd
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-01-10 18:55:47 +0100

    Use JSON files to store steel shapes properties

--- python_modules/materials/sections/structural_shapes/aisc_metric_shapes.py
+++ python_modules/materials/sections/structural_shapes/aisc_metric_shapes.py
@@ -4,6 +4,12 @@
 from __future__ import print_function
 from __future__ import division
 
+__author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
+__copyright__= "Copyright 2016, LCPT and AOO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
 # Section axis:
 
 #    AISC            XC
