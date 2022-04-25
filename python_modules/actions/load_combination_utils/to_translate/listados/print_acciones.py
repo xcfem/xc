b9271217fad143a8cc677a953cf16e1d05f8daf9
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-04-25 20:35:17 +0200

    Translate to Python (work in progress)

--- python_modules/actions/load_combination_utils/to_translate/listados/print_acciones.py
+++ python_modules/actions/load_combination_utils/to_translate/listados/print_acciones.py
@@ -1,25 +1,20 @@
 # -*- coding: utf-8 -*-
-def listaAccionesFamilia(nmbArchivo):
-  {
-    nmbArchivo.write("\\hline\n")
-    nmbArchivo.write("\\multicolumn{6}{|c|}{",getName,"}\\\\\n")
-    nmbArchivo.write("\\hline\n")
-    nmbArchivo.write("Name & Description & $\\psi_0$ & $\\psi_1$ & $\\psi_2$ & Incompatibles \\\\\n")
-    nmbArchivo.write("\\hline\n")
-    \for_each_accion
-      {
-        nmbArchivo.write(getName)
-        nmbArchivo.write(" & ",getDescripcion)
-        nmbArchivo.write(" & ",psi_0)
-        nmbArchivo.write(" & ",psi_1)
-        nmbArchivo.write(" & ",psi_2)
-        nmbArchivo.write(" & ",incompatibles,"\\\\\n")
-      }
-  }
+def listaAccionesFamilia(actionFamily, outputFile):
+    outputFile.write("\\hline\n")
+    outputFile.write("\\multicolumn{6}{|c|}{"+actionFamily.name+"}\\\\\n")
+    outputFile.write("\\hline\n")
+    outputFile.write("Name & Description & $\\psi_0$ & $\\psi_1$ & $\\psi_2$ & Incompatibles \\\\\n")
+    outputFile.write("\\hline\n")
+    for action in actionFamily:
+        outputFile.write(action.getName)
+        outputFile.write(" & ",action.getDescripcion)
+        outputFile.write(" & ",action.psi_0)
+        outputFile.write(" & ",action.psi_1)
+        outputFile.write(" & ",action.psi_2)
+        outputFile.write(" & ",action.incompatibles,"\\\\\n")
 
-def listaAcciones(pathArchivo):
-  {
-    outAcciones.open(pathArchivo,"w")
+def listaAcciones(actionFamilies, filePath):
+    outAcciones= open(filePath,"w")
     outAcciones.write("\\begin{center}\n")
     outAcciones.write("\\begin{small}\n")
     outAcciones.write("\\begin{longtable}{|l|p{4cm}|c|c|c|l|}\n")
@@ -29,21 +24,17 @@ def listaAcciones(pathArchivo):
     outAcciones.write("\\hline \\hline\n")
     outAcciones.write("\\endlastfoot\n")
 
-    \for_each
-      {
-        if(getSize>0)
-          listaAccionesFamilia("outAcciones"){}
-      }
+    for actionFamily in actionFamilies:
+        if(len(actionFamily)>0):
+            listaAccionesFamilia(actionFamily, outAcciones)
 
     outAcciones.write("\\end{longtable}\n")
     outAcciones.write("\\end{small}\n")
     outAcciones.write("\\end{center}\n")
     outAcciones.close()
-  }
 
-def listaPonderacion(pathArchivo,elimite,accidental):
-  {
-    outPonderacion.open(pathArchivo,"w")
+def listaPonderacion(actions, filePath,elimite,accidental):
+    outPonderacion= open(filePath,"w")
     outPonderacion.write("\\begin{center}\n")
     outPonderacion.write("\\begin{small}\n")
     outPonderacion.write("\\begin{tabular}{|l|l|l|l|}\n")
@@ -51,34 +42,17 @@ def listaPonderacion(pathArchivo,elimite,accidental):
     outPonderacion.write("Familia & Acciones & \\multicolumn{2}{|c|}{Efecto} \\\\\n")
     outPonderacion.write("        &          & favorable & desfavorable \\\\\n")
     outPonderacion.write("\\hline\n")
-    \for_each
-      {
-        if(num_acciones>0)
-            \then
-              {
-                outPonderacion.write(getName)
-                outPonderacion.write(" & ",lista_acciones)
-                \gammaf{\elimite
-                  {
-                    if(accidental==0)
-                        \then
-                          {
-                            outPonderacion.write(" & ",favorable)
-                            outPonderacion.write(" & ",desfavorable,"\\\\\n")
-                          }
-                        else:
-                          {
-                            outPonderacion.write(" & ",favorable_accidental)
-                            outPonderacion.write(" & ",desfavorable_accidental,"\\\\\n")
-                          }
-                      }
-                  }}
-              }
-          }
-      }
+    for action in actions:
+        outPonderacion.write(action.name)
+        outPonderacion.write(" & ",lista_acciones)
+        if(accidental==0):
+            outPonderacion.write(" & ",favorable)
+            outPonderacion.write(" & ",desfavorable,"\\\\\n")
+        else:
+            outPonderacion.write(" & ",favorable_accidental)
+            outPonderacion.write(" & ",desfavorable_accidental,"\\\\\n")
     outPonderacion.write("\\hline\n")
     outPonderacion.write("\\end{tabular}\n")
     outPonderacion.write("\\end{small}\n")
     outPonderacion.write("\\end{center}\n")
     outPonderacion.close()
-  }
--- python_modules/actions/load_combination_utils/to_translate/listados/print_combinaciones.py
+++ python_modules/actions/load_combination_utils/to_translate/listados/print_combinaciones.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-def printComb(pathArchSalida):
-  {
-    arch_salida_ltx.open(pathArchSalida,"w")
+def printComb(combs, outputPath):
+    arch_salida_ltx= open(outputPath,"w")
     arch_salida_ltx.write("\\begin{center}\n")
     arch_salida_ltx.write("\\begin{small}\n")
     arch_salida_ltx.write("\\begin{longtable}{|c|l|}\n")
@@ -21,56 +20,39 @@ def printComb(pathArchSalida):
     arch_salida_ltx.write("\\endfoot\n")
     arch_salida_ltx.write("\\hline \\hline\n")
     arch_salida_ltx.write("\\endlastfoot\n")
-    \expr{cont= 1}
-    \for_each
-      {
-        arch_salida_ltx.write(cont," & ",ascii2ltx(getExpandedName),"\\\\\n")
-        \expr{cont= tonum(cont+1)}
-      }
+    for cont, comb in enumerate(combs):
+        arch_salida_ltx.write(cont," & ",ascii2ltx(comb.getExpandedName),"\\\\\n")
     arch_salida_ltx.write("\\end{longtable}\n")
     arch_salida_ltx.write("\\end{small}\n")
     arch_salida_ltx.write("\\end{center}\n")
     arch_salida_ltx.close()
-  }
 
-def printCombEluSitPersistentes(pathArchSalida):
-  '''Write the combinations that correspond to ultimate limit states in
-   persistent or transient situations.'''
-  {
-    \combinations{ \comb_elu_persistentes{ \printComb(pathArchSalida){} } }
-  }
+def printCombEluSitPersistentes(outputPath):
+    '''Write the combinations that correspond to ultimate limit states in
+       persistent or transient situations.'''
+    printComb(combinations.comb_elu_persistentes, outputPath)
 
-def printCombEluSitAccidentales(pathArchSalida):
-  '''Write the combinations that correspond to ultimate limit states in
-   accidental situations.'''
-  {
-    \combinations{ \comb_elu_accidentales{ \printComb(pathArchSalida){} } }
-  }
+def printCombEluSitAccidentales(outputPath):
+    '''Write the combinations that correspond to ultimate limit states in
+       accidental situations.'''
+    printComb(combinations.comb_elu_accidentales, outputPath)
 
-def printCombEluSitSismicas(pathArchSalida):
-  '''Write the combinations that correspond to ultimate limit states in
-   seismic situations.'''
-  {
-    \combinations{ \comb_elu_sismicas{ \printComb(pathArchSalida){} } }
-  }
+def printCombEluSitSismicas(outputPath):
+    '''Write the combinations that correspond to ultimate limit states in
+       seismic situations.'''
+    printComb(combinations.comb_elu_sismicas, outputPath)
 
-def printCombElsSitPocoFrecuentes(pathArchSalida):
-  '''Write the combinations that correspond to serviceability limit states in
-   rare situations.'''
-  {
-    \combinations{ \comb_els_poco_frecuentes{ \printComb(pathArchSalida){} } }
-  }
+def printCombElsSitPocoFrecuentes(outputPath):
+    '''Write the combinations that correspond to serviceability limit states in
+     rare situations.'''
+    printComb(combinations.comb_els_poco_frecuentes, outputPath)
 
-def printCombElsSitFrecuentes(pathArchSalida):
-  '''Write the combinations that correspond to serviceability limit states in
-   frequent situations.'''
-  {
-    \combinations{ \comb_els_frecuentes{ \printComb(pathArchSalida){} } }
-  }
+def printCombElsSitFrecuentes(outputPath):
+    '''Write the combinations that correspond to serviceability limit states in
+       frequent situations.'''
+    printComb(combinations.comb_els_frecuentes, outputPath)
 
-def printCombElsSitCuasiPermanentes(pathArchSalida):
-  '''Write the combinations that correspond to serviceability limit states in
-   quasi-permanent situations.'''
-  {
-    \combinations{ \comb_els_cuasi_permanentes{ \printComb(pathArchSalida){} } }
-  }
+def printCombElsSitCuasiPermanentes(outputPath):
+    '''Write the combinations that correspond to serviceability limit states in
+       quasi-permanent situations.'''
+    printComb(combinations.comb_els_cuasi_permanentes, outputPath)
