8ea730a409598c14c9f91835df41ef4969bd212b
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-05-02 18:54:32 +0200

    Translate code to Python

--- python_modules/actions/load_combination_utils/to_translate/listados/trata_comb_els.py
+++ python_modules/actions/load_combination_utils/to_translate/listados/trata_comb_els.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-execfile("comb_acciones/listados/trata_comb_elsf.xci")
-execfile("comb_acciones/listados/trata_comb_elspf.xci")
-execfile("comb_acciones/listados/trata_comb_elscp.xci")
+execfile("comb_acciones/listados/trata_comb_elsf.py")
+execfile("comb_acciones/listados/trata_comb_elspf.py")
+execfile("comb_acciones/listados/trata_comb_elscp.py")
