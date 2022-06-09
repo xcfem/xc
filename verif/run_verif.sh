761d69ed250d0786b55c47c58f53f5763a9d7b7d
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-06-09 13:12:33 +0200

    Add verification test

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -849,6 +849,7 @@ echo "$BLEU" "    Steel relaxation determination tests (EHE-08)." "$NORMAL"
 python tests/materials/ehe/test_steel_relaxation_01.py
 
 echo "$BLEU" "    EC2 tests." "$NORMAL"
+python tests/materials/ec2/test_anchorage_length_01.py
 python tests/materials/ec2/test_EC2Concrete.py
 python tests/materials/ec2/test_EC2creep.py
 python tests/materials/ec2/test_EC2_2004_shrinkage.py
