ffd68619ff2c82b429a6b1546e2d9ae4b37f33ac
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-06-24 13:16:59 +0200

    Add trivial quad regions tests

---
+++ verif/tests/materials/xc_materials/sections/section_geom/regions/quad_sect_region_test_01.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+''' Trivial test of a rectangular quad region.'''
+from __future__ import print_function
+from __future__ import division
+
+import math
+import xc
+from materials import typical_materials
+
+feProblem= xc.FEProblem()
+preprocessor=  feProblem.getPreprocessor
+
+# Create the simplest quad region.
+testMat= typical_materials.defElasticMaterial(preprocessor, "testMat",1e6)
+coordinates= xc.Matrix([[0,0], [1,0], [1,1], [0,1]])
+quadSectRegion= xc.QuadSectRegion(testMat, 1, 1, coordinates)
+
+# Compute and test some derived values.
+pMin= quadSectRegion.pMin
+pMax= quadSectRegion.pMax
+refModulus= math.sqrt(2)
+ratio1= abs((pMax-pMin).getModulus()-refModulus)/refModulus
+area= quadSectRegion.getArea()
+ratio2= abs(area-1)
+
+'''
+print('pMin= ', pMin)
+print('pMax= ', pMax)
+print('ratio1= ', ratio1)
+print('area= ', area)
+print('ratio2= ', ratio2)
+'''
+
+from misc_utils import log_messages as lmsg
+import os
+fname= os.path.basename(__file__)
+if (abs(ratio1)<1e-12) and (abs(ratio2)<1e-12):
+    print('test '+fname+': ok.')
+else:
+    lmsg.error(fname+' ERROR.')
+
---
+++ verif/tests/materials/xc_materials/sections/section_geom/regions/readme.md
@@ -0,0 +1,4 @@
+# Region classes verification tests
+
+Test of the classes uses to define material regions in member cross-sections.
+
