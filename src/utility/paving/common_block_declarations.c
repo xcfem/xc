55e42c2f307ff050c95f7f31914724dee8711329
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-09-15 18:53:47 +0200

    Make XC compile under Ubuntu 22.04

---
+++ src/utility/paving/common_block_declarations.c
@@ -0,0 +1,55 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "paving.h"
+#include "common_block_declarations.h"
+
+struct struct_status status_;
+struct struct_device device_;
+struct struct_color color_;
+struct struct_text text_;
+struct struct_vectrc vectrc_;
+struct struct_font font_;
+struct struct_graph graph_;
+struct struct_mappar mappar_;
+struct struct_storag storag_;
+struct struct_pltsty pltsty_;
+struct struct_map map_;
+struct struct_psave psave_;
+struct struct_timing timing_;
+struct struct_grpcoc grpcoc_;
+struct struct_grpcom grpcom_;
+struct struct_dbnums dbnums_;
+struct struct_d3nums d3nums_;
+struct struct_etcopt etcopt_;
+struct struct_devdat devdat_;
+struct struct_grcol grcol_;
+struct struct_bcolr bcolr_;
+struct struct_plcol2 plcol2_;
+struct struct_cmap cmap_;
+struct struct_clst clst_;
+struct struct_debugc debugc_;
+struct struct_debugn debugn_;
+struct struct_vcmodr vcmodr_;
+struct struct_vcvec1 vcvec1_;
+struct struct_vcndcm vcndcm_;
+struct struct_vcattr vcattr_;
+struct struct_vccrps vccrps_;
+struct struct_vcpstc vcpstc_;
+struct struct_vcscal vcscal_;
+struct struct_vcddim vcddim_;
+struct struct_vcjob vcjob_;
+struct struct_devcap devcap_;
+struct struct_vcescp vcescp_;
+struct struct_vcpsta vcpsta_;
+struct struct_vcpstb vcpstb_;
+struct struct_vcpstd vcpstd_;
+struct struct_pcolst pcolst_;
+struct struct_vcpage vcpage_;
+struct struct_vcvec2 vcvec2_;
+struct struct_blotans blotans_;
+  
+#ifdef __cplusplus
+        }
+#endif
