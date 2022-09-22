bff4139022ad8e6c18b502193d5f3244632eaa7f
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-09-22 15:58:14 +0200

    Add graphic output commands if needed

--- verif/tests/preprocessor/geom_entities/line.py
+++ verif/tests/preprocessor/geom_entities/line.py
@@ -89,4 +89,17 @@ else:
     lmsg.error(fname+' ERROR.')
 
 
+#Graphic stuff.
+# from postprocess import output_handler
+# oh= output_handler.OutputHandler(modelSpace)
+
+# oh.displayBlocks()#setToDisplay= )
+# oh.displayFEMesh()#setsToDisplay=[])
+# oh.displayLocalAxes()
+# oh.displayLoads()
+# oh.displayReactions()
+# oh.displayDispRot(itemToDisp='uX')
+# oh.displayDispRot(itemToDisp='uY')
+# oh.displayDispRot(itemToDisp='uZ')
+
 
