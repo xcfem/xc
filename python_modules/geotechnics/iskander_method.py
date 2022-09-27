01275b23b13898aa3d11c0a5027ce2f0839dfa46
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-09-27 16:19:02 +0200

    Integrage Iskander method in XC

--- python_modules/geotechnics/iskander_method.py
+++ python_modules/geotechnics/iskander_method.py
@@ -12,6 +12,8 @@
 
 
 import numpy as np
+from scipy import constants
+from scipy import interpolate
 
 class Iskander(object):
     ''' Calculation of Active static and seismic earth pressure for cohesive 
@@ -26,9 +28,10 @@ class Iskander(object):
     :ivar gamma: specific weight of the soil.
     :ivar c: cohesion
     :ivar H: wall height.
+    :ivar g: gravitational acceleration.
     '''
 
-    def __init__(self, kh, kv, omega, beta, phi, gamma, c, H, g=9.80665):
+    def __init__(self, kh, kv, omega, beta, phi, gamma, c, H, g= constants.g):
         ''' Constructor.
 
         :param kh: horizontal seismic coefficient.
@@ -43,80 +46,81 @@ class Iskander(object):
         '''
         self.kh = kh
         self.kv = kv
-        self.g = g     # Gravitational acceleration (m/s2)
+        self.g = g # Gravitational acceleration (m/s2)
         self.omega = omega
         self.beta = beta
         self.phi = phi
-        self.gamma = gamma    # unit weight (kN/m3)
-        self.c = c     # cohesion (kPa)
-        self.H = H     # wall height (m)
+        self.gamma = gamma # unit weight (kN/m3)
+        self.c = c  # cohesion (kPa)
+        self.H = H  # wall height (m)
 
     def alpha_h(self):
-        """
+        '''
         Horizontal component of pseudo-static acceleration
-        """
+        '''
         return self.kh * self.g
 
     def alpha_v(self):
-        """
+        '''
         Vertical component of pseudo-static acceleration
-        """
+        '''
         return self.kv * self.g
 
+    def OnePlusKv(self):
+        ''' Return 1+kv (vertical seismic coefficient).'''
+        return 1+self.kv
+
     def zl(self, zw):
-        """
-        Depth along length of retaining wall
-        """
+        ''' Depth along length of retaining wall.'''
         return zw/np.cos(self.omega)
 
     def z(self, zw):
-        """
-        Depth below the sloping ground surface
-        """
+        '''Depth below the sloping ground surface.'''
         return zw * (np.cos(self.beta-self.omega))/(np.cos(self.beta) \
                 * np.cos(self.omega))
 
     def g_theta(self):
-        """
+        '''
         Equation 1: Modified gravitational acceleration
-        """
+        '''
         return np.sqrt((self.g + self.alpha_v())**2 + self.alpha_h()**2)
 
     def theta(self):
-        """
+        '''
         Equation 2: Angle to vertical
-        """
-        return np.arctan(self.kh/(1 + self.kv))
+        '''
+        return np.arctan(self.kh/self.OnePlusKv())
 
     def gamma_theta(self):
-        """
+        '''
         Equation 3: Modified unit weight
-        """
-        return (self.gamma * (1 + self.kv))/(np.cos(self.theta()))
+        '''
+        return (self.gamma * self.OnePlusKv())/(np.cos(self.theta()))
 
     def Ja(self, zw):
-        """
+        '''
         Equation 13: Active condition
-        """
+        '''
+        onePlusKv= self.OnePlusKv()
         J_p1 = ((self.gamma*self.z(zw)*np.cos(self.beta)*np.cos(self.beta \
-                + self.theta())*(1+self.kv))/np.cos(self.theta())) \
+                + self.theta())*onePlusKv)/np.cos(self.theta())) \
                 + self.c*np.cos(self.phi)*np.sin(self.phi)
         J_p2 = (self.gamma**2)*(self.z(zw)**2)*((np.cos(self.beta))**2) \
-                * ((1+self.kv)**2) * ((((np.cos(self.beta+self.theta()))**2) \
+                * (onePlusKv**2) * ((((np.cos(self.beta+self.theta()))**2) \
                 - ((np.cos(self.phi))**2))/((np.cos(self.theta()))**2))
         J_p3 = (self.c**2)*((np.cos(self.phi))**2)
         J_p4 = (2*self.c*self.gamma*self.z(zw)*np.cos(self.phi)*np.sin(self.phi) \
-                * np.cos(self.beta) * np.cos(self.beta+self.theta())*(1+self.kv)) \
+                * np.cos(self.beta) * np.cos(self.beta+self.theta())*onePlusKv) \
                 / (np.cos(self.theta()))
 
         return (1/((np.cos(self.phi))**2))*(J_p1 - np.sqrt(np.absolute(J_p2+J_p3+J_p4)))
 
     def alpha_a(self, zw):
-        """
+        '''
         Equation 18: Obliquity
-        """
+        '''
         alpha_a_p1 = (((2*np.cos(self.theta())*np.cos(self.beta+self.theta())) \
-                        / (np.cos(self.beta)*(1+self.kv)))*(self.Ja(zw) \
+                        / (np.cos(self.beta)*self.OnePlusKv()))*(self.Ja(zw) \
                         / (self.gamma*self.z(zw)))-1)
         alpha_a_p2 = 2*(np.sin(self.beta-self.omega))*(np.cos(self.beta-self.omega))
         alpha_a_p3 = 2*(np.sin(self.theta()+self.omega))*(np.cos(self.theta()+self.omega))
@@ -127,10 +131,10 @@ class Iskander(object):
         return alpha_a
 
     def Ka(self, zw):
-        """
+        '''
         Equation 16: Active lateral earth pressure coefficient
-        """
-        return ((np.cos(self.beta)*(1+self.kv)*(np.sin(self.theta()+self.omega)**2 \
+        '''
+        return ((np.cos(self.beta)*self.OnePlusKv()*(np.sin(self.theta()+self.omega)**2 \
                 - np.cos(self.beta-self.omega)**2)) \
                /(np.cos(self.alpha_a(zw))*np.cos(self.beta+self.theta()) \
                * np.cos(self.theta()))
@@ -139,45 +143,45 @@ class Iskander(object):
                / np.cos(self.alpha_a(zw))))
 
     def sigma_a(self, zw, user_Ka=999):
-        """
+        '''
         Equation 15: stress acting on the wall
 
         Allow manual user input for Ka to facilitate multiple layers.
-        """
+        '''
         if user_Ka == 999:
             return self.gamma * self.z(zw) * self.Ka(zw)
         else:
             return self.gamma * self.z(zw) * user_Ka
 
     def sigma_AEH(self, zw, user_Ka=999, user_alpha_a=999):
-        """
+        '''
         Equation 19:
 
         Allow manual user input for Ka and alpha_a to facilitate multiple layers.
-        """
+        '''
         if user_Ka == 999:
             return self.sigma_a(zw) * np.cos(self.alpha_a(zw) + self.omega)
         else:
             return self.sigma_a(zw,user_Ka) * np.cos(user_alpha_a + self.omega)
 
     def Hz(self):
-        """
+        '''
         Equation 21: Vertical distance between the heel of the retaining wall
                      and its backfill slope surface
-        """
+        '''
         return self.H * (np.cos(self.beta-self.omega)) \
                 / (np.cos(self.beta)*np.cos(self.omega))
 
     def Hl(self):
-        """
+        '''
         Equation 23: Length of retaining wall
-        """
+        '''
         return self.H/np.cos(self.omega)
 
     def zc(self):
-        """
+        '''
         Equation 20: Depth of tension crack
-        """
+        '''
         var1 = self.Hz()
         var2 = self.sigma_AEH(self.H)
         var3 = self.sigma_AEH(0.1*self.H)
@@ -185,35 +189,37 @@ class Iskander(object):
         return var1*(1-(0.9*var2)/(var2-var3))
 
     def Hzc(self):
-        """
+        '''
         add...
-        """
+        '''
         return self.zc()*(np.cos(self.beta)/np.cos(self.beta-self.omega))
 
     def Hp1(self):
-        """
+        '''
         add...
-        """
+        '''
         return (self.Hl() - self.Hzc())/3
 
     def P_AEH1(self):
-        """
+        '''
         Equation 22: Horizontal lateral earth thrust
-        """
+        '''
         return 0.5*self.sigma_AEH(self.H)*(self.Hl() - self.zc() \
                 * (np.cos(self.beta)/np.cos(self.beta-self.omega)))
 
     def Hp2(self):
-        """
-        add...
-        """
+        ''' Point of application of the the horizontal lateral earth thrust with respect to the wall heel.'''
         return self.Hl()/3
 
     def P_AEH2(self):
-        """
-        Equation 24:
-        """
+        '''Equation 24: pragmatic value of the horizontal lateral earth thrust.'''
         return 0.5*self.sigma_AEH(self.H)*self.Hl()
+
+    def getStressDistribution2(self):
+        ''' Return the stress distribution that corresponds to the figure 6b of Iskander's article.'''
+        zi= [0.0, self.Hl()]
+        sigma_i= [0.0, self.sigma_AEH(self.H)] # See method P_AEH2.
+        return interpolate.interp1d(x= zi, y= sigma_i, fill_value="extrapolate")
 
 
 ### Test with:
---
+++ verif/tests/utility/soil_mechanics/test_iskander_02.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+''' Test active static and seismic earth pressure for cohesive soils 
+    according to the article:
+
+   Active static and seismic earth pressure for c–φ soils Magued Iskandern, 
+   Zhibo (Chris) Chen , Mehdi Omidvar, Ivan Guzman, Omar Elsheri. Polytechnic 
+   Institute of New York University, USA.
+
+   https://www.researchgate.net/publication/259094234_Active_static_and_seismic_earth_pressure_for_c-PH_soils
+'''
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2022, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import geom
+import math
+from geotechnics import iskander_method
+
+# Figure 5 of the article.
+# kh: horizontal seismic coefficient.
+# kv: vertical seismic coefficient.
+# omega: back face inclination of the structure with respect to the vertical (rad).
+# beta: slope inclination of terrain (rad).
+# phi: angle of internal friction of soil (rad).
+# gamma: specific weight of the soil (N/m3).
+# c: cohesion (Pa)
+# H: wall height (m).
+figure5= iskander_method.Iskander(kh= 0.2, kv= -0.1, omega= math.radians(20), beta= math.radians(15), phi= math.radians(30), gamma= 23e3, c= 20e3, H= 15)
+
+Hp2= figure5.Hp2() # Point of application of the the horizontal lateral
+                   # earth thrust with respect to the wall heel.
+                   
+P_AEH2= figure5.P_AEH2() # Pragmatic value of the horizontal lateral
+                         # earth thrust.
+
+earthPressure= figure5.getStressDistribution2()
+
+zi= [1e-4, 3, 6, 9, 12, 15, figure5.Hl()]
+pi= list()
+points= list()
+points.append(geom.Pos2d(figure5.Hl(),0.0))
+points.append(geom.Pos2d(0.0,0.0))
+for z in zi:
+    pressure= float(earthPressure(z))
+    pi.append(pressure)
+    points.append(geom.Pos2d(z,pressure))
+
+plg= geom.Polygon2d(points)
+plgArea= plg.getArea()
+plgCentroid= plg.getCenterOfMass()
+hCentroid= (figure5.Hl()-plgCentroid.x)
+
+    
+
+ratio1= abs(Hp2-5.320888862379561)/5.320888862379561
+ratio2= abs(P_AEH2-1726.786768897252e3)/1726.786768897252e3
+ratio3= abs(P_AEH2-plgArea)/P_AEH2
+ratio4= abs(hCentroid-Hp2)/Hp2
+
+'''
+print('Hp2= ', Hp2, 'm')
+print(ratio1)
+print('P_AEH2= ', P_AEH2/1e3, 'kN')
+print(ratio2)
+print('plgArea= ', plgArea)
+print(ratio3)
+print('plgCentroid= ', plgCentroid)
+print(ratio4)
+'''
+
+import os
+from misc_utils import log_messages as lmsg
+fname= os.path.basename(__file__)
+if (ratio1<1e-6) & (ratio2<1e-6) & (ratio3<1e-6) & (ratio4<1e-6):
+    print('test: '+fname+': ok.')
+else:
+    lmsg.error(fname+": ERROR.")
+
+# Graphic stuff
+# zi= list()
+# sigma_AEHi= list()
+# for zw in [1e-4, 3, 6, 9, 12, 15]:
+#     zi.append(-zw)
+#     sigma_AEHi.append(figure5.sigma_AEH(zw)/1e3)
+
+# import matplotlib.pyplot as plt
+# plt.title("Pseudo-statik lateral pressure")
+# plt.xlabel("Horizontal pressure (kPa)")
+# plt.ylabel("Depth (m)")
+# plt.plot(sigma_AEHi, zi)
+# plt.grid()
+# plt.show()
+
