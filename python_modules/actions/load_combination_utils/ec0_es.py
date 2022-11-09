ebd883fcc04145a6f8358ad83cd529fee1f4633d
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-11-09 20:17:53 +0100

    Implement Eurocode 0 load combinations (Spanish annex)

--- python_modules/actions/load_combination_utils/ec0_es.py
+++ python_modules/actions/load_combination_utils/ec0_es.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
+''' Load combinations according to Spanish IAP-11.'''
+
+__author__= 'Luis C. Pérez Tato (LCPT) and Ana Ortega (AO_O)'
+__copyright__= 'Copyright 2015, LCPT and AO_O'
+__license__= 'GPL'
+__version__= '3.0'
+__email__= 'l.pereztato@ciccp.es ana.ortega@ciccp.es'
 
 import loadCombinations
+from actions.load_combination_utils import utils
+from misc_utils import log_messages as lmsg
 
 factors= loadCombinations.Factors()
 
-#Coef.FULS("favorable","desfavorable","favorable accidental","desfavorable accidental")
+#Coef.FULS('favorable','desfavorable','favorable accidental','desfavorable accidental')
 
 
 #Partial safety factors EC0
@@ -13,20 +22,50 @@ factors= loadCombinations.Factors()
 partial_safety_factors= factors.getPartialSafetyFactors()
 
 #Partial safety factors for permanent actions
-partial_safety_factors["permanent"]= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(1,1.35,1,1),loadCombinations.SLSPartialSafetyFactors(1,1))
+partial_safety_factors['permanent']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(1,1.35,1,1),loadCombinations.SLSPartialSafetyFactors(1,1))
+partial_safety_factors['settlement_linear_analysis']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(0,1.20,0,1),loadCombinations.SLSPartialSafetyFactors(1,1))
+partial_safety_factors['settlement_non_linear_analysis']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(0,1.35,0,1),loadCombinations.SLSPartialSafetyFactors(0,1))
+#Coeficientes de ponderación para acciones variables (Q).
+#Sobrecarga de  uso
+partial_safety_factors['road_traffic']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(0,1.35,0,1),loadCombinations.SLSPartialSafetyFactors(0,1))
+partial_safety_factors['pedestrian']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(0,1.35,0,1),loadCombinations.SLSPartialSafetyFactors(0,1))
+partial_safety_factors['railway_traffic']= loadCombinations.PartialSafetyFactors(loadCombinations.ULSPartialSafetyFactors(0,1.45,0,1),loadCombinations.SLSPartialSafetyFactors(0,1))
 
 
 # Combination factors for road bridges (table AN.5 (table A2.1) )
 combination_factors= factors.getCombinationFactors()
 # 
-combination_factors.insert("permanent",loadCombinations.CombinationFactors(1,1,1))
+combination_factors.insert('permanent',loadCombinations.CombinationFactors(1,1,1))
+combination_factors.insert('road_traffic_loads_gr1a_trucks',loadCombinations.CombinationFactors(0.75,0.75,0.0))
+combination_factors.insert('road_traffic_loads_gr1a_udl',loadCombinations.CombinationFactors(0.40,0.40,0.0))
+combination_factors.insert('road_traffic_loads_gr1a_footway',loadCombinations.CombinationFactors(0.40,0.40,0.0))
+combination_factors.insert('road_traffic_loads_gr1b_single_axe',loadCombinations.CombinationFactors(0.0,0.75,0.0))
+combination_factors.insert('road_traffic_loads_gr2_horizontal_forces',loadCombinations.CombinationFactors(0.0,0.0,0.0))
+combination_factors.insert('road_traffic_loads_gr3_pedestrian_loads',loadCombinations.CombinationFactors(0.0,0.0,0.0))
+combination_factors.insert('road_traffic_loads_gr4_crowd_loading',loadCombinations.CombinationFactors(0.0,0.0,0.0))
+combination_factors.insert('road_traffic_loads_gr5_vertical_forces',loadCombinations.CombinationFactors(0.0,0.0,0.0)) # Special vehicles. Vertical forces.
+combination_factors.insert('road_traffic_loads_gr6_horizontal_forces',loadCombinations.CombinationFactors(0.0,0.0,0.0)) # Special vehicles. Horizontal forces.
+combination_factors.insert('road_bridge_wind_persistent_situation',loadCombinations.CombinationFactors(0.6,0.2,0.0))
+combination_factors.insert('road_bridge_wind_during_execution',loadCombinations.CombinationFactors(0.8,0.0,0.0))
+combination_factors.insert('road_bridge_wind_aster',loadCombinations.CombinationFactors(1.0,0.0,0.0))
+combination_factors.insert('road_bridge_thermal',loadCombinations.CombinationFactors(0.6,0.6,0.5))
+combination_factors.insert('road_bridge_snow',loadCombinations.CombinationFactors(0.8,0.0,0.0))
+combination_factors.insert('road_bridge_hidrostatic_pressure',loadCombinations.CombinationFactors(1.0,1.0,1.0))
+combination_factors.insert('road_bridge_construction_loads',loadCombinations.CombinationFactors(1.0,0.0,1.0))
 
 class CombGenerator(utils.CombGenerator):
-    ''' Generate combinations corresponding to Eurocode 0 (Spanish annex).'''
+    ''' Generate combinations corresponding to Eurocode 0 (Spanish annex).
 
-    def __init__(self):
-        ''' Constructor.'''
+    :ivar structureType: structure type (road_bridge or footbridge or railway_bridge or building).
+    '''
+
+    def __init__(self, structureType= 'road_bridge'):
+        ''' Constructor.
+
+        :param structureType: structure type (road_bridge or footbridge or railway_bridge or building).
+        '''
         super().__init__(combGeneratorName= 'EC0_ES', factors= factors)
+        self.structureType= structureType
         
     def newPermanentAction(self, actionName: str, actionDescription: str, dependsOn= None, incompatibleActions= None):
         ''' Creates a permanent action and appends it to the combinations 
@@ -37,20 +76,44 @@ class CombGenerator(utils.CombGenerator):
         :param dependsOn: name of another load that must be present with this one (for example brake loads depend on traffic loads).
         :param incompatibleActions: list of regular expressions that match the names of the actions that are incompatible with this one.
         '''
-        raise NotImplementedError()
-        return None
+        return self.newAction(family= 'permanent',actionName= actionName, actionDescription= actionDescription, combinationFactorsName= 'permanent', partialSafetyFactorsName= 'permanent', dependsOn= dependsOn, incompatibleActions= incompatibleActions)
     
-    def newHeavyVehicleAction(self, actionName: str, actionDescription: str, dependsOn= None, incompatibleActions= None):
+    def newHeavyVehicleAction(self, actionName: str, actionDescription: str, group:str, dependsOn= None, incompatibleActions= None):
         ''' Creates a heavy vehicle action and appends it to the combinations 
             generator.
+
+        :param actionName: name of the action.
+        :param actionDescription: description of the action.
+        :param group: load group (gr1a, gr1b, gr4 or gr5).
+        :param dependsOn: name of another load that must be present with this one (for example brake loads depend on traffic loads).
+        :param incompatibleActions: list of regular expressions that match the names of the actions that are incompatible with this one.
+        '''
+        cFactors= 'road_traffic_loads_gr1a_trucks'
+        if(group=='gr1a'):
+            cFactors= 'road_traffic_loads_gr1a_trucks'
+        elif(group=='gr1b'):
+            cFactors= 'road_traffic_loads_gr1b_single_axe'
+        elif(group=='gr4'):
+            cFactors= 'road_traffic_loads_gr4_crowd_loading'
+        elif(group=='gr5'): # Special vehicles.
+            cFactors= 'road_traffic_loads_gr5_vertical_forces'
+        else:
+            className= type(self).__name__
+            methodName= sys._getframe(0).f_code.co_name
+            lmsg.error(className+'.'+methodName+'; group: '+str(group) + ' doesn\'t correspond to vertical loads from heavy vehicles (punctual loads).')
+        
+        return self.newAction(family= 'variables',actionName= actionName, actionDescription= actionDescription, combinationFactorsName= cFactors, partialSafetyFactorsName= 'road_traffic', dependsOn= dependsOn, incompatibleActions= incompatibleActions)
+    
+    def newRoadBridgeUniformAction(self, actionName: str, actionDescription: str, dependsOn= None, incompatibleActions= None):
+        ''' Creates a uniformly distributed load (UDL system) and appends it 
+            to the combinations generator.
 
         :param actionName: name of the action.
         :param actionDescription: description of the action.
         :param dependsOn: name of another load that must be present with this one (for example brake loads depend on traffic loads).
         :param incompatibleActions: list of regular expressions that match the names of the actions that are incompatible with this one.
         '''
-        raise NotImplementedError()
-        return None
+        return self.newAction(family= 'variables',actionName= actionName, actionDescription= actionDescription, combinationFactorsName= 'road_traffic_loads_gr1a_udl', partialSafetyFactorsName= 'road_traffic', dependsOn= dependsOn, incompatibleActions= incompatibleActions)
     
     def newFootbridgeAction(self, actionName: str, actionDescription: str, dependsOn= None, incompatibleActions= None):
         ''' Creates a footbridge action and appends it to the combinations 
