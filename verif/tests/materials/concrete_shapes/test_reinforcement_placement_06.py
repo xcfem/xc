2a2090e12eab03cabd2ae82c6cdd061a3088ffb8
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-11-14 15:54:09 +0100

    Enhance code documentation

--- verif/tests/materials/concrete_shapes/test_reinforcement_placement_06.py
+++ verif/tests/materials/concrete_shapes/test_reinforcement_placement_06.py
@@ -163,15 +163,15 @@ outCfg.controller= EC2_limit_state_checking.BiaxialBendingNormalStressController
 outCfg.controller.verbose= False # Don't display log messages.
 feProblem.logFileName= "/tmp/erase.log" # Ignore warning messagess about computation of the interaction diagram.
 feProblem.errFileName= "/tmp/erase.err" # Ignore warning messagess about maximum error in computation of the interaction diagram.
-meanFCs= reinfConcreteSectionDistribution.internalForcesVerification3D(lsd.normalStressesResistance,"d",outCfg)
+meanCFs= reinfConcreteSectionDistribution.internalForcesVerification3D(lsd.normalStressesResistance,"d",outCfg)
 feProblem.errFileName= "cerr" # From now on display errors if any.
 feProblem.logFileName= "clog" # From now on display warnings if any.
 
-ratio1= abs(meanFCs[0]-0.49950728265667205)/0.49950728265667205
-ratio2= abs(meanFCs[1]-0.5151243799585318)/0.5151243799585318
+ratio1= abs(meanCFs[0]-0.49950728265667205)/0.49950728265667205
+ratio2= abs(meanCFs[1]-0.5151243799585318)/0.5151243799585318
 
 '''
-print('meanFCs= ', meanFCs)
+print('meanCFs= ', meanCFs)
 print("ratio1= ",ratio1)
 print("ratio2= ",ratio2)
 '''
