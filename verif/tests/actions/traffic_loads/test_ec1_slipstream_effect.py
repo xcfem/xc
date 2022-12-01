bc1ac66b97a960117ec1bd331247715af05652c5
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-12-01 12:04:37 +0100

    Code cosmetics

--- verif/tests/actions/traffic_loads/test_ec1_slipstream_effect.py
+++ verif/tests/actions/traffic_loads/test_ec1_slipstream_effect.py
@@ -12,26 +12,26 @@ __email__= "l.pereztato@gmail.com"
 
 from actions.railway_traffic import ec1_slipstream_effect
 
-q1k_300_2dot3=  ec1_slipstream_effect.q1k(v= 300/3.6, a_g= 2.3)
+# Compute some values
+q1k_300_2dot3= ec1_slipstream_effect.q1k(v= 300/3.6, a_g= 2.3)
+q1k_300_6dot3= ec1_slipstream_effect.q1k(v= 300/3.6, a_g= 6.0)
+q2k_300_5= ec1_slipstream_effect.q2k(v= 300/3.6, h_g= 5.0)
+q2k_300_9= ec1_slipstream_effect.q2k(v= 300/3.6, h_g= 9.0)
+q3k_200_2= ec1_slipstream_effect.q3k(v= 200/3.6, a_g= 2.0)
+q3k_200_6= ec1_slipstream_effect.q3k(v= 200/3.6, a_g= 6.0)
+
+# Check results.
 ratio1= abs(q1k_300_2dot3-1.755501356315947e3)/1.755501356315947e3
-
-q1k_300_6dot3=  ec1_slipstream_effect.q1k(v= 300/3.6, a_g= 6.0)
 ratio2= abs(q1k_300_6dot3-0.36458333333333326e3)/0.36458333333333326e3
-
-q2k_300_5=  ec1_slipstream_effect.q2k(v= 300/3.6, h_g= 5.0)
 ratio3= abs(q2k_300_5-2.4696901931363495e3)/2.4696901931363495e3
-q2k_300_9=  ec1_slipstream_effect.q2k(v= 300/3.6, h_g= 9.0)
 ratio4= abs(q2k_300_9-0.31447376033387586e3)/0.31447376033387586e3
-
-q3k_200_2=  ec1_slipstream_effect.q3k(v= 200/3.6, a_g= 2.0)
 ratio5= abs(q3k_200_2-0.6004943987197074e3)/0.6004943987197074e3
-q3k_200_6=  ec1_slipstream_effect.q3k(v= 200/3.6, a_g= 6.0)
 ratio6= abs(q3k_200_6-0.10300925925925926e3)/0.10300925925925926e3
 
 '''
 print('q1k_300_2dot3= ', q1k_300_2dot3/1e3, 'kN/m2')
-print('q1k_300_6dot3= ', q1k_300_6dot3/1e3, 'kN/m2')
 print("ratio1= ", ratio1)
+print('q1k_300_6dot3= ', q1k_300_6dot3/1e3, 'kN/m2')
 print("ratio2= ", ratio2)
 
 print('q2k_300_5= ', q2k_300_5/1e3, 'kN/m2')
