a5a8c1ef5e8ea1a27598407a760535d1b4324fc9
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-12-11 18:14:57 +0100

    Code cosmetics

--- verif/tests/geotechnics/earthworks/test_embankment_05.py
+++ verif/tests/geotechnics/earthworks/test_embankment_05.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-''' Test Embankment getLateralPressuresOnPoints method.'''
+''' Test Embankment getEarthPressuresOnPointGrid method.'''
 
 from __future__ import print_function
 
