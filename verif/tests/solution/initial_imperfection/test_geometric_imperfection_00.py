0e872625ad9dd9cc05bd985b0beef667c1df0b75
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2022-12-12 20:47:52 +0100

    Add initial imperfection verification tests
    Test related with setting up initial imperfections in order
    to analyze second order effects.

---
+++ verif/tests/solution/initial_imperfection/test_geometric_imperfection_00.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+''' Check element update after a "manual" change in the position of
+the nodes.'''
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Ana Ortega (AO_O) and Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2022, AO_O and LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= " ana.Ortega.Ort@gmail.com, l.pereztato@gmail.com"
+
+import math
+import os
+import geom
+import xc
+from materials.ec3 import EC3_materials
+from model import predefined_spaces
+from solution import predefined_solutions
+
+#Materials
+## Steel material
+steel= EC3_materials.S235JR
+steel.gammaM= 1.00
+## Profile geometry
+shape= EC3_materials.HEShape(steel= steel, name= 'HE_100_B')
+
+## Problem geometry
+height= 10.0 # column height.
+
+# Finite element model.
+## Problem type
+steelColumn= xc.FEProblem()
+steelColumn.title= 'Test geometry imperfection 01.'
+preprocessor= steelColumn.getPreprocessor
+nodes= preprocessor.getNodeHandler
+modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+
+## Model geometry
+
+### Nodes
+n0= nodes.newNodeXY(0,0)
+n1= nodes.newNodeXY(0,height)
+
+### Material.
+xcSection= shape.defElasticShearSection2d(preprocessor)
+### Mesh generation
+
+### Geometric transformations
+cooTrf= modelSpace.newLinearCrdTransf("lin") # Linear transformation.
+# Elements definition
+elements= preprocessor.getElementHandler
+elements.defaultTransformation= cooTrf.name
+elements.defaultMaterial= xcSection.name
+#  syntax: beam2d_02[<tag>] 
+
+elemA= elements.newElement("ElasticBeam2d",xc.ID([n0.tag,n1.tag]))
+
+### Imperfection
+e0= 10
+pos= n1.getInitialPos3d
+newX= e0
+pos.x+= newX
+n1.setPos(pos)
+newCoo= n1.getCoo
+err0= (newCoo-xc.Vector([10,10])).Norm()
+
+# Update the coordinate transformation after a change in the node position.
+elemA.initializeCoordTransf()
+elemB= elements.newElement("ElasticBeam2d",xc.ID([n0.tag,n1.tag]))
+
+iVectorA= elemA.getIVector3d(True)
+iVectorB= elemB.getIVector3d(True)
+err1= (iVectorA-iVectorB).getModulus()
+
+lengthA= elemA.getLength(True)
+lengthB= elemB.getLength(True)
+err2= abs(lengthA-lengthB)/lengthB
+
+'''
+print(newCoo)
+print('err0= ', err0)
+print(iVectorA, lengthA)
+print(iVectorB, lengthB)
+print('err1= ',err1)
+print('err2= ',err2)
+'''
+
+import os
+from misc_utils import log_messages as lmsg
+fname= os.path.basename(__file__)
+if(abs(err0)<1e-10 and abs(err1)<1e-10 and abs(err2)<1e-10):
+    print('test '+fname+': ok.')
+else:
+    lmsg.error(fname+' ERROR.')
+
---
+++ verif/tests/solution/initial_imperfection/test_geometric_imperfection_01.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+''' Check internal forces on imperfect member.'''
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Ana Ortega (AO_O) and Luis C. Pérez Tato (LCPT)"
+__copyright__= "Copyright 2022, AO_O and LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= " ana.Ortega.Ort@gmail.com, l.pereztato@gmail.com"
+
+import math
+import os
+import geom
+import xc
+from materials.ec3 import EC3_materials
+from model import predefined_spaces
+from solution import predefined_solutions
+
+#Materials
+## Steel material
+steel= EC3_materials.S235JR
+steel.gammaM= 1.00
+## Profile geometry
+shape= EC3_materials.HEShape(steel= steel, name= 'HE_100_B')
+
+## Problem geometry
+height= 10.0 # column height.
+
+# Finite element model.
+## Problem type
+steelColumn= xc.FEProblem()
+steelColumn.title= 'Test geometry imperfection 01.'
+preprocessor= steelColumn.getPreprocessor
+nodes= preprocessor.getNodeHandler
+modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+
+## Model geometry
+
+### Points.
+p0= modelSpace.newKPoint(0.0,0.0,0.0)
+p1= modelSpace.newKPoint(0.0,height,0.0)
+
+### Lines
+l1= modelSpace.newLine(p0,p1)
+l1.nDiv= 6
+
+### Material.
+xcSection= shape.defElasticShearSection2d(preprocessor)
+### Mesh generation
+
+### Geometric transformations
+cooTrf= modelSpace.newCorotCrdTransf("corot") # Linear transformation.
+
+seedElemHandler= preprocessor.getElementHandler.seedElemHandler
+seedElemHandler.dimElem= 2 # Bars defined in a two-dimensional space.
+seedElemHandler.defaultMaterial= xcSection.name
+seedElemHandler.defaultTransformation= cooTrf.name
+elem= seedElemHandler.newElement("ElasticBeam2d",xc.ID([0,0]))
+xcTotalSet= preprocessor.getSets.getSet('total')
+mesh= xcTotalSet.genMesh(xc.meshDir.I)
+
+
+### Constraints
+modelSpace.fixNode00F(p0.getNode().tag)
+modelSpace.fixNode0FF(p1.getNode().tag)
+
+### Imperfection
+e0= 0.1
+for n in xcTotalSet.nodes:
+    pos= n.getInitialPos3d
+    newX= e0*math.sin(pos.y*math.pi/height)
+    pos.x+= newX
+    n.setPos(pos)
+
+# Update the coordinate transformations after a change in the position of
+# the nodes.
+for e in xcTotalSet.elements:
+    e.initializeCoordTransf()
+    #print('I=', e.getIVector3d(True))
+    #print('J=', e.getJVector3d(True))
+
+# Load definition.
+lp0= modelSpace.newLoadPattern(name= '0')
+## Point load.
+cLC= modelSpace.setCurrentLoadPattern(lp0.name)
+P= 10e3
+pLoad= xc.Vector([0.0,-P, 0.0])
+p1.getNode().newLoad(pLoad)
+modelSpace.addLoadCaseToDomain(lp0.name)
+
+# Solution
+analysis= predefined_solutions.plain_newton_raphson(steelColumn, maxNumIter= 20, convergenceTestTol= 1e-6)
+result= analysis.analyze(1)
+
+# Compute maximum bending moment.
+mMax= 0.0
+for e in xcTotalSet.elements:
+    M= abs(e.getM1)
+    if(M>mMax):
+        mMax= M
+    M= abs(e.getM2)
+    if(M>mMax):
+        mMax= M
+
+# Compute maximum bending displacement.
+mxDisp= 0.0
+for n in xcTotalSet.nodes:
+    xDisp= abs(n.getDisp[0])
+    if(xDisp>mxDisp):
+        mxDisp= xDisp
+leverArm= e0+mxDisp
+
+# Check results
+err= abs(mMax-leverArm*P)
+
+'''
+print(mMax/1e3)        
+print(mxDisp*1e3)
+print(err)
+'''
+
+import os
+from misc_utils import log_messages as lmsg
+fname= os.path.basename(__file__)
+if(abs(err)<1e-8):
+    print('test '+fname+': ok.')
+else:
+    lmsg.error(fname+' ERROR.')
+
+# # #########################################################
+# # # Graphic stuff.
+# from postprocess import output_handler
+# oh= output_handler.OutputHandler(modelSpace)
+# # oh.displayLoads()
+# # oh.displayFEMesh()
+# oh.displayDispRot(itemToDisp='uX')
+# # oh.displayDispRot(itemToDisp='uY')
+# oh.displayIntForcDiag(itemToDisp='N')
+# oh.displayIntForcDiag(itemToDisp='Mz')
+# oh.displayIntForcDiag(itemToDisp='Vy')
