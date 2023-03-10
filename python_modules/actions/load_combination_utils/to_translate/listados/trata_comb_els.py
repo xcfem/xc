175edb6c76419bcd4229dab80f9ed700ae7be3b0
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-03-10 21:44:43 +0100

    Remove deprecated files
    Those files are too old to be useful and they comme from a very
    rough translation from the PPL language.

--- python_modules/actions/load_combination_utils/to_translate/listados/trata_comb_els.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-exec(open("comb_acciones/listados/trata_comb_elsf.py").read())
-exec(open("comb_acciones/listados/trata_comb_elspf.py").read())
-exec(open("comb_acciones/listados/trata_comb_elscp.py").read())
