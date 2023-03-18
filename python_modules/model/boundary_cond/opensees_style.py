18ae9ec9a487a230be55e94d54b6fbd324f7cc2b
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-03-18 21:26:46 +0100

    Code cosmetics

--- python_modules/model/boundary_cond/opensees_style.py
+++ python_modules/model/boundary_cond/opensees_style.py
@@ -2,7 +2,7 @@
 
 # Impone movimiento nulo en todos los DOF of the node.
 def fix(constraints, idNode,gdls):
-  sz= len(gdls)
-  for i in range(0,sz):
-    if i!=0:
-      constraints.newSPConstraint(idNode,i,0.0) # Node id: idNode
+    sz= len(gdls)
+    for i in range(0,sz):
+        if i!=0:
+            constraints.newSPConstraint(idNode,i,0.0) # Node id: idNode
