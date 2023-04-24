cc2991d00017cc1a070c04d3ec99ea0851e6f446
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-04-24 20:20:50 +0200

    Code cosmetics

--- python_modules/model/boundary_cond/opensees_style.py
+++ python_modules/model/boundary_cond/opensees_style.py
@@ -1,8 +1,23 @@
-# OpenSees style constraints
+# -*- coding: utf-8 -*-
+'''OpenSees style commands. Useful when translating OpenSees tcl files.'''
+
+from __future__ import division
+
+__author__= " Ana Ortega (AO_O), Luis C. Pérez Tato (LCPT) "
+__copyright__= "Copyright 2017, AO_O, LCPT"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "ana.ortega@ciccp.es, l.pereztato@ciccp.es "
 
 # Impone movimiento nulo en todos los DOF of the node.
-def fix(constraints, idNode,gdls):
+def fix(constraintHandler, idNode, gdls):
+    ''' Simulates OpenSees fix command.
+
+    :param constraintHandler: constraint handler.
+    :param idNode: identifier of the node to constraint.
+    :param gdls: degrees of freedom to fix (0: free, 1 fixed).
+    '''
     sz= len(gdls)
     for i in range(0,sz):
         if i!=0:
-            constraints.newSPConstraint(idNode,i,0.0) # Node id: idNode
+            constraintHandler.newSPConstraint(idNode,i,0.0) # Node id: idNode
