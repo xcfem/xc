a2853620f1cddb1ffbd5eb20137ca399023d3b21
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2023-10-16 16:10:05 +0200

    Add verification test for track structure interaction material

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -909,6 +909,9 @@ python tests/materials/xc_materials/uniaxial/soil_structure_interaction/test_soi
 python tests/materials/xc_materials/uniaxial/soil_structure_interaction/test_soil_response_03.py
 python tests/materials/xc_materials/uniaxial/soil_structure_interaction/test_soil_response_04.py
 python tests/materials/xc_materials/uniaxial/soil_structure_interaction/test_soil_response_05.py
+echo "$BLEU" "      Rail structure interaction materials." "$NORMAL"
+python tests/materials/xc_materials/uniaxial/track_structure_interaction/test_track_structure_interaction_spring_01.py
+
 #nD Materials
 echo "$BLEU" "      nD materials tests." "$NORMAL"
 python tests/materials/xc_materials/nD/test_elastic_isotropic_plane_strain_2d_01.py
