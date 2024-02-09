0e56d8e321adafa7e009607782c6e80a236f80f2
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-02-09 17:05:22 +0100

    Add buckling verification test using linear buckling analysis
    3D version of the previous problem problem (two commits ago).

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -1229,6 +1229,7 @@ python tests/materials/ec3/test_buckling_resistance_01.py
 python tests/materials/ec3/test_buckling_resistance_02.py
 python tests/materials/ec3/test_buckling_resistance_03.py
 python tests/materials/ec3/test_buckling_resistance_04.py
+python tests/materials/ehe/test_buckling_resistance_05.py
 echo "$BLEU" "      lateral-torsional buckling tests." "$NORMAL"
 python tests/materials/ec3/test_lateral_torsional_buckling00.py
 python tests/materials/ec3/test_lateral_torsional_buckling01.py
--- verif/tests/materials/ec3/test_buckling_resistance_05.py
+++ verif/tests/materials/ec3/test_buckling_resistance_05.py
@@ -1,4 +1,4 @@
-''' Check the computation of critical axial loads based on the results of an eigenvalue analysis. Two-dimensional model. See also test_buckling_resistance_02.py
+''' Check the computation of critical axial loads based on the results of an eigenvalue analysis. Three-dimensional model. See also test_buckling_resistance_04.py
 
 Inspired on the problem described in: https://sdcverifier.com/benchmarks/eurocode-3-buckling-resistance-of-a-compression-member/
 '''
@@ -26,25 +26,25 @@ feProblem= xc.FEProblem()
 feProblem.title= 'Building columns effective lengths.'
 preprocessor= feProblem.getPreprocessor
 nodes= preprocessor.getNodeHandler
-modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
 
 ## Problem geometry
 H= 4
-p2= modelSpace.newKPoint(0,H,0)
+p2= modelSpace.newKPoint(0,0,H)
 p1= modelSpace.newKPoint(0,0,0)
 l1= modelSpace.newLine(p1, p2)
 l1.nDiv= 6
 
 ## Geometric transformations
-corot= modelSpace.newCorotCrdTransf("corot")
+corot= modelSpace.newCorotCrdTransf("corot", xzVector= xc.Vector([1,0,0]))
 modelSpace.setDefaultCoordTransf(corot)
 
 ## Finite element material
-xcSection= steelShape.defElasticShearSection2d(preprocessor)
+xcSection= steelShape.defElasticShearSection3d(preprocessor)
 modelSpace.setDefaultMaterial(xcSection)
 
 ## FiniteElement.
-modelSpace.newSeedElement("ElasticBeam2d")
+modelSpace.newSeedElement("ElasticBeam3d")
 
 ## Mesh generation.
 l1.genMesh(xc.meshDir.I)
@@ -52,23 +52,26 @@ l1.genMesh(xc.meshDir.I)
 ## Constraints.
 bottomNode= p1.getNode()
 topNode= p2.getNode()
-modelSpace.fixNode('00F',bottomNode.tag)
-modelSpace.fixNode('0FF',topNode.tag)
+#modelSpace.fixNode('000_FF0',bottomNode.tag)
+#modelSpace.fixNode('000_FFF',topNode.tag)
+modelSpace.fixNode('000_FF0',bottomNode.tag)
+modelSpace.fixNode('00F_FFF',topNode.tag)
 
 ## Loads
 lp0= modelSpace.newLoadPattern(name= 'lp0')
 NEd= 1630e3
-lp0.newNodalLoad(topNode.tag, xc.Vector([0,-NEd,0]))
+lp0.newNodalLoad(topNode.tag, xc.Vector([0,0,-NEd,0,0,0]))
 modelSpace.addLoadCaseToDomain(lp0.name)
 
 # Perform linear buckling analysis.
-linearBucklingAnalysis= predefined_solutions.LinearBucklingAnalysis(prb= feProblem, numModes= 3, constraintHandlerType= 'transformation', numberingMethod= 'rcm', convTestType= "norm_disp_incr_conv_test", convergenceTestTol= 1e-8, maxNumIter= 1000, soeType= "band_gen_lin_soe", solverType= "band_gen_lin_lapack_solver", solutionAlgorithmType= 'krylov_newton_soln_algo', eigenSOEType= "spectra_soe", eigenSolverType= "spectra_solver")
+linearBucklingAnalysis= predefined_solutions.LinearBucklingAnalysis(prb= feProblem, numModes= 4, constraintHandlerType= 'transformation', numberingMethod= 'rcm', convTestType= "norm_disp_incr_conv_test", convergenceTestTol= 1e-8, maxNumIter= 1000, soeType= "band_gen_lin_soe", solverType= "band_gen_lin_lapack_solver", solutionAlgorithmType= 'krylov_newton_soln_algo', eigenSOEType= "spectra_soe", eigenSolverType= "spectra_solver")
 linearBucklingAnalysis.setup()
 analOk= linearBucklingAnalysis.solve()
 
 # Get results.
 eulerBucklingLoadFactor1= linearBucklingAnalysis.analysis.getEigenvalue(1)
 eulerBucklingLoadFactor2= linearBucklingAnalysis.analysis.getEigenvalue(2)
+eulerBucklingLoadFactor3= linearBucklingAnalysis.analysis.getEigenvalue(3)
 
 xcTotalSet= modelSpace.getTotalSet()
 
@@ -78,23 +81,28 @@ avgNbRd= 0.0
 for e in xcTotalSet.elements:
     N= e.getN()
     # Critical axial load.
-    Ncri= [eulerBucklingLoadFactor1*N, eulerBucklingLoadFactor2*N]
+    Ncri= [eulerBucklingLoadFactor1*N, eulerBucklingLoadFactor2*N, eulerBucklingLoadFactor3*N]
     # Effective length.
     section= e.physicalProperties.getVectorMaterials[0]
-    EI= section.sectionProperties.EI()
+    EIz= section.sectionProperties.EIz()
+    EIy= section.sectionProperties.EIy()
     Leffi= list()
     for Ncr in Ncri:
-        Leff= math.sqrt((EI*math.pi**2)/abs(Ncr))
+        Leffz= math.sqrt((EIz*math.pi**2)/abs(Ncr))
+        Leffy= math.sqrt((EIy*math.pi**2)/abs(Ncr))
         if(Ncr>0):
-            Leff= -Leff
-        Leffi.append(Leff)
+            Leffz= -Leffz
+            Leffy= -Leffy
+        Leffi.append((Leffz, Leffy))
     # Buckling reduction factor and buckling resistance.
     Xi= list()
     NbRdi= list()
-    for Leff in Leffi:
-        Xi.append(steelShape.getBucklingReductionFactorY(Leff))
-        NbRdi.append(steelShape.getBucklingResistance(Leff,Leff))
-    avgLeff+= Leffi[0]
+    for (Leffz, Leffy) in Leffi:
+        Xz= steelShape.getBucklingReductionFactorZ(Leffz)
+        Xy= steelShape.getBucklingReductionFactorY(Leffy)
+        Xi.append((Xz,Xy))
+        NbRdi.append(steelShape.getBucklingResistance(Leffy,Leffz))
+    avgLeff+= Leffi[0][0]
     avgNbRd+= NbRdi[0]
 
 sz= len(xcTotalSet.elements)
@@ -106,6 +114,7 @@ ratio2= abs(avgNbRd-2306.398883222727e3)/2306.398883222727e3
 '''
 print('first euler buckling load factor: ', eulerBucklingLoadFactor1)
 print('second euler buckling load factor: ', eulerBucklingLoadFactor2)
+print('third euler buckling load factor: ', eulerBucklingLoadFactor3)
 print('effective length: ', avgLeff, 'm', ratio1)
 print('buckling resistance: ', avgNbRd/1e3, 'kN', ratio2)
 '''
