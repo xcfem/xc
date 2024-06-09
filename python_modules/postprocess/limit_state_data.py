16385d9405374ba8943188f4aefede18b95a3209
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-06-09 16:01:56 +0200

    Minor debugging

--- python_modules/postprocess/limit_state_data.py
+++ python_modules/postprocess/limit_state_data.py
@@ -707,7 +707,7 @@ class BucklingParametersLimitStateData(ULS_LimitStateData):
             mechLambdai= extendedData['mechLambdai']
             Efi= extendedData['Efi']
             strongAxisBucklingPercent= extendedData['strongAxisBucklingPercent']
-            CFncri= extendedData['CFncri']
+            CFncr_i= extendedData['CFncr_i']
             for index in elementControlVars:
                 controlVar= elementControlVars[index]
                 loadCombinationName= controlVar.combName
@@ -717,7 +717,7 @@ class BucklingParametersLimitStateData(ULS_LimitStateData):
                     Leff= Leffi[mode0]
                     mechLambda= mechLambdai[mode0]
                     (EfZ, EfY)= Efi[mode0]
-                    CFncr= CFncri[mode0]
+                    CFncr= CFncr_i[mode0]
                     dotProduct= strongAxisBucklingPercent[mode0]
                     controlVar.setBucklingParameters(Leff= Leff, mechLambda= mechLambda, efZ= EfZ, efY= EfY, mode= mode, CFncr= CFncr, strongAxisBucklingPercent= dotProduct)
                 else:
