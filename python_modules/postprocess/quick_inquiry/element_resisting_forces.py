698d91aa9770c0d65fb189b647c27bee5c7f23e1
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-07-09 20:20:17 +0200

    Pass the tolerance parameter to the code inside the function
    When using a Polygon3d as contour object a tolerance is needed to
    catch the nodes that are slightly out of the polygon plane.

--- python_modules/postprocess/quick_inquiry/element_resisting_forces.py
+++ python_modules/postprocess/quick_inquiry/element_resisting_forces.py
@@ -10,7 +10,7 @@ __email__= "l.pereztato@ciccp.es, ana.Ortega@ciccp.es "
 import geom
 import xc
 
-def compute_element_resisting_forces(contour, elemSet):
+def compute_element_resisting_forces(contour, elemSet, tol= 0.0):
     ''' Extract the resisting forces that act on the given contour. To do that
     the elements that cross the contour border (i. e. the elements which have
     nodes inside and outside the given contour) are selected. For those 
@@ -23,12 +23,12 @@ def compute_element_resisting_forces(contour, elemSet):
               to the contour. The key of the dictionary is the node identifier.
     '''
     contourSetName= elemSet.name+'_contour_elements'
-    elementsOnContourBorder= elemSet.pickElemsCrossing(contourSetName, contour, 0.0)
+    elementsOnContourBorder= elemSet.pickElemsCrossing(contourSetName, contour, tol)
     elementsOnContourBorder.fillDownwards()
     retval= dict() # forces on the nodes outside the contour.
     for e in elementsOnContourBorder.elements:
         for i, n in enumerate(e.nodes):
-            if(n.Out(contour, 1, 0)): # node outside the critical contour.
+            if(n.Out(contour, 1, tol)): # node outside the critical contour.
                 nodePosition= n.getInitialPos3d
                 nTag= n.tag
                 e.getResistingForce()
