9a75d2217a99379d295eef4139dcb5734d5915a8
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-07-13 18:02:59 +0200

    Code to extract element resisting forces results (work in progress)
    The idea is to have a set of functions that given a contour that
    delimites a region of the model, compute the forces acting on
    that region.

--- python_modules/postprocess/quick_inquiry/element_resisting_forces.py
+++ python_modules/postprocess/quick_inquiry/element_resisting_forces.py
@@ -10,34 +10,135 @@ __email__= "l.pereztato@ciccp.es, ana.Ortega@ciccp.es "
 import geom
 import xc
 
-def compute_element_resisting_forces(contour, elemSet, tol= 0.0):
-    ''' Extract the resisting forces that act on the given contour. To do that
-    the elements that cross the contour border (i. e. the elements which have
-    nodes inside and outside the given contour) are selected. For those 
-    elements, the resisting forces at the nodes outside the contour are
-    computed.
+def sort_control_positions(controlPositions):
+    ''' Sort the controls positions according to its placement along the
+        contour.
 
-    :param contour: geometric object subjected to the forces to be extracted.
-    :param elemSet: set of finite element to extract the forces from.
-    :returns: dictionary containing the forces in each of the nodes external
-              to the contour. The key of the dictionary is the node identifier.
+    :param controlPositions: dictionary containing, for each outside node,
+                             the corresponding position in the contour and
+                             the elements touching that node.
     '''
-    contourSetName= elemSet.name+'_contour_elements'
-    elementsOnContourBorder= elemSet.pickElemsCrossing(contourSetName, contour, tol)
-    elementsOnContourBorder.fillDownwards()
-    retval= dict() # forces on the nodes outside the contour.
-    for e in elementsOnContourBorder.elements:
-        for i, n in enumerate(e.nodes):
-            if(n.Out(contour, 1, tol)): # node outside the critical contour.
-                nodePosition= n.getInitialPos3d
-                nTag= n.tag
+    tmp= list()
+    for nTag in controlPositions:
+        contourLambda= controlPositions[nTag]['contour_lambda']
+        tmp.append((nTag, contourLambda))
+    sortedTags= sorted(tmp, key=lambda tup: tup[1])
+    retval= dict()
+    for (nTag, contourLambda) in sortedTags:
+        retval[nTag]= controlPositions[nTag]
+    return retval
+
+def compute_tributary_lengths(controlPositions, contourPerimeter):
+    ''' Compute the tributary length for each control position.
+
+    :param controlPositions: dictionary containing, for each outside node,
+                             the corresponding position in the contour and
+                             the elements touching that node.
+    :param contourPerimeter: length of the contour perimeter.
+    '''
+    # Compute distances between positions.
+    distances= list()
+    keys= list(controlPositions.keys())
+    s0= controlPositions[keys[0]]['contour_lambda']
+    for key in keys[1:]:
+        s1= controlPositions[key]['contour_lambda']
+        dist= s1-s0
+        distances.append(dist)
+        s0= s1
+    distances.append(contourPerimeter-controlPositions[keys[-1]]['contour_lambda'])
+    # Compute tributary lengths.
+    retval= dict()
+    d0= distances[-1]
+    for d, key in zip(distances, keys):
+        retval[key]= (d0+d)/2.0
+        d0= d
+    return retval
+
+class ContourControlPositions(object):
+    ''' Control positions along a contour.
+
+    :ivar controlPositions: dictionary containing, for each outside node,
+                            the corresponding position in the contour and
+                            the elements touching that node.
+    '''
+
+
+    def __init__(self, contour, elemSet, tol= 0.0):
+        ''' Compute the positions on the contour perimeter that correspond to 
+            the nodes, which, being part of the elements that cross the 
+            contour, lie outside it.
+
+        :param contour: geometric object subjected to the forces to be 
+                        extracted.
+        :param elemSet: set of finite element to extract the forces from.
+        :returns: dictionary that, for each of the nodes outside the contour
+                  contains: 
+
+                  - the positions on the contour perimeter that correspond to
+                    the node. 
+                  - the crossing elements that touch each of the nodes outside
+                    the contour.
+        '''
+        contourSetName= elemSet.name+'_contour_elements'
+        elementsOnContourBorder= elemSet.pickElemsCrossing(contourSetName, contour, tol)
+        elementsOnContourBorder.fillDownwards()
+        tmp= dict() # contour positions and elements for each node outside it.
+        for e in elementsOnContourBorder.elements:
+            for i, n in enumerate(e.nodes):
+                if(n.Out(contour, 1, tol)): # node outside the critical contour.
+                    nodePosition= n.getInitialPos3d
+                    # Project the node position onto the contour.
+                    projPosition= contour.getProjection(nodePosition)
+                    # Compute the parameter of the projected position in the contour.
+                    contourLambda= contour.getLambda(projPosition)
+                    nTag= n.tag
+                    if(nTag in tmp):
+                        tmp[nTag]['neighbour_elements'].append((e, i)) 
+                    else:
+                        tmp[nTag]= {'node_position': nodePosition, 'contour_position': projPosition, 'contour_lambda':contourLambda, 'neighbour_elements': [(e,i)]}
+        tmp= sort_control_positions(tmp)
+        # Compute tributary lengths.
+        contourPerimeter= contour.getPerimeter()
+        tributaryLengths= compute_tributary_lengths(controlPositions= tmp, contourPerimeter= contourPerimeter)
+        for nTag in tmp:
+            tmp[nTag]['tributary_length']= tributaryLengths[nTag]
+        self.controlPositions= tmp
+    
+    def computeElementResistingForces(self):
+        ''' Extract the resisting forces that act on the given control 
+            positions.
+        '''
+        retval= dict() # forces on the nodes outside the contour.
+        for nTag in self.controlPositions:
+            values= self.controlPositions[nTag]
+            neighbourElements= values['neighbour_elements']
+            for (e, i) in neighbourElements:
                 e.getResistingForce()
                 nodeResistingForce= e.getNodeResistingForceIncInertia(i)
+                # Get resisting force.
                 F= geom.Vector3d(nodeResistingForce[0],nodeResistingForce[1],nodeResistingForce[2])
+                # Get resisting moment.
                 M= geom.Vector3d(nodeResistingForce[3],nodeResistingForce[4],nodeResistingForce[5])
+                nodePosition= values['node_position']
                 svs= geom.SlidingVectorsSystem3d(nodePosition, F, M)
                 if(nTag in retval):
                     retval[nTag]+= svs
                 else:
                     retval[nTag]= svs
-    return retval
+        return retval
+
+    def computeContourForces(self):
+        ''' Compute the forces acting on the contour.'''
+        retval= dict()
+        elementResistingForces= self.computeElementResistingForces()
+        for nTag in self.controlPositions:
+            svs= elementResistingForces[nTag]
+            pos= svs.getOrg()
+            contourPos= self.controlPositions[nTag]['contour_position']
+            contourLambda= self.controlPositions[nTag]['contour_lambda']
+            tributaryLength= self.controlPositions[nTag]['tributary_length']
+            newSvs= svs.reduceTo(contourPos)
+            retval[nTag]= {'contour_position':contourPos, 'contour_lambda': contourLambda, 'sliding_vector_system': newSvs, 'tributary_length':tributaryLength}
+        return retval
+
+    
