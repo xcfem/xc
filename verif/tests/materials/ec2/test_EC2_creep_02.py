0aaa527d900c8981bbfef570ff998206f19ca0dd
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-08-01 16:50:30 +0200

    Code cosmetics

--- verif/tests/materials/ec2/test_EC2_creep_02.py
+++ verif/tests/materials/ec2/test_EC2_creep_02.py
@@ -22,7 +22,7 @@ from materials.ec2 import EC2_materials
 # Data
 
 # Type of concrete used in the deck slab
-concrDeck= EC2_materials.EC2Concrete("C35/45",-35e6,1.5) # page 73
+concrete= EC2_materials.EC2Concrete("C35/45",-35e6,1.5) # page 73
  
 Ac= 17.43 # area of the concrete slab (m2); page 80.
 u= 28.64 # perimeter exposed to drying (m); page 80.
@@ -31,30 +31,30 @@ RH= 70 # ambient relative humidity(%); page 79.
 t0= 28 # age of concrete at prestressing time; page 79.
 t_inf= 25550 # corresponding to a life-time of 70 years; page 79.
 
-fiRH= concrDeck.getCreepFiRH(RH,h0) # factor to allow for the effect of relative
+fiRH= concrete.getCreepFiRH(RH,h0) # factor to allow for the effect of relative
                                    # humidity on the notional creep coefficient
                                    
-betafcm= concrDeck.getCreepBetafcm() # factor to allow for the effect of
+betafcm= concrete.getCreepBetafcm() # factor to allow for the effect of
                                      # concrete strength on the notional creep
                                      # coefficient
 
-betat0= concrDeck.getCreepBetat0(t0)  # factor to allow for the effect of
+betat0= concrete.getCreepBetat0(t0)  # factor to allow for the effect of
                                       # concrete age at loading
                                       # on the notional creep coefficient
                                       
-betactt0= concrDeck.getCreepBetactt0(t_inf,t0,RH,h0) # coefficient to describe
+betactt0= concrete.getCreepBetactt0(t_inf,t0,RH,h0) # coefficient to describe
                                                      # the development of creep
                                                      # with time after loading.
 
-betaH= concrDeck.getCreepBetaH(RH,h0) # coefficient depending on the relative
+betaH= concrete.getCreepBetaH(RH,h0) # coefficient depending on the relative
                                       # humidity (RH) and the notional member
                                       # size (h0).
                                       
-fi0=concrDeck.getCreepFi0(t0,RH,h0) # notional creep coefficient for the
+fi0=concrete.getCreepFi0(t0,RH,h0) # notional creep coefficient for the
                                     # calculation of the creep coefficient.
                             
 #Creep coefficients:
-fit_inf_t0= concrDeck.getCreepFitt0(t_inf,t0,RH,h0)
+fit_inf_t0= concrete.getCreepFitt0(t_inf,t0,RH,h0)
 
 ratio0= round(fi0,3)-1.493
 
