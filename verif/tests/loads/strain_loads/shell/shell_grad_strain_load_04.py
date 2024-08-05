5a7c6912a31dfc67d9887dc00498d485cde5828e
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-08-05 18:27:19 +0200

    Fix error when checking ratio value

--- verif/tests/loads/strain_loads/shell/shell_grad_strain_load_04.py
+++ verif/tests/loads/strain_loads/shell/shell_grad_strain_load_04.py
@@ -105,7 +105,7 @@ print("ratio= ", ratio)
 import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if(ratio<1e-8):
+if(abs(ratio)<1e-8):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
