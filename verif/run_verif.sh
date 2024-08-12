bf17d49f9449bb90e513778f0229c13a64fb42cf
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-08-12 18:11:19 +0200

    Update verification tests script

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -712,6 +712,7 @@ python tests/preprocessor/geom_entities/test_block_04.py
 python tests/preprocessor/geom_entities/test_block_05.py
 python tests/preprocessor/geom_entities/test_block_06.py
 python tests/preprocessor/geom_entities/test_block_07.py
+python tests/preprocessor/geom_entities/test_block_08.py
 
 echo "$BLEU" "  Meshing routines tests." "$NORMAL"
 python tests/preprocessor/meshing/test_surface_axes_01.py
