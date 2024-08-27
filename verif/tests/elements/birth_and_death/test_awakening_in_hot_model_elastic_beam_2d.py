04ae69a8a3322995eb238d5d2a77e1a7c99327ec
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-08-27 17:25:22 +0200

    Use more restrictive test for results
    The improvement of the constraint handlers has increased the
    accuracy of the results.

--- verif/tests/elements/birth_and_death/test_awakening_in_hot_model_elastic_beam_2d.py
+++ verif/tests/elements/birth_and_death/test_awakening_in_hot_model_elastic_beam_2d.py
@@ -141,7 +141,7 @@ print('Ux1c= ', Ux1c, ' ratio10= ', ratio10)
 import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if (abs(ratio0)<1e-8) &(abs(ratio1)<1e-5) & (abs(ratio2)<1e-5) & (abs(ratio3)<1e-8) & (abs(ratio4)<1e-5) & (abs(ratio5)<1e-5) & (abs(ratio6)<1e-5) & (abs(ratio7)<1e-8) & (abs(ratio8)<1e-5) & (abs(ratio9)<1e-5) & (abs(ratio10)<1e-5):
+if (abs(ratio0)<1e-8) &(abs(ratio1)<1e-5) & (abs(ratio2)<1e-8) & (abs(ratio3)<1e-8) & (abs(ratio4)<1e-8) & (abs(ratio5)<1e-8) & (abs(ratio6)<1e-8) & (abs(ratio7)<1e-8) & (abs(ratio8)<1e-8) & (abs(ratio9)<1e-8) & (abs(ratio10)<1e-8):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
