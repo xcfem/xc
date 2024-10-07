db7c29f01adda54b0d2e220cf074cc69bccbfd45
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-10-07 11:56:18 +0200

    Replace pyexcel_ods with openpyxl

--- python_modules/materials/sections/structural_shapes/aux/bs_en_10219/import_en_10219_profiles.py
+++ python_modules/materials/sections/structural_shapes/aux/bs_en_10219/import_en_10219_profiles.py
@@ -6,12 +6,13 @@ https://www.steelforlifebluebook.co.uk/hfshs/ec3-ukna/section-properties-dimensi
 '''
 
 __author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
-__copyright__= "Copyright 2022, LCPT and AOO"
+__copyright__= "Copyright 2024, LCPT and AOO"
 __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
-from pyexcel_ods import get_data
+#from misc_utils import spreadsheet_utils as su
+import spreadsheet_utils_link as su
 import json
 
 # Data obtained from: https://www.steelforlifebluebook.co.uk/hfshs/ec3-ukna/section-properties-dimensions-properties/
@@ -35,7 +36,7 @@ import json
 # in XC (strong axis parallel to z axis) in other words: values for Y
 # and Z axis are swapped with respect to those in the catalog.
 
-fNameIn= "bs_en_10219.ods"
+fNameIn= "bs_en_10219.xlsx"
 fNameOut= '../bs_en_10219_profiles.json'
 
 columnOrder= dict()
@@ -43,8 +44,7 @@ columnOrder['CFSHS']= ['','e','b','nmb', 'P', 'A', 'Iz', 'iz', 'Wzel', 'Wzpl', '
 columnOrder['RHS']= ['','e', 'b', 'h', 'nmb', 'P', 'A', 'Iz', 'Iy', 'iz', 'iy', 'Wzel', 'Wyel', 'Wzpl', 'Wypl', 'It', 'Wt', 'AL', '', 'AG', 'bSlendernessRatio', 'hSlendernessRatio']
 columnOrder['CFCHS']= ['D','e','nmb', 'P', 'A', 'Iz', 'iz', 'Wzel', 'Wzpl', 'It', 'Wt', 'AL', 'AG', '', 'slendernessRatio']
 
-
-columnUnits= {'b':'e-3', 'h':'e-3', 'e':'e-3', 'P':'', 'A':'e-4', 'hSlendernessRatio':'', 'bSlendernessRatio':'', 'slendernessRatio':'', 'Iz':'e-8', 'iz':'e-2', 'Wzel':'e-6', 'Wzpl':'e-6', 'Iy':'e-8', 'iy':'e-2', 'Wyel':'e-6', 'Wypl':'e-6', 'It':'e-8',  'Wt':'e-6'}
+columnUnits= {'b':1e-3, 'h':1e-3, 'e':1e-3, 'A':1e-4, 'Iz':1e-8, 'iz':1e-2, 'Wzel':1e-6, 'Wzpl':1e-6, 'Iy':1e-8, 'iy':1e-2, 'Wyel':1e-6, 'Wypl':1e-6, 'It':1e-8,  'Wt':1e-6}
 
 def getOutsideCornerRadii(thickness):
     ''' Return the radius of the outside corners.'''
@@ -57,64 +57,19 @@ def getOutsideCornerRadii(thickness):
         retval*= 3.0
     return retval
 
-def importSections(rows, namePrefix):
-    ''' Import the data corresponding to cold formed
-        square hollow sections.'''
+def post_process(shapesDict):
+    ''' Post-process data readed from the spreadsheet
+
+    :param shapesDict: dictionary containing the data imported from the
+                       spreadsheet.
+    '''
+    # Some post-processing:
     retval= dict()
-    cOrder= columnOrder[namePrefix]
-    numColumns= len(cOrder)
-    columnKeys= dict()
-    for count, key in enumerate(cOrder):
-        if(len(key)>0):
-            columnKeys[key]= count
-    for r in rows:
-        if(len(r)>=numColumns): # if not empty.
-            name= r[columnKeys['nmb']].replace(' ','')
-            if(name.startswith(namePrefix)):
-                shapeRecord= dict()
-                for key in cOrder:
-                    if(len(key)>0):
-                        column= columnKeys[key]
-                        shapeRecord[key]= r[column]
-                if(namePrefix=='CFSHS'): # Square section
-                    shapeRecord['h']= shapeRecord['b']
-                    shapeRecord['bSlendernessRatio']=  shapeRecord['hSlendernessRatio']
-                if(namePrefix in ['CFSHS','CFCHS']): # Square or circular section
-                    shapeRecord['Iy']= shapeRecord['Iz']
-                    shapeRecord['iy']= shapeRecord['iz']
-                    shapeRecord['Wyel']= shapeRecord['Wzel']
-                    shapeRecord['Wypl']= shapeRecord['Wzpl']
-                    
-                shapeRecord['E']= 210000e6 # To deprecate
-                shapeRecord['nu']= 0.3 # To deprecate
-                retval[name]= shapeRecord
-    return retval
-
-    
-shapesDict= dict() # Dictionary with all the shapes.
-data= get_data(fNameIn)
-for sheetName in data:
-    rows= data[sheetName]
-    #if(sheetName in ['CFSHS', 'RHS']):
-    shapesDict[sheetName]= importSections(rows= rows, namePrefix= sheetName)
-    
-# Some post-processing:
-for shapePrefix in shapesDict:
-    sheetDict= shapesDict[shapePrefix]
-    for shapeName in sheetDict:
-        shapeRecord= sheetDict[shapeName]
-        for key in shapeRecord:
-            if(key=='nmb'):
-                value= shapeRecord[key].replace(' ','')
-                shapeRecord[key]= value
-            if(key=='CheckAvailability'):
-                value= shapeRecord[key]
-                shapeRecord[key]= (value=='#')
-
-            if key in columnUnits:
-                factor= columnUnits[key]
-                value= str(shapeRecord[key])#.replace(',','')
-                shapeRecord[key]= float(value+factor)
+    for shapeName in shapesDict:
+        shapeRecord= dict()
+        shapeRecord.update(shapesDict[shapeName])
+        if('CheckAvailability' in shapeRecord):
+            shapeRecord['CheckAvailability']= (shapeRecord['CheckAvailability']=='#')
         if('h' in shapeRecord): # Square or rectangular shape
             outsideCornerRadius= getOutsideCornerRadii(shapeRecord['e'])
             insideCornerRadius= outsideCornerRadius-shapeRecord['e']
@@ -124,25 +79,34 @@ for shapePrefix in shapesDict:
             shapeRecord['h_flat']= h_flat
             b_flat= shapeRecord['b']-outsideCornerRadius
             shapeRecord['b_flat']= b_flat
+        if(shapeName.startswith('CFSHS') or shapeName.startswith('CFCHS')): # Square or circular section
+            shapeRecord['Iy']= shapeRecord['Iz']
+            shapeRecord['iy']= shapeRecord['iz']
+            shapeRecord['Wyel']= shapeRecord['Wzel']
+            shapeRecord['Wypl']= shapeRecord['Wzpl']
+        if(shapeName.startswith('CFSHS')): # Square section
+           # A= shapeRecord['A']
+           E= shapeRecord['E']
+           nu= shapeRecord['nu']
+           b= shapeRecord['b']
+           shapeRecord['h']= shapeRecord['b']
+           h= b
+           shapeRecord['bSlendernessRatio']=  shapeRecord['hSlendernessRatio']
+           e= shapeRecord['e']
+           shapeRecord['alpha']= 0.5*5/6.0
+           shapeRecord['G']= E/(2*(1+nu))
+           shapeRecord['Avy']= 2*0.7*h*e
+           shapeRecord['Avz']= 2*0.7*b*e
+           shapeRecord['Wyel']= shapeRecord['Wzel']
+           shapeRecord['Wypl']= shapeRecord['Wzpl']
+        retval[shapeName]= shapeRecord
+    return retval
 
-# Compute auxiliary data for CFSHS shapes.
-CFSHS= shapesDict['CFSHS']
-for item in CFSHS:
-    shape= CFSHS[item]
-    A= shape['A']
-    E= shape['E']
-    nu= shape['nu']
-    b= shape['b']
-    h= shape['h']
-    e= shape['e']
-    shape['alpha']= 0.5*5/6.0
-    shape['G']= E/(2*(1+nu))
-    shape['Avy']= 2*0.7*h*e
-    shape['Avz']= 2*0.7*b*e
-    shape['Wyel']= shape['Wzel']
-    shape['Wypl']= shape['Wzpl']
+shapesDict= dict()
+for namePrefix in ['CFCHS', 'CFSHS', 'RHS']:
+    print(namePrefix)
+    shapesDict[namePrefix]= su.read_shapes_from_spreadsheet(namePrefix= namePrefix, columnOrder= columnOrder[namePrefix], columnUnits= columnUnits, spreadsheetFileName= fNameIn, E= 210000e6, nu= 0.3, post_processing_function= post_process)
     
 jsonFile= open(fNameOut, "w")
 jsonFile.write(json.dumps(shapesDict))
 jsonFile.close()
-
