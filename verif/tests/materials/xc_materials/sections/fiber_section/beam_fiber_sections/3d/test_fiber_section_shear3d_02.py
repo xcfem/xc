b53c67f34d60a7dd99e815222ee107b9209e0c63
Luis Claudio Pérez Tato <l.pereztato@ciccp.es>
2024-10-27 12:27:19 +0100

    Use a bigger tolerance (1e-11 instead of 1e-12)
    The results are different in different machines but the differences
    are small enough.

--- verif/tests/materials/xc_materials/sections/fiber_section/beam_fiber_sections/3d/test_fiber_section_shear3d_02.py
+++ verif/tests/materials/xc_materials/sections/fiber_section/beam_fiber_sections/3d/test_fiber_section_shear3d_02.py
@@ -146,7 +146,7 @@ print("ratio3= ",ratio3)
 import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if((abs(ratio1)<1e-15) & (abs(ratio2)<1e-15) & (abs(ratio3)<1e-12)):
+if((abs(ratio1)<1e-15) & (abs(ratio2)<1e-15) & (abs(ratio3)<1e-11)):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
