f85d77abaecbfc3527c7055f5f2970f41058517f
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-11-24 15:00:06 +0100

    Fix error in parameter order when calling defElasticSection3d
    Use explicit keyword arguments to avoid the issue.

--- verif/tests/materials/xc_materials/sections/elastic_section/test_section_rotation_3d_03.py
+++ verif/tests/materials/xc_materials/sections/elastic_section/test_section_rotation_3d_03.py
@@ -43,9 +43,8 @@ n2= nodes.newNodeXYZ(L,0.0,0.0)
 lin= modelSpace.newLinearCrdTransf("lin",xc.Vector([0,1,0]))
 
 # Materials definition
-scc= typical_materials.defElasticSection3d(preprocessor, "scc",E,G,A,Iz,Iy,J)
+scc= typical_materials.defElasticSection3d(preprocessor, name= "scc",E= E,G= G,A= A,Iz= Iz, Iy= Iy, J= J)
 scc.sectionProperties.rotate(math.radians(90))
-
 
 # Elements definition
 elements= preprocessor.getElementHandler
