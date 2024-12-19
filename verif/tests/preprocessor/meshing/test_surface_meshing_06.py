907c0deada732e902d4f385f964c080044608427
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2024-12-19 16:20:04 +0100

    Check surface and element orientation
    Check the orientation of the surface with respect to its definition
    points and the orientation of the element with respect to their
    surface.

--- verif/tests/preprocessor/meshing/test_surface_meshing_06.py
+++ verif/tests/preprocessor/meshing/test_surface_meshing_06.py
@@ -9,6 +9,7 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
+import math
 import geom
 import xc
 import os
@@ -37,45 +38,76 @@ seedElemHandler= preprocessor.getElementHandler.seedElemHandler
 seedElemHandler.defaultMaterial= elast2d.name
 elem= seedElemHandler.newElement("FourNodeQuad")
 
-points= preprocessor.getMultiBlockTopology.getPoints
-pt0= points.newPoint(geom.Pos3d(0.0,0.0,0.0))
-pt1= points.newPoint(geom.Pos3d(CooMaxX,0.0,0.0))
-pt2= points.newPoint(geom.Pos3d(CooMaxX,CooMaxY,0.0))
-pt3= points.newPoint(geom.Pos3d(0.0,CooMaxY,0.0))
+pt0= modelSpace.newKPoint(0.0,0.0,0.0)
+pt1= modelSpace.newKPoint(CooMaxX,0.0,0.0)
+pt2= modelSpace.newKPoint(CooMaxX,CooMaxY,0.0)
+pt3= modelSpace.newKPoint(0.0,CooMaxY,0.0)
 
-surfaces= preprocessor.getMultiBlockTopology.getSurfaces
-s1= surfaces.newQuadSurfacePts(pt0.tag,pt1.tag,pt2.tag,pt3.tag)
+s1= modelSpace.newQuadSurface(pt0, pt1, pt2, pt3)
 s1.nDivI= 4
 s1.nDivJ= 2
 
 s1.genMesh(xc.meshDir.I)
 
-modelSpace.getTotalSet().fillDownwards()
+xcTotalSet= modelSpace.getTotalSet()
+xcTotalSet.fillDownwards()
 
+# Check results.
+## Check nodes on perimeter.
 numNodesOnPerimeter= -4
 for l in s1.getSides:
     edge= l.getEdge
     numNodesOnPerimeter+= len(edge.nodes)
-    # for n in edge.nodes:
-    #     print('line= ', edge.name, ' node: ',n.tag)
 
 numNodesOnPerimeterRef= 2*s1.nDivI+2*s1.nDivJ
 ratio= abs(numNodesOnPerimeter-numNodesOnPerimeterRef)
 
+testOK= (ratio<=1e-15)
+
+## Check surface orientation.
+refIVector= (pt1.getPos-pt0.getPos).normalized()
+refJVector= geom.Vector3d(-refIVector.y, refIVector.x, refIVector.z)
+sIVector= s1.getIVector
+sJVector= s1.getJVector
+surfErrI= (sIVector-refIVector).getModulus()
+surfErrJ= (sJVector-refJVector).getModulus()
+
+testOK= testOK and (surfErrI<1e-10) and (surfErrJ<1e-10)
+
+
+## Check element orientation.
+elemErrI= 0.0
+elemErrJ= 0.0
+for e in xcTotalSet.elements:
+    iVector= e.getIVector3d(True)
+    elemErrI+= (iVector-sIVector).getModulus2()
+    jVector= e.getJVector3d(True)
+    elemErrJ+= (jVector-sJVector).getModulus2()
+elemErrI= math.sqrt(elemErrI)
+elemErrJ= math.sqrt(elemErrJ)
+
+testOK= testOK and (elemErrI<1e-10) and (elemErrJ<1e-10)
+
 '''
 print(numNodesOnPerimeter)
 print(numNodesOnPerimeterRef)
+print(elemErrI)
+print(elemErrJ)
+print(refIVector)
+print(refJVector)
+print(surfErrI)
+print(surfErrJ)
 '''
 
 import os
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if(ratio<=1e-15) :
+if(testOK):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
     
-# Graphic stuff.
+# # Graphic stuff.
 # from postprocess import output_handler
 # oh= output_handler.OutputHandler(modelSpace)
 # oh.displayBlocks()
