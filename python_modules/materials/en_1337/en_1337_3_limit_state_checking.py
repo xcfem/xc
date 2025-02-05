dc6b8cc6b6c69c838e9220c82639bc348abff8ee
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-02-05 16:37:46 +0100

    Remove redundant data from the dictionary
    The load combination is already known.

--- python_modules/materials/en_1337/en_1337_3_limit_state_checking.py
+++ python_modules/materials/en_1337/en_1337_3_limit_state_checking.py
@@ -81,7 +81,7 @@ def compute_results_under_variable_loads_on_bearings(modelSpace, bearingElements
             alpha_ad= abs(elemMaterialRY.getStrain())
             alpha_bd= abs(elemMaterialRX.getStrain())
             # Store results
-            combResults[eTag]= {'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'comb':lcName, 'bearing_type':bearingType}
+            combResults[eTag]= {'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'bearing_type':bearingType}
         retval[lcName]= combResults
     return retval
 
@@ -133,10 +133,10 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                 cf= results['sigma_perm_cf']
                 if(sigma_perm_cf>cf): # Update worst-case for the element.
                     sigma_perm= bearingMat.getCompressiveStress(vxd= vxd, vyd= vyd, Fzd= Fzd)
-                    sigma_perm_results[eTag]= {'sigma_perm_cf':sigma_perm_cf, 'sigma_perm':sigma_perm, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    sigma_perm_results[eTag]= {'sigma_perm_cf':sigma_perm_cf, 'sigma_perm':sigma_perm, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 sigma_perm= bearingMat.getCompressiveStress(vxd= vxd, vyd= vyd, Fzd= Fzd)
-                sigma_perm_results[eTag]= {'sigma_perm_cf':sigma_perm_cf, 'sigma_perm':sigma_perm, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                sigma_perm_results[eTag]= {'sigma_perm_cf':sigma_perm_cf, 'sigma_perm':sigma_perm, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             # Check friction
             Fxd= elem_results['Fxd']
             Fyd= elem_results['Fyd']
@@ -147,11 +147,11 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                 if(sliding_perm_cf>cf):
                     friction_coef= bearingMat.getFrictionCoefficient(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
                     friction_force= bearingMat.getFrictionForce(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
-                    sliding_perm_results[eTag]= {'sliding_perm_cf':sliding_perm_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    sliding_perm_results[eTag]= {'sliding_perm_cf':sliding_perm_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 friction_coef= bearingMat.getFrictionCoefficient(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
                 friction_force= bearingMat.getFrictionForce(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
-                sliding_perm_results[eTag]= {'sliding_perm_cf':sliding_perm_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                sliding_perm_results[eTag]= {'sliding_perm_cf':sliding_perm_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
     # Store results in retval variable.
     perm_check_results= {'sigma_perm_results':sigma_perm_results, 'sliding_perm_results':sliding_perm_results}
     retval= {'perm_check_results':perm_check_results}
@@ -197,14 +197,14 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                     compressive_strain_outer= bearingMat.getCompressiveStrain(vxd= vxd, vyd= vyd, Fzd= Fzd, innerLayer= False)
                     shear_strain= bearingMat.getShearStrain(vxd= vxd, vyd= vyd)
                     rotation_strain= bearingMat.getAngularRotationStrain(alpha_ad= alpha_ad, alpha_bd= alpha_bd)
-                    total_strain_results[eTag]= {'total_strain_cf':total_strain_cf, 'total_strain':total_strain, 'compressive_strain_inner':compressive_strain_inner, 'compressive_strain_outer':compressive_strain_outer, 'shear_strain':shear_strain, 'rotation_strain':rotation_strain, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    total_strain_results[eTag]= {'total_strain_cf':total_strain_cf, 'total_strain':total_strain, 'compressive_strain_inner':compressive_strain_inner, 'compressive_strain_outer':compressive_strain_outer, 'shear_strain':shear_strain, 'rotation_strain':rotation_strain, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 total_strain= bearingMat.getCompressiveStress(vxd= vxd, vyd= vyd, Fzd= Fzd)
                 compressive_strain_inner= bearingMat.getCompressiveStrain(vxd= vxd, vyd= vyd, Fzd= Fzd, innerLayer= True)
                 compressive_strain_outer= bearingMat.getCompressiveStrain(vxd= vxd, vyd= vyd, Fzd= Fzd, innerLayer= False)
                 shear_strain= bearingMat.getShearStrain(vxd= vxd, vyd= vyd)
                 rotation_strain= bearingMat.getAngularRotationStrain(alpha_ad= alpha_ad, alpha_bd= alpha_bd)
-                total_strain_results[eTag]= {'total_strain_cf':total_strain_cf, 'total_strain':total_strain, 'compressive_strain_inner':compressive_strain_inner, 'compressive_strain_outer':compressive_strain_outer, 'shear_strain':shear_strain, 'rotation_strain':rotation_strain, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                total_strain_results[eTag]= {'total_strain_cf':total_strain_cf, 'total_strain':total_strain, 'compressive_strain_inner':compressive_strain_inner, 'compressive_strain_outer':compressive_strain_outer, 'shear_strain':shear_strain, 'rotation_strain':rotation_strain, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             # Check minimum thickness of steel laminate.
             laminate_thickness_cf= bearingMat.getPlateThicknessEfficiency(vxd= vxd, vyd= vyd, Fzd= Fzd, withHoles= False)
             if(eTag in laminate_thickness_results):
@@ -212,19 +212,19 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                 cf= results['laminate_thickness_cf']
                 if(laminate_thickness_cf>cf): # Update worst-case for the element.
                     req_laminate_thickness= bearingMat.getRequiredReinforcedPlateThickness(vxd= vxd, vyd= vyd, Fzd= Fzd, withHoles= False)
-                    laminate_thickness_results[eTag]= {'laminate_thickness_cf':laminate_thickness_cf, 'req_laminate_thickness':req_laminate_thickness, 'laminate_thickness':bearingMat.ts, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'with_holes':False, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    laminate_thickness_results[eTag]= {'laminate_thickness_cf':laminate_thickness_cf, 'req_laminate_thickness':req_laminate_thickness, 'laminate_thickness':bearingMat.ts, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'with_holes':False,  'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 req_laminate_thickness= bearingMat.getRequiredReinforcedPlateThickness(vxd= vxd, vyd= vyd, Fzd= Fzd, withHoles= False)
-                laminate_thickness_results[eTag]= {'laminate_thickness_cf':laminate_thickness_cf, 'req_laminate_thickness':req_laminate_thickness, 'laminate_thickness':bearingMat.ts, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'with_holes':False, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                laminate_thickness_results[eTag]= {'laminate_thickness_cf':laminate_thickness_cf, 'req_laminate_thickness':req_laminate_thickness, 'laminate_thickness':bearingMat.ts, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'with_holes':False, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             # Check buckling stability of steel laminate.
             buckling_stability_cf= bearingMat.getBucklingEfficiency(vxd= vxd, vyd= vyd, Fzd= Fzd)
             if(eTag in buckling_stability_results):
                 results= buckling_stability_results[eTag]
                 cf= results['buckling_stability_cf']
                 if(buckling_stability_cf>cf): # Update worst-case for the element.
-                    buckling_stability_results[eTag]= {'buckling_stability_cf':buckling_stability_cf, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    buckling_stability_results[eTag]= {'buckling_stability_cf':buckling_stability_cf, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
-                buckling_stability_results[eTag]= {'buckling_stability_cf':buckling_stability_cf, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                buckling_stability_results[eTag]= {'buckling_stability_cf':buckling_stability_cf, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             # Check rotational deflection.
             rotational_deflection_cf= bearingMat.getRotationalLimitEfficiency(vxd= vxd, vyd= vyd, Fzd= Fzd, alpha_ad= alpha_ad, alpha_bd= alpha_bd)
             if(eTag in rotational_deflection_results):
@@ -233,11 +233,11 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                 if(rotational_deflection_cf>cf): # Update worst-case for the element.
                     vertical_deflection= bearingMat.getTotalVerticalDeflection(vxd= vxd, vyd= vyd, Fzd= Fzd)
                     rotational_deflection= bearingMat.getRotationalDeflection(alpha_ad= alpha_ad, alpha_bd= alpha_bd)
-                    rotational_deflection_results[eTag]= {'rotational_deflection_cf':rotational_deflection_cf, 'rotational_deflection':rotational_deflection, 'vertical_deflection':vertical_deflection, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    rotational_deflection_results[eTag]= {'rotational_deflection_cf':rotational_deflection_cf, 'rotational_deflection':rotational_deflection, 'vertical_deflection':vertical_deflection, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 vertical_deflection= bearingMat.getTotalVerticalDeflection(vxd= vxd, vyd= vyd, Fzd= Fzd)
                 rotational_deflection= bearingMat.getRotationalDeflection(alpha_ad= alpha_ad, alpha_bd= alpha_bd)
-                rotational_deflection_results[eTag]= {'rotational_deflection_cf':rotational_deflection_cf, 'rotational_deflection':rotational_deflection, 'vertical_deflection':vertical_deflection, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                rotational_deflection_results[eTag]= {'rotational_deflection_cf':rotational_deflection_cf, 'rotational_deflection':rotational_deflection, 'vertical_deflection':vertical_deflection, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'alpha_ad':alpha_ad, 'alpha_bd':alpha_bd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             # Check friction.
             sliding_cf= bearingMat.getSlidingConditionEfficiency(vxd= vxd, vyd= vyd, Fxd= Fxd, Fyd= Fyd, Fzd_min= Fzd, concreteBedding= True)
             if(eTag in sliding_var_results):
@@ -246,11 +246,11 @@ def check_results_en_1337_3(analysisResults, bearingElements, bearingMatDict):
                 if(sliding_cf>cf): # Update worst-case for the element.
                     friction_coef= bearingMat.getFrictionCoefficient(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
                     friction_force= bearingMat.getFrictionForce(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
-                    sliding_var_results[eTag]= {'sliding_cf':sliding_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                    sliding_var_results[eTag]= {'sliding_cf':sliding_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
             else: # no data for this element yet.
                 friction_coef= bearingMat.getFrictionCoefficient(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
                 friction_force= bearingMat.getFrictionForce(vxd= vxd, vyd= vyd, Fzd_min= Fzd, concreteBedding= True)
-                sliding_var_results[eTag]= {'sliding_cf':sliding_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'comb':lcName, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
+                sliding_var_results[eTag]= {'sliding_cf':sliding_cf, 'friction_coef':friction_coef, 'friction_force':friction_force, 'Fxd':Fxd, 'Fyd':Fyd, 'Fzd':Fzd, 'vxd':vxd, 'vyd':vyd, 'bearing_type':bearingType, 'x': centroid.x, 'y':centroid.y, 'z':centroid.z}
     # Store results in retval variable.
     var_check_results= {'total_strain_results': total_strain_results, 'laminate_thickness_results': laminate_thickness_results, 'buckling_stability_results': buckling_stability_results, 'rotational_deflection_results': rotational_deflection_results, 'sliding_var_results': sliding_var_results} 
     retval.update({'var_check_results':var_check_results})
