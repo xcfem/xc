f9a77fb3bf43c2518325ddb89ffcd92d987cbdbb
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-02-13 12:12:44 +0100

    Update reference values
    Following the change in the earth_pressure module to fix the error
    described in the previous commit, the value of the overturning safety
    factor in this test has slightly changed. The error tolerance has
    also been increased to account for little differences in the results
    depending on the machine.

--- verif/tests/rough_calculations/earth_retaining/retaining_wall_test_01.py
+++ verif/tests/rough_calculations/earth_retaining/retaining_wall_test_01.py
@@ -227,16 +227,16 @@ sr= wall.performStabilityAnalysis(stabilityULSCombinations,foundationSoilModel, 
 uls_results= wall.performULSAnalysis(strengthULSCombinations)
 wall.setULSInternalForcesEnvelope(uls_results.internalForces)
 
-err= ratioStemPositions**2+math.sqrt((sr.Foverturning-7.126392782772212)**2+(sr.Fsliding-1.5388825671842905)**2+(sr.Fbearing-1.1391242090325084)**2+(sr.FadmPressure-0.9478396196024435)**2)
+err= ratioStemPositions**2+math.sqrt((sr.Foverturning-7.126392782772217)**2+(sr.Fsliding-1.5388825671842905)**2+(sr.Fbearing-1.1391242090325084)**2+(sr.FadmPressure-0.9478396196024435)**2)
 err= math.sqrt(err)
 
-'''
 print('ratioStemPositions= ', ratioStemPositions)
 print("Overturning: ",sr.Foverturning)
 print("Sliding: ",sr.Fsliding)
 print("Bearing: ",sr.Fbearing)
 print("Allow. press. ratio: ",sr.FadmPressure)
 print("error.: ",err)
+'''
 '''
 
 pth= "/tmp/tmp_results/"
@@ -249,7 +249,7 @@ wall.drawSchema(pth)
 
 from misc_utils import log_messages as lmsg
 fname= os.path.basename(__file__)
-if(abs(err)<1e-9):
+if(abs(err)<1e-7):
     print('test '+fname+': ok.')
 else:
     lmsg.error(fname+' ERROR.')
