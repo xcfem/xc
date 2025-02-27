c60be7ace40cef291f2760148629931f3a51125b
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-02-27 15:15:21 +0100

    Prepare elastomeric bearing data from EN 1337-3:2005 (E) Table 3
    Create a JSON file containing the data from the table.

---
+++ python_modules/materials/en_1337/aux/import_round_elastomeric_bearings.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+''' EN 1337-3:2005 (E) Table 3 — Standard sizes for bearings type B.
+    Round elastomeric bearings.
+'''
+
+__author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
+__copyright__= "Copyright 2025, LCPT and AOO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import csv
+
+fNameIn= 'tata_steel_chs_shapes.csv'
+fNameOut= 'dict_tata_steel_chs_shapes.py'
+dictName= 'CHSshapes'
