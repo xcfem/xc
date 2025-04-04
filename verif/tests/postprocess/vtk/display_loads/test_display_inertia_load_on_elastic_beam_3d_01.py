3f1f7971cccd88d692830d8fae91d6eeb9fd0316
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-04-04 16:38:08 +0200

    Add verification test for representation of inertia loads

---
+++ verif/tests/postprocess/vtk/display_loads/test_display_inertia_load_on_elastic_beam_3d_01.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+'''Check representation of inertia loads on ElasticBeam3d elements.'''
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
+__copyright__= "Copyright 2025, LCPT and AOO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import os
+import xc
+from model import predefined_spaces
+from materials.ehe import EHE_materials
+from materials.sections.fiber_section import def_simple_RC_section
+#from postprocess import output_handler
+feProblem= xc.FEProblem()
+preprocessor=  feProblem.getPreprocessor
+nodes= preprocessor.getNodeHandler
+
+# Problem type
+modelSpace= predefined_spaces.StructuralMechanics3D(nodes)
+
+# Create nodes.
+l= 10.0
+n1= nodes.newNodeXYZ(0,0,0)
+n2= nodes.newNodeXYZ(l,0,0)
+
+# Create elements.
+## Coordinate transformation.
+lin= modelSpace.newLinearCrdTransf("lin",xc.Vector([0,0,1]))
+modelSpace.setDefaultCoordTransf(lin)
+
+## Material
+structureConcrete= EHE_materials.HA30
+reinfSteel= EHE_materials.B500S
+stiffnessReductionFactor= 1
+lintelsRCSection= def_simple_RC_section.RCRectangularSection(name= 'lintelsRCSection', sectionDescr= 'lintels section', concrType= structureConcrete, reinfSteelType= reinfSteel, width= 1.5, depth= 1.5)
+lintelsMaterial= lintelsRCSection.defElasticShearSection3d(preprocessor, reductionFactor= stiffnessReductionFactor)
+modelSpace.setDefaultMaterial(lintelsMaterial)
+
+## Element definition.
+elements= preprocessor.getElementHandler
+beam= elements.newElement("ElasticBeam3d",xc.ID([n1.tag,n2.tag]))
+
+## Whole model mass data.
+xcTotalSet= modelSpace.getTotalSet()
+massZ= xcTotalSet.getTotalMassComponent(2)
+massRefZ= 2500*1.5*1.5*l
+ratio1= abs(massZ-massRefZ)/massRefZ
+
+# Constraints.
+# Zero movement for node 1.
+modelSpace.fixNode000_0FF(n1.tag)
+# Partial constraint for node 2.
+modelSpace.fixNode000_FFF(n2.tag)
+
+
+# Load definition.
+lp0= modelSpace.newLoadPattern(name= '0')
+modelSpace.setCurrentLoadPattern("0")
+gravity= 9.81
+accel= xc.Vector([0,0,gravity])
+xcTotalSet= modelSpace.getTotalSet()
+modelSpace.createSelfWeightLoad(xcTotalSet, accel)
+# We add the load case to domain.
+modelSpace.addLoadCaseToDomain(lp0.name)
+
+# Graphic stuff.
+from postprocess import output_handler
+oh= output_handler.OutputHandler(modelSpace)
+# oh.displayLoads()
+
+fname= os.path.basename(__file__)
+outputFileName= '/tmp/'+fname.replace('.py', '.jpeg')
+oh.displayLoads(fileName= outputFileName)
+
+# Check that file exists
+testOK= os.path.isfile(outputFileName)
+
+if testOK:
+    print('test '+fname+': ok.')
+else:
+    lmsg.error(fname+' ERROR.')
+
+os.remove(outputFileName)
---
+++ verif/tests/postprocess/vtk/display_loads/test_display_inertia_load_on_truss_01.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+'''Check representation of inertia loads on Truss elements.'''
+
+from __future__ import division
+from __future__ import print_function
+
+__author__= "Luis C. Pérez Tato (LCPT) and Ana Ortega (AOO)"
+__copyright__= "Copyright 2015, LCPT and AOO"
+__license__= "GPL"
+__version__= "3.0"
+__email__= "l.pereztato@gmail.com"
+
+import os
+import xc
+from model import predefined_spaces
+from materials import typical_materials
+
+gravity= 1#9.81
+E= 30e6 # Young modulus (psi)
+nu= 0.3 # Poisson's ratio
+G= E/(2*(1+nu)) # Shear modulus
+l= 10 # Bar length
+b= 0.1
+A= b*b #Área in square inches.
+Iz= 1/12.0*b**4 # Cross section moment of inertia (m4)
+
+feProblem= xc.FEProblem()
+preprocessor=  feProblem.getPreprocessor
+nodes= preprocessor.getNodeHandler
+
+# Problem type
+modelSpace= predefined_spaces.StructuralMechanics2D(nodes)
+
+n1= nodes.newNodeXY(0,0)
+n2= nodes.newNodeXY(l,0)
+
+# Materials definition
+elast= typical_materials.defElasticMaterial(preprocessor, "elast",E)
+elast.E= E
+elast.rho= 10.0
+lin= modelSpace.newLinearCrdTransf("lin")
+# Materials
+sectionProperties= xc.CrossSectionProperties2d()
+sectionProperties.A= A; sectionProperties.E= E; sectionProperties.G= G
+sectionProperties.I= Iz;
+sectionProperties.rho= elast.rho*A # Linear rho
+section= typical_materials.defElasticSectionFromMechProp2d(preprocessor, "section",sectionProperties)
+
+
+# Element definition.
+elements= preprocessor.getElementHandler
+elements.dimElem= 2 # Bidimensional space.
+elements.defaultMaterial= elast.name
+truss= elements.newElement("Truss",xc.ID([n1.tag,n2.tag]))
+truss.sectionArea= A
+
+trussMassZ= truss.getTotalMassComponent(1)
+trussMassRefZ= truss.sectionArea*elast.rho*l
+ratio1= abs(trussMassZ-trussMassRefZ)/trussMassRefZ
+
+constraints= preprocessor.getBoundaryCondHandler
+# Zero movement for node 1.
+spc1= constraints.newSPConstraint(n1.tag,0,0.0)
+spc2= constraints.newSPConstraint(n1.tag,1,0.0)
+spc3= constraints.newSPConstraint(n1.tag,2,0.0)
+# Zero movement for node 2.
+spc4= constraints.newSPConstraint(n2.tag,0,0.0)
+spc5= constraints.newSPConstraint(n2.tag,1,0.0)
+spc6= constraints.newSPConstraint(n2.tag,2,0.0)
+
+# Load definition.
+lp0= modelSpace.newLoadPattern(name= '0')
+modelSpace.setCurrentLoadPattern(lp0.name)
+accel= xc.Vector([0,gravity])
+truss.createInertiaLoad(accel)
+
+# We add the load case to domain.
+modelSpace.addLoadCaseToDomain(lp0.name)
+
+# Graphic stuff.
+from postprocess import output_handler
+oh= output_handler.OutputHandler(modelSpace)
+# oh.displayLoads()
+
+fname= os.path.basename(__file__)
+outputFileName= '/tmp/'+fname.replace('.py', '.jpeg')
+oh.displayLoads(fileName= outputFileName)
+
+# Check that file exists
+testOK= os.path.isfile(outputFileName)
+
+if testOK:
+    print('test '+fname+': ok.')
+else:
+    lmsg.error(fname+' ERROR.')
+
+os.remove(outputFileName)
