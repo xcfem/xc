45fbc69c30b41ab0cab87cd5146d55ca23799b4a
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-04-06 15:16:22 +0200

    Add plotting stresses on brick element verification test

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -1918,6 +1918,7 @@ python tests/postprocess/vtk/test_display_zero_length_local_axes_01.py
 python tests/postprocess/vtk/test_display_zero_length_local_axes_02.py
 python tests/postprocess/vtk/test_display_reactions_01.py
 python tests/postprocess/vtk/test_display_eigenvectors_01.py
+python tests/postprocess/vtk/test_display_stresses_on_brick_01.py
 echo "$BLEU" "    Graphic output. Loads." "$NORMAL"
 python tests/postprocess/vtk/display_loads/test_display_loads_01.py
 python tests/postprocess/vtk/display_loads/test_display_loads_02.py
