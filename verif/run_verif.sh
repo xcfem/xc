d3c7c8f6dcbfc9aecff08d69a700c1bf91f9b914
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-04-10 17:42:12 +0200

    Implement graphical representation of strain loads (work in progress)
    Implement graphical output for strain loads on 2D beams.

--- verif/run_verif.sh
+++ verif/run_verif.sh
@@ -1931,6 +1931,7 @@ python tests/postprocess/vtk/display_loads/test_display_inertia_load_on_truss_01
 python tests/postprocess/vtk/display_loads/test_display_strain_load_on_brick_01.py
 python tests/postprocess/vtk/display_loads/test_display_strain_load_on_quad_01.py
 python tests/postprocess/vtk/display_loads/test_display_strain_load_on_truss_01.py
+python tests/postprocess/vtk/display_loads/test_display_strain_load_on_elastic_beam_2d_01.py
 
 # Report tests.
 echo "$BLEU" "  Report output." "$NORMAL"
