720dad655940667ee517a356c3ac89305b77a847
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-04-22 13:06:11 +0200

    Enhance documentation

--- verif/tests/postprocess/vtk/test_display_reactions_01.py
+++ verif/tests/postprocess/vtk/test_display_reactions_01.py
@@ -112,4 +112,4 @@ if testOK:
 else:
     lmsg.error(fname+' ERROR.')
 
-os.remove(outputFileName)
+os.remove(outputFileName) # Clean after yourself.
--- verif/tests/postprocess/vtk/test_display_zero_length_local_axes_01.py
+++ verif/tests/postprocess/vtk/test_display_zero_length_local_axes_01.py
@@ -64,4 +64,4 @@ if testOK:
 else:
     lmsg.error(fname+' ERROR.')
 
-os.remove(outputFileName)
+os.remove(outputFileName) # Clean after yourself.
