5d15412f00ce01573785c7eaa0f438c20e0a129f
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-08-10 18:20:05 +0200

    Verify calculation of K1 factor according to figure 2.29 of IAPF

--- verif/tests/actions/thermal/test_iapf_thermal.py
+++ verif/tests/actions/thermal/test_iapf_thermal.py
@@ -10,11 +10,20 @@ __license__= "GPL"
 __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
+import math
 from actions.thermal import IAPF_thermal
 
 annualTempVariation= IAPF_thermal.get_annual_temp_variation(deck_type= 'concrete_beams', z= 5, h= 1.8, s=2.5)
+err= (annualTempVariation-43.6176116422547)**2
 
-err= (annualTempVariation-43.6176116422547)/43.6176116422547
+# Compute K1 factor according to to figure 2.29 of IAPF-07.
+## h: depth of the deck beams (m).
+## d: distance between beams (m).
+k1= IAPF_thermal.get_k1_according_to_figure_2_29(h= 2.0, d=2.5)
+err+= (k1-0.8)**2
+
+err= math.sqrt(err)
+
 '''
 print(annualTempVariation, err)
 '''
