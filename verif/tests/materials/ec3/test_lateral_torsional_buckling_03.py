77b91fd3bdf869a4999677097ef649e47527d45a
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-08-11 14:12:55 +0200

    Remove unused code

--- verif/tests/materials/ec3/test_lateral_torsional_buckling_03.py
+++ verif/tests/materials/ec3/test_lateral_torsional_buckling_03.py
@@ -22,9 +22,6 @@ S355JR.gammaM= gammaM0
 IPE400= EC3_materials.IPEShape(S355JR,"IPE_400")
 IPE400.sectionClass= 1
 
-# Geometry
-k1= 1.0; k2= 1.0
-
 # Check results pages 34 and 35
 L= 3 # Bar length (m)
 x= [0.0,0.25*L,0.5*L,0.75*L,1.0*L] # points along the beam.
