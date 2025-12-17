c50d01d8ca94c0da1a4e7ee4e58dfa547a522877
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2025-12-17 15:31:45 +0100

    Minor debugging

--- python_modules/model/mesh/strut_and_tie_utils.py
+++ python_modules/model/mesh/strut_and_tie_utils.py
@@ -11,6 +11,9 @@ __version__= "3.0"
 __email__= "l.pereztato@gmail.com"
 
 from model.mesh import strut_and_tie_base
+from model.mesh.strut_and_tie_base import dummy_spring_stiffness
+from model.mesh.strut_and_tie_base import create_dummy_spring_material
+from model.mesh.strut_and_tie_base import define_dummy_springs
 from model.mesh.strut_and_tie_base import create_dummy_spring
 from model.mesh.strut_and_tie_base import StrutAndTieModel
 from model.mesh.pile_cap_2_piles import PileCap2Piles
