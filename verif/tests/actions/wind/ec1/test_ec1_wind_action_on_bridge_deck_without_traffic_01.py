0c993c02a302acbb770fcd26524f7142758c2b3b
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2026-03-02 17:15:02 +0100

    Minor debugging

--- verif/tests/actions/wind/ec1/test_ec1_wind_action_on_bridge_deck_without_traffic_01.py
+++ verif/tests/actions/wind/ec1/test_ec1_wind_action_on_bridge_deck_without_traffic_01.py
@@ -85,7 +85,7 @@ ratio9= abs(Fw-1341.5323304243852e3)/1341.5323304243852e3
 alph= math.radians(10)
 beta= 0.0
 cfz= ec1_wind.get_bridge_deck_vertical_force_coefficient(b= deckWidth, dtotVP= dtot, alpha= alph, beta= beta)
-Fwz= cscd*cfx*qp*deckWidth*deckLength
+Fwz= cscd*cfz*qp*deckWidth*deckLength
 # Compute vertical pressure distribution.
 vpd= ec1_wind.get_vertical_pressure_distribution(terrainCategory= terrainCategory, x0= -deckWidth/2.0, x1= deckWidth/2.0, dtotVP= dtot, z= z, vb= vb, zMax= zMax, rho= rho, k1= k1, c0= c0, alpha= alph, beta= beta)
 
