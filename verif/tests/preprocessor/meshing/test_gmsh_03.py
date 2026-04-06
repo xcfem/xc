955b41122cd51a16ad488d65ec651d89e74d6d0b
Ana Ortega <ana.ortega.ort@gmail.com>
2026-04-06 18:40:50 +0200

    Add results of Gmsh 5.0.0

--- verif/tests/preprocessor/meshing/test_gmsh_03.py
+++ verif/tests/preprocessor/meshing/test_gmsh_03.py
@@ -83,8 +83,8 @@ xcTotalSet= modelSpace.getTotalSet()
 nNodes= len(xcTotalSet.nodes)
 nElements= len(xcTotalSet.elements)
 
-nNodesRef= [206, 207, 215] # Different Gmsh versions give different results
-nElementsRef= [179, 180, 188]
+nNodesRef= [205, 206, 207, 215] # Different Gmsh versions give different results
+nElementsRef= [178, 179, 180, 188]
 
 nNodesOk= False
 if nNodes in nNodesRef:
