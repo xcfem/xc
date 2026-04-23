aac56ea368bf31d38603a694c02ccee13caa5084
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2026-04-23 18:45:53 +0200

    Make sure that the required propery has been created

--- verif/tests/postprocess/vtk/geotechnics/test_display_pile_spring_stiffnesses_01.py
+++ verif/tests/postprocess/vtk/geotechnics/test_display_pile_spring_stiffnesses_01.py
@@ -82,6 +82,18 @@ micropile.pileSet= l
 micropile.generateSpringsPile(alphaKh_x=1,alphaKh_y=1,alphaKv_z=1)
 bottomTipNode= pBottom.getNode()
 
+# Check that the properties are set.
+k_x_ref_values= {4: 190500.0, 5: 381000.0, 6: 571500.0, 7: 762000.0, 8: 952500.0, 9: 1143000.0, 10: 1333500.0, 11: 1524000.0, 12: 1714500.0, 13: 1905000.0, 14: 2095500.0, 15: 2286000.0, 16: 2476500.0, 17: 2667000.0, 18: 2857500.0, 19: 3048000.0, 20: 3238500.0, 1: 1714500.0}
+err= 0.0
+for n in micropile.pileSet.nodes:
+    if(n.hasProp('k_x')):
+        nTag= n.tag
+        k_x= n.getProp('k_x')
+        k_x_ref= k_x_ref_values[nTag]
+        err+= (k_x-k_x_ref)**2
+err= math.sqrt(err)
+testOK= (err<1e-6)
+
 # Graphic stuff.
 from postprocess import output_handler
 oh= output_handler.OutputHandler(modelSpace)
@@ -91,7 +103,7 @@ outputFileName= '/tmp/'+fname.replace('.py', '.jpeg')
 oh.displayNodeValueDiagram(itemToDisp= 'k_x', caption= 'Spring stiffness x direction', defaultDirection= 'X', defaultValue= 0.0, fileName= outputFileName)
 
 # Check that file exists
-testOK= os.path.isfile(outputFileName)
+testOK= testOK and os.path.isfile(outputFileName)
 
 from misc_utils import log_messages as lmsg
 if testOK:
