bdf8468a060fe551f70f199f511a02c8af2994a6
Luis Claudio Pérez Tato <l.pereztato@ciccp.es>
2026-05-09 22:08:58 +0200

    Write display region cell centers verification test

--- verif/tests/solution/push_over/test_display_section_region_cells.py
+++ verif/tests/solution/push_over/test_display_section_region_cells.py
@@ -24,6 +24,9 @@ As= 0.60  # area of no. 7 bars
 columnFiberSection= rc_column_fiber_section.def_rc_column_fiber_section(preprocessor, As= As)
 
 sectionGeometry= columnFiberSection.getSectionGeometry
+diagonalLength= sectionGeometry.getBnd().diagonal.getModulus()
+offset= diagonalLength/2e2
+fontsize= 5
 regions= sectionGeometry.getRegions
 
 import matplotlib.pyplot as plt
@@ -42,8 +45,8 @@ for color, region in zip(colors, regions):
         yi.append(y)
         area= cell.getArea()
         matName= regionMaterialName
-        plt.annotate(matName, (x, y + 0.2))
-        plt.annotate(cf.Area.format(area), (x, y - 0.2))
+        plt.annotate(matName, (x, y + offset), fontsize= fontsize)
+        plt.annotate(cf.Area.format(area), (x, y - offset), fontsize= fontsize)
     plt.plot(xi, yi, 'o', color= color)
 plt.axis('equal')
 plt.show()
