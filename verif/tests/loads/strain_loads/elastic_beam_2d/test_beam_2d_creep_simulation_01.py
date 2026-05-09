8c7d08e69769415dd038fecc16ceaadfe10b72ef
Luis Claudio Pérez Tato <l.pereztato@ciccp.es>
2026-05-09 09:21:10 +0200

    Simplify code
    No need to define a solution procedure and then use the same one
    through the ModelSpace Python object.

--- verif/tests/loads/strain_loads/elastic_beam_2d/test_beam_2d_creep_simulation_01.py
+++ verif/tests/loads/strain_loads/elastic_beam_2d/test_beam_2d_creep_simulation_01.py
@@ -75,8 +75,7 @@ eleLoad.frontEndDeformationPlane= thermalDeformation
 # We add the load case to domain.
 modelSpace.addLoadCaseToDomain(lp0.name)
 
-analysis= predefined_solutions.simple_static_linear(feProblem)
-result= analysis.analyze(1)
+modelSpace.analyze(1)
 
 # Get internal forces.
 beam.getResistingForce()
