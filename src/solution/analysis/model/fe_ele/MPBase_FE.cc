a3f3175238dbfe1c06ff7c7df65f8653ce6d9651
Luis C. Pérez Tato <l.pereztato@ciccp.es>
2026-08-18 12:45:27 +0200

    Remove debug messages.

--- src/solution/analysis/model/fe_ele/MPBase_FE.cc
+++ src/solution/analysis/model/fe_ele/MPBase_FE.cc
@@ -69,7 +69,7 @@ int XC::MPBase_FE::determineConstrainedDOFsIDs(const MFreedom_ConstraintBase &th
 		      << constrained << " at constrained node: "
 		      << theConstrainedNode->getTag()
 		      << Color::def << std::endl;
-	    myID(i+offset) = -1; // modify so nothing will be added to equations
+	    this->myID(i+offset) = -1; // modify so nothing will be added to equations
 	    retval = -3;
 	  }    	
 	else
@@ -79,11 +79,11 @@ int XC::MPBase_FE::determineConstrainedDOFsIDs(const MFreedom_ConstraintBase &th
 		std::cerr << Color::red << getClassName() << "::" << __FUNCTION__
 			  << "; WARNING nodes DOF_Group too small"
 			  << Color::def << std::endl;
-		myID(i+offset) = -1; // modify so nothing will be added to equations
+		this->myID(i+offset) = -1; // modify so nothing will be added to equations
 		retval = -4;
 	      }
 	    else
-	      myID(i+offset) = theConstrainedNodesID(constrained);
+	      this->myID(i+offset) = theConstrainedNodesID(constrained);
 	  }
       }
     return retval;
@@ -111,8 +111,6 @@ XC::DOF_Group *XC::MPBase_FE::determineConstrainedNodeDofGrpPtr(void)
 		    << "; WARNING - node no Group yet?"
 		    << Color::def << std::endl;
       }
-    std::cout << getClassName() << "::" << __FUNCTION__
-              << " retval= " << retval << std::endl;
     return retval;
   }
 
